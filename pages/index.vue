c7cf1542aea844ff28423f2762a7cec418f72bda
drewbaker <drewrb@gmail.com>
2021-04-07 23:35:09 +0200

    Added SASS and font files

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+    <div class="page page-home">
+        Home page here.
+
+        <p>Test</p>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.page-home {
+    p {
+        color: red;
+    }
+}
+</style>
