c426ed7aa46c9ace288b118e2d5b671c05bfd5c7
jendiamond <thejendiamond@gmail.com>
2021-04-20 11:28:07 -0700

    SiteBrandBar
    **Stories:** ~/stories/{filename}.stories.js
    
    **Notes:**
    
    {Any notes about what you built. How does it work? Anything missing?}
    
    **Time Report:**
    
    This took me {x} hours to build this.
    
    **Checklist:**
    
    -   [ ] I double checked it looks like the designs
    -   [ ] I completed any required mobile breakpoint styling
    -   [ ] I completed any required hover state styling
    -   [ ] I included a working Storybook file
    -   [ ] I included a Story that showed some edge case working correctly (long text, short screen, missing image etc.)
    -   [ ] I added notes above about how long it took to build this component
    -   [ ] I assigned this PR to someone to review
    
    ---
    
    modified:   components/SiteBrandBar.vue
    new file:   stories/SiteBrandBar.stories.js

--- components/SiteBrandBar.vue
+++ components/SiteBrandBar.vue
@@ -1,24 +1,34 @@
 <template lang="html">
-    <div  class="site-brand-bar">
-        <nuxt-link to="https://www.ucla.edu"><img src="https://brand.ucla.edu/images/logo-ucla.svg" alt="UCLA logo" /></nuxt-link>
+    <div class="site-brand-bar">
+        <a
+            href="https://www.ucla.edu"
+            target="_blank"
+        >
+            <img
+                class="ucla-logo"
+                src="https://brand.ucla.edu/images/logo-ucla.svg"
+                alt="UCLA logo"
+            >
+        </a>
     </div>
 </template>
-
-<script>
-export default {
-    name: "SiteBrandBar"
-}
-</script>
 
 <style lang="scss" scoped>
 .site-brand-bar {
     height: 40px;
-    background-color: #3284bf; //--color-blue-01;
-    margin: -8px;
-}
-img {
-  height: 25px;
-  padding-top: 5px;
-  padding-left: 70px;
+    background-color: var(--color-blue-01);
+    padding: 0 var(--unit-gutter);
+
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-content: center;
+    align-items: flex-start;
+
+    .ucla-logo {
+        height: 16px;
+        width: auto;
+    }
 }
 </style>
