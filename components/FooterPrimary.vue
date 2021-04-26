b8927ae4be6956d6d09674a0329a2125fa01625f
jendiamond <thejendiamond@gmail.com>
2021-04-26 12:51:25 -0700

    All info in

---
+++ components/FooterPrimary.vue
@@ -0,0 +1,214 @@
+<template lang="html">
+    <footer class="footer-primary">
+        <div class="container">
+
+            <div class="footer-links">
+                <div class="ucla-library-logo">
+                    <svg-logo-ucla-library-underline class="logo-svg" />
+                </div>
+                <div class="socials">
+                    <ul class="social-links">
+                        <li v-for="item in items">
+                            <smart-link
+                                class="social-links"
+                                :to="item.to"
+                                :target="item.target"
+                                v-html="item.text"
+                            />
+                        </li>
+                    </ul>
+                </div>
+                <div class="press-room-block">
+                    <ul>
+                        <li class="press-room">Press Room</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="form">
+                <div class="stay-updated-block">
+                    <h1 class="stay-updated">
+                        Stay updated
+                    </h1>
+                    <p class="stay-updated">
+                        Subscribe to get the latest updates on what's happening
+                        with UCLA Library.
+                    </p>
+                    <div  class="stay-updated-form">
+                        <input placeholder="Email Address"  class="stay-updated-email"></input><input class="submit-button " type="submit">
+                        <svg-arrow-right class="arrow-svg" />
+                        </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+import SvgLogoUclaLibraryUnderline from "~/assets/svg/logo-ucla-library-underline"
+
+import SvgArrowRight from "~/assets/svg/arrow-right"
+
+export default {
+    name: "FooterPrimary",
+    components: {
+        SvgLogoUclaLibraryUnderline,
+        SvgArrowRight,
+    },
+    props: {
+        items: {
+            // Mock: api.links
+            type: Array,
+            default: () => [{ text: "", url: "", target: "_self" }],
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.footer-primary {
+    background-color: var(--color-primary-blue);
+    height: --unit-footer-height;
+    border-bottom: 40px solid var(--color-yelow);
+
+    .container {
+        max-width: 1077;
+        border-bottom: 40px solid var(--color-yelow);
+
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        justify-content: center;
+        align-content: center;
+        align-items: center;
+
+        .ucla-library-logo {
+            flex: 1 1 auto;
+            height: 350px;
+
+            display: flex;
+            align-items: flex-end;
+
+            .logo-svg {
+                max-width: 300px;
+                width: 100%;
+                height: auto;
+            }
+        }
+
+        .socials {
+            .social-links {
+                display: flex;
+                flex-direction: row;
+                flex-wrap: nowrap;
+                justify-content: space-between;
+                align-content: flex-start;
+                align-items: flex-start;
+
+                line-height: 1em;
+                color: var(--color-white);
+                padding-right: 10px;
+                padding-left: 10px;
+                border-right: 1px solid var(--color-white);
+                text-decoration: none;
+            }
+        }
+        .press-room-block {
+            .press-room {
+                display: block;
+                color: var(--color-white);
+                border-bottom: 2px solid var(--color-primary-yellow);
+                text-transform: uppercase;
+                &:hover {
+                    color: var(--color-primary-yellow);
+                }
+            }
+        }
+        .stay-updated {
+            color: var(--color-white);
+            font-weight: 200;
+        }
+        .stay-updated-form {
+            background-color: var(--color-primary-blue);
+            border: none;
+            border-bottom: 2px solid var(--color-green-01);
+
+            display: flex;
+            flex-direction: row;
+            flex-wrap: nowrap;
+            justify-content: flex-start;
+            align-content: flex-end;
+            align-items: flex-start;
+
+            &:hover {
+                color: var(--color-white);
+                background-color: coral;
+                opacity: 0.1;
+            }
+        }
+        .stay-updated-email {
+            display: flex;
+            order: 0;
+            flex: 1 1 auto;
+            align-self: auto;
+        }
+        ::placeholder{
+            color: white;
+        }
+        .submit-button {
+            background-color: unset;
+            border: 0px;
+            color: var(--color-white)
+        }
+        .arrow-svg {
+            fill: var(--color-white);
+        }
+    }
+
+    @media #{$lte-tablet} {
+        height: 647px;
+        display: block;
+        padding-top: 25px;
+        padding-bottom: 54px;
+
+        .container {
+            display: flex;
+            flex-direction: column;
+            flex-wrap: nowrap;
+            justify-content: space-between;
+            align-content: flex-end;
+            align-items: flex-end;
+
+            padding-left: 37px;
+
+            .social-links {
+                display: flex;
+                flex-direction: row;
+                flex-wrap: nowrap;
+                justify-content: flex-end;
+                align-content: flex-end;
+                align-items: flex-end;
+
+
+                font-size: 12px;
+                line-height: 2em;
+
+                margin-block-start: unset;
+                margin-block-end: unset;
+                margin-inline-start: unset;
+                margin-inline-end: unset;
+                margin:0;
+                padding: 0;
+                text-indent: 0;
+
+            }
+            .press-room {
+                display: block;
+                margin-right: 0px;
+                margin-left: 40px;
+                margin-top: 50px;
+            }
+        }
+    }
+}
+</style>
---
+++ stories/FooterPrimary.stories.js
@@ -0,0 +1,28 @@
+// Import mock api data
+import * as API from "~/stories/mock-api.json"
+
+// Storybook default settings
+export default {
+  title: "FOOTER / FooterPrimary",
+}
+
+// Variations of stories below
+export const Default = () => ({
+  data() {
+      return {
+          items: [...API.links,...API.links],
+      }
+  },
+  computed: {
+    parsedItems(){
+      // Restructuring item to support text key
+      return this.items.map(obj => {
+        return {
+          ...obj,
+          text: obj.name
+        }
+      })
+    }
+  },
+  template: `<footer-primary :items="parsedItems"/>`,
+})
