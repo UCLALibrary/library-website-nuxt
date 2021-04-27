f4fe9369af08b73c0faec38995905f6fea00d8cf
darrowcoucla <darrowco@library.ucla.edu>
2021-04-27 16:49:08 -0700

    merge fix

--- components/BlockPostSmall.vue
+++ components/BlockPostSmall.vue
@@ -1,42 +1,40 @@
 <template lang="html">
-    <nuxt-link
-        :to="to"
-        :class="classes"
-    >
-        <responsive-image
-            :image="image"
-            :aspect-ratio="100"
-            class="image"
-        />
-        <div class="meta">
-            <div
-                class="category"
-                v-html="categoryName"
+    <nuxt-link :to="to">
+        <div :class="classes">
+            <responsive-image
+                :image="image"
+                class="block-post-image"
             />
-            <h3
-                class="title"
-                v-html="title"
-            />
-            <div
-                class="author"
-                v-html="parsedAuthor"
-            />
+            <div class="block-post-text">
+                <div class="block-post-category">
+                    {{ category }}
+                </div>
+                <div class="block-post-title">
+                    {{ title }}
+                </div>
+                <div class="block-post-author">
+                    By {{ author }}
+                </div>
+            </div>
         </div>
     </nuxt-link>
 </template>
 
 <script>
+import ResponsiveImage from "~/components/ResponsiveImage"
 import getSectionName from "~/utils/getSectionName"
 
 export default {
     props: {
         image: {
+            // Mock: api.page
             type: Object,
             default: () => {},
         },
-        categoryName: {
-            type: String,
-            default: "",
+        category: {
+            // Mock: {name, to}
+            type: Object,
+            default: () => {},
         },
         title: {
             type: String,
@@ -58,9 +56,6 @@ export default {
         sectionName() {
             return getSectionName(this.to)
         },
-        parsedAuthor() {
-            return `By ${this.author}`
-        },
     },
 }
 </script>
@@ -68,70 +63,61 @@ export default {
 <style lang="scss" scoped>
 .block-post-small {
     display: flex;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    justify-content: space-between;
-    align-content: space-between;
     align-items: center;
+    position: absolute;
 
-    width: 320px;
-    min-height: 150px;
-    padding: 25px;
-    box-sizing: border-box;
-    border-radius: 4px;
+    width: 390px;
+    height: 175px;
+}
 
-    transition-property: background-color, box-shadow;
-    transition-duration: 400ms;
-    transition-timing-function: ease-in-out;
+.color-visit {
+    &:hover {
+        background-color: var(--color-fushia-03);
+    }
+}
+.color-help {
+    &:hover {
+        background-color: var(--color-green-03);
+    }
+}
+.color-about {
+    &:hover {
+        background-color: var(--color-purple-03);
+    }
+}
 
-    .image {
-        width: 100px;
-    }
-    .meta {
-        width: 150px;
-    }
-    .category {
-        font-weight: 500;
-        font-size: 14px;
-        line-height: 100%;
-        letter-spacing: 0.06em;
-        text-transform: uppercase;
-        color: var(--color-dark-blue);
-    }
-    .title {
-        font-weight: 500;
-        font-size: 16px;
-        line-height: 120%;
-        letter-spacing: 0.01em;
-        color: var(--color-primary-blue);
-        margin: 15px 0 0 0;
-    }
-    .author {
-        font-weight: 300;
-        font-size: 14px;
-        line-height: 100%;
-        color: #434343;
-        margin-top: 10px;
-    }
+.block-post-text {
+    display: flex;
+    width: 180px;
+    flex-direction: column;
+}
 
-    // Themes
-    --color-theme: var(--color-primary-light-blue);
-    &.color-visit {
-        --color-theme: var(--color-fushia-03);
-    }
-    &.color-help {
-        --color-theme: var(--color-green-03);
-    }
-    &.color-about {
-        --color-theme: var(--color-purple-03);
-    }
+.block-post-category {
+    font-weight: bold;
+    font-size: 14.6705px;
+    line-height: 100%;
+    /* identical to box height, or 15px */
+    letter-spacing: 0.06em;
+    text-transform: uppercase;
+    color: var(--color-dark-blue);
+}
+.block-post-title {
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 120%;
+    letter-spacing: 0.01em;
+    color: var(--color-primary-blue);
+}
 
-    // Hovers
-    @media #{$has-hover} {
-        &:hover {
-            background-color: var(--color-theme);
-            box-shadow: 0px 10px 17px rgba(0, 0, 0, 0.04);
-        }
-    }
+.block-post-author {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 100%;
+    color: #434343;
+}
+
+.block-post-image {
+    height: 100px;
+    width: 100px;
 }
 </style>
---
+++ components/SectionPostSmall.vue
@@ -0,0 +1,80 @@
+<template lang="html">
+    <nav class="section-post-small">
+        <ul class="list">
+            <li
+                v-for="item in items"
+                class="list-item"
+            >
+                <div class="list">
+                    <div>
+                        <block-post-small class="button" />
+                    </div>
+                </div>
+            </li>
+        </ul>
+        <div class="button">
+            <button-more class="button" />
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    props: {
+        items: {
+            type: Array,
+            default: () => [],
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.section-post-small {
+    padding: 0 94px;
+
+    .list {
+        list-style-type: none;
+        display: flex;
+        flex-direction: row;
+
+        justify-content: space-between;
+        align-content: flex-start;
+        align-items: flex-start;
+        background-color: turquoise;
+    }
+
+    .list-item {
+        height: 140px;
+        width: 320px;
+        padding: 25px;
+        order: 0;
+        flex: 0 1 auto;
+        align-self: center;
+
+        &:first-child {
+            margin-left: 0;
+        }
+    }
+
+    .link {
+        color: var(--color-black);
+        text-decoration: none;
+    }
+
+    .button {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 40%;
+    }
+
+    // Hover states
+    @media #{$has-hover} {
+        .link:hover,
+        .link:active {
+            color: var(--color-primary-blue);
+        }
+    }
+}
+</style>
