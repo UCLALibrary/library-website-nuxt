c873bf8100e6c34daf1649d1413b5ba4b49056cf
pghorpade <pghorpade@library.ucla.edu>
2021-04-27 10:22:54 -0700

    Add MastheadSecondary component

--- components/MastheadSecondary.vue
+++ components/MastheadSecondary.vue
@@ -1,21 +1,19 @@
 <template lang="html">
-    <div class="masthead-secondary">
+    <div :class="classes">
         <div class="background" />
 
         <div class="container">
             <div class="overlay">
-                <h1
+                <h2
                     class="title"
                     v-html="title"
                 />
-                <p class="text">
-                    {{ this.text }}
-                </p>
+                <div
+                    class="text"
+                    v-html="text"
+                />
             </div>
-            <search-home
-                :class="hasSlot ? 'has-slot' : ''"
-                :has-tabs="false"
-            />
+            <slot> <!-- TODO Search goes here --> </slot>
         </div>
     </div>
 </template>
@@ -35,9 +33,16 @@ export default {
             type: String,
             default: "",
         },
-        hasSlot: {
-            type: Boolean,
-            default: true,
+    },
+    computed: {
+        classes() {
+            return [
+                "masthead-secondary",
+                {
+                    "has-molecule": this.hasMolecules,
+                    "has-slot": this.$slots.default,
+                },
+            ]
         },
     },
 }
@@ -47,7 +52,13 @@ export default {
 .masthead-secondary {
     position: relative;
     z-index: 0;
-
+    &.has-molecule {
+        .background {
+            background: url(~/assets/svg/molecule-background.svg?url) center -100px,
+                var(--gradient-03);
+            background-size: cover;
+        }
+    }
     .background {
         position: absolute;
         z-index: 10;
@@ -56,10 +67,7 @@ export default {
         width: 100%;
         height: 320px;
         overflow: hidden;
-
-        background: url(~/assets/svg/molecule-background.svg?url) center -100px,
-            var(--gradient-03);
-        background-size: cover;
+        background: var(--gradient-03);
     }
     .container {
         max-width: 990px;
@@ -80,7 +88,7 @@ export default {
         flex: 1 1 auto;
         height: 264px;
         color: var(--color-white);
-        margin: 0 50px;
+        margin: 0 auto;
 
         display: flex;
         align-items: flex-start;
@@ -111,9 +119,7 @@ export default {
             height: 185px;
             justify-content: center;
             align-content: center;
-            align-items: center;
-            padding: 0 10px;
-            max-width: 325px;
+            padding: 0 var(--unit-gutter);
 
             .title {
                 font-size: 40px;
