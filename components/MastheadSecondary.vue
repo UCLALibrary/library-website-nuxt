375c0090c9c21c5ed960beca44e5a0c53356a5e8
drewbaker <drewrb@gmail.com>
2021-04-28 02:42:41 +0200

    Fixed styles and cleaned up stories

--- components/MastheadSecondary.vue
+++ components/MastheadSecondary.vue
@@ -3,7 +3,7 @@
         <div class="background" />
 
         <div class="container">
-            <div class="overlay">
+            <div class="meta">
                 <h2
                     class="title"
                     v-html="title"
@@ -13,7 +13,9 @@
                     v-html="text"
                 />
             </div>
-            <slot> <!-- TODO Search goes here --> </slot>
+            <slot>
+                <!-- TODO Search goes here -->
+            </slot>
         </div>
     </div>
 </template>
@@ -40,6 +42,8 @@ export default {
                 "masthead-secondary",
                 {
                     "has-molecule": this.hasMolecules,
+                },
+                {
                     "has-slot": this.$slots.default,
                 },
             ]
@@ -50,45 +54,34 @@ export default {
 
 <style lang="scss" scoped>
 .masthead-secondary {
+    --unit-height: 320px;
+
     position: relative;
     z-index: 0;
-    &.has-molecule {
-        .background {
-            background: url(~/assets/svg/molecule-background.svg?url) center -100px,
-                var(--gradient-03);
-            background-size: cover;
-        }
-    }
+
     .background {
         position: absolute;
         z-index: 10;
         top: 0;
         left: 0;
         width: 100%;
-        height: 320px;
-        overflow: hidden;
+        height: var(--unit-height);
         background: var(--gradient-03);
     }
     .container {
         max-width: 990px;
-        height: 100%;
+        height: var(--unit-height);
 
         position: relative;
         z-index: 20;
         margin: 0 auto;
-
-        display: flex;
-        flex-direction: column;
-        flex-wrap: nowrap;
-        justify-content: center;
-        align-content: center;
-        align-items: stretch;
     }
-    .overlay {
-        flex: 1 1 auto;
-        height: 264px;
+    .meta {
+        min-height: 264px;
+        height: 100%;
         color: var(--color-white);
         margin: 0 auto;
+        padding: 0 var(--unit-gutter);
 
         display: flex;
         align-items: flex-start;
@@ -100,32 +93,44 @@ export default {
             line-height: 100%;
             letter-spacing: 0.01em;
             font-weight: 400;
-            margin-block-start: unset;
-            margin-block-end: unset;
+            margin: 0 0 0 -5px; // Fixing buggy font that has extra space on left
         }
         .text {
             font-size: 24px;
             line-height: 140%;
             letter-spacing: 0.02em;
+            margin-top: 15px;
+        }
+    }
+
+    // Variants
+    &.has-molecule {
+        .background {
+            background: url(~/assets/svg/molecule-background.svg?url) center -100px,
+                var(--gradient-03);
+            background-size: cover;
+        }
+    }
+    &.has-slot {
+        .container {
+            height: auto;
         }
     }
 
     // Breakpoints
     @media #{$lte-phone} {
-        .background {
-            height: 230px;
-        }
-        .overlay {
-            height: 185px;
-            justify-content: center;
-            align-content: center;
-            padding: 0 var(--unit-gutter);
+        --unit-height: 230px;
+
+        .meta {
+            min-height: 185px;
 
             .title {
                 font-size: 40px;
+                margin-left: -2px;
             }
             .text {
                 font-size: 14px;
+                max-width: 400px;
             }
         }
     }
