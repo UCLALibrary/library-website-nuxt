03dad4d3e23c1fe1bab5cefae989c13a5a55e3a0
drewbaker <drewrb@gmail.com>
2021-04-30 01:29:25 +0200

    Added edge case story

--- stories/NavPrimary.stories.js
+++ stories/NavPrimary.stories.js
@@ -6,26 +6,45 @@ export default {
     title: "NAV / Primary",
 }
 
+const items = [
+    { ...API.primaryNavlinks[0] },
+    {
+        ...API.primaryNavlinks[0],
+        name: "Visit",
+        url: "/visit/",
+    },
+    {
+        ...API.primaryNavlinks[0],
+        name: "About",
+        url: "/about/",
+    },
+    { ...API.primaryNavlinks[1] },
+]
+
 // TODO Write seperate story for NavMenuItem
 
 // Variations of stories below
 export const Default = () => ({
     data() {
         return {
-            items: [
-                { ...API.primaryNavlinks[0] },
-                {
-                    ...API.primaryNavlinks[0],
-                    name: "Visit",
-                    url: "/visit/",
-                },
-                {
-                    ...API.primaryNavlinks[0],
-                    name: "About",
-                    url: "/about/",
-                },
-                { ...API.primaryNavlinks[1] },
-            ],
+            items,
+        }
+    },
+    template: `
+        <nav-primary
+            :items="items"
+            current-path="/about/foo/bar"
+        />
+    `,
+})
+
+export const ExtraSupportLinks = () => ({
+    data() {
+        const newItems = items
+        newItems.push({ ...API.primaryNavlinks[1] })
+
+        return {
+            items: newItems,
         }
     },
     template: `
