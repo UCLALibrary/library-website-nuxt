679d7d7faf80ac39ff0158c6c5eb28bc724f9ad4
drewbaker <drewrb@gmail.com>
2021-04-30 01:21:25 +0200

    Built NavPrimary and styled it
    - TODO NavMenuItem story
    - TODO Click outside NavPrimary to close the menu

--- components/NavPrimary.vue
+++ components/NavPrimary.vue
@@ -1,56 +1,59 @@
 <template lang="html">
-    <nav class="nav-primary">
-        <nuxt-link to="/">
-            <svg-logo-ucla-library-blue
-                class="svg ucla-library-logo"
-                alt="UCLA Library logo blue"
-            />
-        </nuxt-link>
+    <nav :class="classes">
+        <div class="item-top">
+            <nuxt-link to="/">
+                <svg-logo-ucla-library
+                    class="svg logo-ucla"
+                    alt="UCLA Library logo blue"
+                />
+            </nuxt-link>
+        </div>
 
         <ul class="menu">
-            <li
-                v-for="item in items"
-                class="menu-item"
+            <nav-menu-item
+                v-for="(item, index) in parsedItems"
+                v-if="item.items.length"
+                :key="item.url"
+                :item="item"
+                :is-active="item.isActive"
+                :is-opened="isOpened"
+                @click.native="toggleMenu"
+                @mouseover.native="setActive(index)"
+                @mouseleave.native="clearActive"
+            />
+        </ul>
+
+        <div class="support-links">
+            <div
+                v-for="item in supportLinks"
+                class="item-top"
             >
-                <span
-                    v-if="item.url == ''"
+                <smart-link
+                    class="support-link underline-hover"
+                    :to="item.url"
+                    target="_blank"
                     v-html="item.name"
                 />
+            </div>
+        </div>
 
-                <ul
-                    v-if="item.url == ''"
-                    class="submenu"
-                >
-                    <li
-                        v-for="link in item.items"
-                        class="submenu-item"
-                    >
-                        <smart-link
-                            class="link underline-hover"
-                            :to="link.to"
-                            :target="link.target"
-                            v-html="link.text"
-                        />
-                    </li>
-                </ul>
-            </li>
-        </ul>
-        <smart-link
-            v-for="item in items"
-            v-if="item.url != ''"
-            class="link underline-hover"
-            :to="item.url"
-            target="_blank"
-            v-html="item.name"
+        <div class="background-white" />
+        <div
+            class="background-blue"
+            @click="toggleMenu"
         />
     </nav>
 </template>
 
 <script>
-import SvgLogoUclaLibraryBlue from "~/assets/svg/logo-ucla-library-blue"
+import SvgLogoUclaLibrary from "~/assets/svg/logo-ucla-library"
+
+// TODO Handle "click outside" event to close menu.
+// SEE https://stackoverflow.com/questions/53013471/vuejs-2-custom-directive-to-close-when-clicked-outside-not-working
+
 export default {
     components: {
-        SvgLogoUclaLibraryBlue,
+        SvgLogoUclaLibrary,
     },
     props: {
         items: {
@@ -58,65 +61,168 @@ export default {
             type: Array,
             default: () => [],
         },
+        currentPath: {
+            type: String,
+            default: "",
+        },
+    },
+    data() {
+        return {
+            isOpened: false,
+            activeMenuIndex: -1,
+        }
+    },
+    computed: {
+        classes() {
+            return ["nav-primary", { "is-opened": this.isOpened }]
+        },
+        supportLinks() {
+            // Generally this is just the last "Support Us" link, but we are going to allwo it to be more than 1
+            return this.items.filter((obj) => {
+                // Return items that don't have sub-menu children
+                return !obj.items.length
+            })
+        },
+        currentPathActiveIndex() {
+            // Find the index for the active menu item based on current route URL
+            const currentPath = this.currentPath || this.$route.path
+            return this.items.findIndex((obj, index) => {
+                return currentPath.includes(obj.url)
+            })
+        },
+        parsedItems() {
+            // Add an isActive property to all menu items
+            return this.items.map((obj, index) => {
+                return {
+                    ...obj,
+                    isActive: index == this.activeMenuIndex,
+                }
+            })
+        },
+    },
+    mounted() {
+        this.activeMenuIndex = this.currentPathActiveIndex
+    },
+    methods: {
+        toggleMenu() {
+            this.isOpened = !this.isOpened
+        },
+        setActive(index) {
+            // On hover, set current active menu item
+            this.activeMenuIndex = index
+        },
+        clearActive() {
+            // Reset active item back to the one from the route URL
+            this.activeMenuIndex = this.currentPathActiveIndex
+        },
     },
 }
 </script>
 
 <style lang="scss" scoped>
 .nav-primary {
+    --unit-height: 80px;
     padding: 0 var(--unit-gutter);
     position: relative;
-    display: flex;
-    flex-wrap: nowrap;
-    justify-content: center;
-    align-content: center;
-    align-items: center;
 
-    .menu {
-        margin: 0;
-        padding: 0;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    align-content: space-between;
+    align-items: flex-start;
+
+    .item-top {
+        height: var(--unit-height);
+        position: relative;
+        z-index: 10;
+        font-size: 18px;
+        font-weight: 600;
+        text-transform: uppercase;
+        letter-spacing: 0.1em;
 
         display: flex;
-        flex-direction: row;
+        flex-direction: column;
         flex-wrap: nowrap;
         justify-content: center;
         align-content: center;
         align-items: center;
 
-        .logo-ucla-library {
-            height: 67px;
-            width: auto;
+        .nuxt-link-active {
+            color: var(--color-primary-blue);
+        }
+    }
+    .logo-ucla {
+        height: 23px;
+        width: auto;
+    }
+    .menu {
+        margin: 0;
+        padding: 0;
+        list-style-type: none;
+        position: relative;
+        z-index: 10;
+    }
+    .support-links {
+        position: relative;
+        z-index: 10;
+
+        &::before {
+            content: "";
+            position: absolute;
+            left: 0;
+            top: 18px;
+            bottom: 18px;
+            width: 1px;
+            background-color: var(--color-grey-03);
         }
 
-        .menu-item {
-            list-style-type: none;
-            padding-right: 10px;
+        .item-top {
+            display: inline-flex;
+            margin-left: 30px;
         }
     }
-    @media #{$has-hover} {
-        .menu:hover .submenu {
-            background-color: var(--color-primary-blue);
-            display: block;
-        }
-        .menu-item:hover {
-            text-decoration: underline;
-            text-decoration-color: yellow;
-        }
-    }
-    .submenu {
-        flex: 1 1 auto;
-        display: none;
+
+    .background-white {
         position: absolute;
-        top: 50px;
-        max-width: 350px;
-        height: 100px;
-        color: var(--color-white);
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 80px;
+        background-color: var(--color-white);
+        border-bottom: 1px solid var(--color-grey-03);
+        z-index: 0;
+    }
+    .background-blue {
+        background-color: var(--color-primary-blue);
+        position: absolute;
+        top: calc(var(--unit-height) + 1px);
+        bottom: 0;
+        width: 100%;
+        left: 0;
+        opacity: 0;
+        transition: opacity 1000ms ease-in-out;
+        border-bottom: 1px solid var(--color-grey-03);
+        z-index: 0;
+    }
 
-        .submenu-item {
-            list-style-type: none;
-            margin: 10px 0;
-            padding: 0 10px;
+    // States
+    &.is-opened {
+        .background-blue {
+            opacity: 0.9;
         }
     }
+
+    // Hovers
+    // @media #{$has-hover} {
+    //     .menu:hover .submenu {
+    //         background-color: var(--color-primary-blue);
+    //         display: block;
+    //     }
+    //     .menu-item:hover {
+    //         text-decoration: underline;
+    //         text-decoration-color: yellow;
+    //     }
+    // }
 }
 </style>
--- stories/NavPrimary.stories.js
+++ stories/NavPrimary.stories.js
@@ -6,12 +6,32 @@ export default {
     title: "NAV / Primary",
 }
 
+// TODO Write seperate story for NavMenuItem
+
 // Variations of stories below
 export const Default = () => ({
-  data() {
-      return {
-          items: [ { ...API.primaryNavlinks[0] }, { ...API.primaryNavlinks[0] }, ...API.primaryNavlinks ],
-      }
-  },
-  template: `<nav-primary :items="items"/>`,
+    data() {
+        return {
+            items: [
+                { ...API.primaryNavlinks[0] },
+                {
+                    ...API.primaryNavlinks[0],
+                    name: "Visit",
+                    url: "/visit/",
+                },
+                {
+                    ...API.primaryNavlinks[0],
+                    name: "About",
+                    url: "/about/",
+                },
+                { ...API.primaryNavlinks[1] },
+            ],
+        }
+    },
+    template: `
+        <nav-primary
+            :items="items"
+            current-path="/about/foo/bar"
+        />
+    `,
 })
