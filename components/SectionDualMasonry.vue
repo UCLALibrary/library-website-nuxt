c267e98b32ce3d1bd7a06b5fedc07e42e9e9e241
jendiamond <thejendiamond@gmail.com>
2021-04-30 16:44:18 -0700

    WIP

--- components/SectionDualMasonry.vue
+++ components/SectionDualMasonry.vue
@@ -2,7 +2,45 @@
     <section class="section-dual-masonry">
         <div class="background" />
 
-        <div class="masonry">
+        <div
+            ref="masonry"
+            class="masonry"
+        >
+            <div class="meta masonry-item">
+                <div class="masonry-content">
+                    <p class="category">
+                        DATA {{ items[0].category }}
+                    </p>
+                    <h2 class="title">
+                        {{ items[0].title }}
+                    </h2>
+                    <h3 class="dates">
+                        {{ items[0].dates }}
+                    </h3>
+                    <button class="button">
+                        Visit Exhibit 1
+                    </button>
+                </div>
+            </div>
+
+            <div class="meta masonry-item">
+                <!-- <nuxt-link>
+                    <responsive-image image="items[0].image" />
+                </nuxt-link> -->
+                <p class="category">
+                    DATA {{ items[0].category }}
+                </p>
+                <h2 class="title">
+                    {{ items[0].title }}
+                </h2>
+                <h3 class="dates">
+                    {{ items[0].dates }}
+                </h3>
+                <button class="button">
+                    Visit Exhibit 2
+                </button>
+            </div>
+
             <div class="meta masonry-item">
                 <p class="category">
                     DATA {{ items[0].category }}
@@ -14,7 +52,7 @@
                     {{ items[0].dates }}
                 </h3>
                 <button class="button">
-                    Visit Exhibit
+                    Visit Exhibit 3
                 </button>
             </div>
 
@@ -32,40 +70,7 @@
                     {{ items[0].dates }}
                 </h3>
                 <button class="button">
-                    Visit Exhibit
-                </button>
-            </div>
-
-            <div class="meta masonry-item">
-                <p class="category">
-                    DATA {{ items[0].category }}
-                </p>
-                <h2 class="title">
-                    {{ items[0].title }}
-                </h2>
-                <h3 class="dates">
-                    {{ items[0].dates }}
-                </h3>
-                <button class="button">
-                    Visit Exhibit
-                </button>
-            </div>
-
-            <div class="meta masonry-item">
-                <!-- <nuxt-link>
-                    <responsive-image image="items[0].image" />
-                </nuxt-link> -->
-                <p class="category">
-                    DATA {{ items[0].category }}
-                </p>
-                <h2 class="title">
-                    {{ items[0].title }}
-                </h2>
-                <h3 class="dates">
-                    {{ items[0].dates }}
-                </h3>
-                <button class="button">
-                    Visit Exhibit
+                    Visit Exhibit 4
                 </button>
             </div>
         </div>
@@ -77,6 +82,8 @@
 </template>
 
 <script>
+import resizeMasonryItem from "~/utils/masonrySizing"
+
 export default {
     props: {
         items: {
@@ -89,13 +96,40 @@ export default {
             default: "",
         },
     },
+    mounted() {
+        // console.log(this.$refs)
+        const allItems = this.$refs.masonry.getElementsByClassName(
+            "masonry-item"
+        )
+        // console.log(resizeAllMasonryItems)
+        // resizeAllMasonryItems(allItems)
+        resizeMasonryItem(allItems[0])
+        resizeMasonryItem(allItems[1])
+        resizeMasonryItem(allItems[2])
+        resizeMasonryItem(allItems[4])
+    },
 }
 </script>
 
 <style lang="scss" scoped>
 .section-dual-masonry {
+    .masonry {
+        display: grid;
+        grid-gap: 1em;
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+        /* Adjust columns according to the available viewport */
+        grid-auto-rows: 0;
+    }
     .masonry-item {
     }
+    // https://w3bits.com/css-grid-masonry/
+    // R - row
+    // G - gap
+    // H- height
+    // H1 - H + G
+    // T = G + R
+    // S - span
+    // S = H1 / T
     .meta {
         color: var(--color-white);
         font-family: Karbon;
@@ -111,7 +145,7 @@ export default {
             line-height: 44px;
             letter-spacing: 0.01em;
         }
-        masonsonry .category {
+        .category {
             color: var(--color-yellow-03);
             letter-spacing: 0.06em;
             text-transform: uppercase;
