f0dc422bae440d5b170448224f525e333a7b7bdb
Ashton Prigge <ashton.prigge@gmail.com>
2021-05-06 13:25:58 -0700

    Update css and svg names.

--- components/BlockCardVertical.vue
+++ components/BlockCardVertical.vue
@@ -1,163 +1,153 @@
 <template lang="html">
-    <nuxt-link
-        :to="to"
-        :class="classes"
-    >
-        <component
-            :is="parsedSvgName"
-            class="svg"
-        />
-        <div class="meta">
-            <h3
-                class="title"
-                v-html="title"
-            />
-            <div
-                class="text"
-                v-html="text"
-            />
-        </div>
-    </nuxt-link>
+  <nuxt-link :to="to" :class="classes">
+    <component :is="parsedSvgName" class="svg" />
+    <div class="meta">
+      <h3 class="title" v-html="title" />
+      <div class="text" v-html="text" />
+    </div>
+  </nuxt-link>
 </template>
 
 <script>
-import getSectionName from "~/utils/getSectionName"
+import getSectionName from "~/utils/getSectionName";
 
 export default {
-    components: {
-        BookBindingIcon: () =>
-            import("~/assets/svg/illustrations/book-binding-icon"),
-        BorrowingBooksIcon: () =>
-            import("~/assets/svg/illustrations/borrowing-books-icon"),
-        DatabasesIcon: () =>
-            import("~/assets/svg/illustrations/databases-icon"),
-        DigitizedResourcesIcon: () =>
-            import("~/assets/svg/illustrations/digitized-resources-icon"),
-        FindSpaceIcon: () =>
-            import("~/assets/svg/illustrations/find-space-icon"),
-        RemoteAccessIcon: () =>
-            import("~/assets/svg/illustrations/remote-access-icon"),
-        ResearchIcon: () => import("~/assets/svg/illustrations/research-icon"),
-        TeachingIcon: () => import("~/assets/svg/illustrations/teaching-icon"),
+  components: {
+    IllustrationBookBinding: () =>
+      import("~/assets/svg/illustration-book-binding"),
+    IllustrationBorrowingBooks: () =>
+      import("~/assets/svg/illustration-borrowing-books"),
+    IllustrationDatabases: () => import("~/assets/svg/illustration-databases"),
+    IllustrationDigitizedResources: () =>
+      import("~/assets/svg/illustration-digitized-resources"),
+    IllustrationFindSpace: () => import("~/assets/svg/illustration-find-space"),
+    IllustrationRemoteAccess: () =>
+      import("~/assets/svg/illustration-remote-access"),
+    IllustrationResearch: () => import("~/assets/svg/illustration-research"),
+    IllustrationTeaching: () => import("~/assets/svg/illustration-teaching"),
+  },
+  props: {
+    iconName: {
+      type: String,
+      default: "illustration-remote-access",
     },
-    props: {
-        iconName: {
-            type: String,
-            default: "",
-        },
-        title: {
-            type: String,
-            default: "",
-        },
-        text: {
-            type: String,
-            default: "",
-        },
-        to: {
-            type: String,
-            default: "",
-        },
+    title: {
+      type: String,
+      default: "",
     },
-    computed: {
-        classes() {
-            return ["block-card-vertical", `color-${this.sectionName}`]
-        },
-        sectionName() {
-            return this.$slots.default ? "default" : getSectionName(this.to)
-        },
-        parsedSvgName() {
-            return `${this.iconName}`
-        },
+    text: {
+      type: String,
+      default: "",
     },
-}
+    to: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    classes() {
+      return ["block-card-vertical", `color-${this.sectionName}`];
+    },
+    sectionName() {
+      return this.$slots.default ? "default" : getSectionName(this.to);
+    },
+    parsedSvgName() {
+      return `${this.iconName}`;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .block-card-vertical {
-    display: flex;
-    flex-direction: column;
-    flex-wrap: nowrap;
-    justify-content: center;
-    align-content: center;
-    align-items: center;
-    border: 2px solid var(--color-lightest-blue);
-    width: 300px;
-    height: 400px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  border: 2px solid var(--color-lightest-blue);
+  width: 300px;
+  height: 400px;
+  overflow: hidden;
+  transition: transform 400ms ease-in-out;
+  transition-property: box-shadow;
+  border-radius: 4px;
+
+  // Themes
+  --color-theme: var(--color-primary-light-blue);
+  &.color-visit {
+    --color-theme: var(--color-fushia-03);
+  }
+  &.color-help {
+    --color-theme: var(--color-green-03);
+  }
+  &.color-about {
+    --color-theme: var(--color-purple-03);
+  }
+
+  .svg {
+    flex-grow: 0;
+    flex-shrink: 0;
+    padding: 20px 0;
+  }
+
+  .meta {
+    transition-property: background-color;
+    transition-duration: 400ms;
+    transition-timing-function: ease-in-out;
+    background-color: var(--color-lightest-blue);
+    flex: 1 1 auto;
+  }
+  .title {
+    font-family: var(--font-primary);
+    font-size: 24px;
+    line-height: 110%;
+    text-align: center;
+    letter-spacing: 0.01em;
+    font-weight: 500;
+    margin-top: 35px;
+    margin-bottom: 10px;
+    color: var(--color-dark-blue);
+    padding-right: 60px;
+    padding-left: 60px;
+
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
     overflow: hidden;
-    transition: transform 400ms ease-in-out;
+  }
+  .text {
+    font-family: var(--font-secondary);
+    font-size: 16px;
+    font-weight: normal;
+    line-height: 150%;
+    text-align: center;
+    padding-left: 40px;
+    padding-right: 40px;
+    color: var(--color-dark-blue);
 
-    // Themes
-    --color-theme: var(--color-primary-light-blue);
-    &.color-visit {
-        --color-theme: var(--color-fushia-03);
-    }
-    &.color-help {
-        --color-theme: var(--color-green-03);
-    }
-    &.color-about {
-        --color-theme: var(--color-purple-03);
-    }
+    display: -webkit-box;
+    -webkit-line-clamp: 4;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+  }
 
-    .svg {
-        flex-grow: 0;
-        flex-shrink: 0;
-        padding: 20px 0;
-    }
+  // Hovers
+  @media #{$has-hover} {
+    &:hover {
+      transform: scale(1.1);
 
-    .meta {
-        transition-property: background-color, box-shadow;
-        transition-duration: 400ms;
-        transition-timing-function: ease-in-out;
-        background-color: var(--color-lightest-blue);
-        flex: 1 1 auto;
+      .meta {
+        background-color: var(--color-theme);
+        box-shadow: 0px 10px 17px rgba(0, 0, 0, 0.04);
+      }
+      .title {
+        text-decoration: underline;
+        text-decoration-color: var(--color-cyan-01);
+        text-decoration-thickness: 1.5px;
+      }
     }
-    .title {
-        font-family: var(--font-primary);
-        font-size: 24px;
-        line-height: 110%;
-        text-align: center;
-        letter-spacing: 0.01em;
-        font-weight: 500;
-        margin-top: 35px;
-        margin-bottom: 10px;
-        color: var(--color-dark-blue);
-
-        display: -webkit-box;
-        -webkit-line-clamp: 2;
-        -webkit-box-orient: vertical;
-        overflow: hidden;
-    }
-    .text {
-        font-family: var(--font-secondary);
-        font-size: 16px;
-        font-weight: normal;
-        line-height: 150%;
-        text-align: center;
-        padding-left: 40px;
-        padding-right: 40px;
-        color: var(--color-dark-blue);
-
-        display: -webkit-box;
-        -webkit-line-clamp: 4;
-        -webkit-box-orient: vertical;
-        overflow: hidden;
-    }
-
-    // Hovers
-    @media #{$has-hover} {
-        &:hover {
-            transform: scale(1.1);
-
-            .meta {
-                background-color: var(--color-theme);
-                box-shadow: 0px 10px 17px rgba(0, 0, 0, 0.04);
-            }
-            .title {
-                text-decoration: underline;
-                text-decoration-color: var(--color-cyan-01);
-                text-decoration-thickness: 1.5px;
-            }
-        }
-    }
+  }
 }
 </style>
--- stories/BlockCardVertical.stories.js
+++ stories/BlockCardVertical.stories.js
@@ -1,116 +1,124 @@
 // Import mock api data
-import * as API from "~/stories/mock-api.json"
+import * as API from "~/stories/mock-api.json";
 
 // Storybook default settings
 export default {
-    title: "BLOCK / Card Vertical",
-}
+  title: "BLOCK / Card Vertical",
+};
 
 const mock = {
-    iconName: "book-binding-icon",
-    to: "/help/foo/bar/",
-    title: "Example Service",
-    text: "Here is a decent amount of text to explain this get help with.",
-}
+  to: "/help/foo/bar/",
+  title: "Example Service",
+  text: "Here is a decent amount of text to explain this get help with.",
+};
 
 export const Default = () => ({
-    data() {
-        return {
-            ...mock,
-        }
-    },
-    template: `
+  data() {
+    return {
+      ...mock,
+    };
+  },
+  template: `
         <block-card-vertical
-            :icon-name="icon-name"
             :to="to"
             :title="title"
             :text="text"
         />
     `,
-})
+});
 
 export const Visit = () => ({
-    data() {
-        return {
-            ...mock,
-        }
-    },
-    template: `
+  data() {
+    return {
+      ...mock,
+    };
+  },
+  template: `
         <block-card-vertical
-            icon-name="find-space-icon"
+            icon-name="illustration-find-space"
             to="/about/foo/bar"
             :title="title"
             :text="text"
         />
     `,
-})
+});
 
 export const LongText = () => ({
-    data() {
-        return {
-            ...mock,
-        }
-    },
-    template: `
+  data() {
+    return {
+      ...mock,
+    };
+  },
+  template: `
         <block-card-vertical
-        icon-name="digitized-resources-icon"
-        to="/help/foo/bar"
-        :title="title"
-        text="We doubt the critics, reject the status quo and see opportunity in dissatisfaction. Our campus, faculty and students are driven by optimism. It is not naïve; it is essential. And it has fueled every accomplishment, allowing us to redefine what's possible, time after time." />`,
-})
+            icon-name="illustration-digitized-resources"
+            to="/help/foo/bar"
+            :title="title"
+            text="We doubt the critics, reject the status quo and see opportunity in dissatisfaction. Our campus, faculty and students are driven by optimism. It is not naïve; it is essential. And it has fueled every accomplishment, allowing us to redefine what's possible, time after time."
+        />
+    `,
+});
 
 export const Databases = () => ({
-    data() {
-        return {
-            ...mock,
-        }
-    },
-    template: `
+  data() {
+    return {
+      ...mock,
+    };
+  },
+  template: `
         <block-card-vertical
-        icon-name="databases-icon"
-        to="/visit/foo/bar"
-        :title="title"
-        :text="text" />`,
-})
+            icon-name="illustration-databases"
+            to="/visit/foo/bar"
+            :title="title"
+            :text="text"
+        />
+    `,
+});
 
 export const RemoteAccess = () => ({
-    data() {
-        return {
-            ...mock,
-        }
-    },
-    template: `
+  data() {
+    return {
+      ...mock,
+    };
+  },
+  template: `
         <block-card-vertical
-        icon-name="remote-access-icon"
-        to="/visit/foo/bar"
-        :title="title"
-        :text="text" />`,
-})
+            icon-name=" illustration-remote-access"
+            to="/visit/foo/bar"
+            :title="title"
+            :text="text"
+        />
+    `,
+});
 
 export const Research = () => ({
-    data() {
-        return {
-            ...mock,
-        }
-    },
-    template: `
+  data() {
+    return {
+      ...mock,
+    };
+  },
+  template: `
         <block-card-vertical
-        icon-name="research-icon"
-        to="/visit/foo/bar"
-        :title="title"
-        :text="text" />`,
-})
+            icon-name="illustration-research"
+            to="/visit/foo/bar"
+            :title="title"
+            :text="text"
+        />
+    `,
+});
 
 export const Teaching = () => ({
-    data() {
-        return {
-            ...mock,
-        }
-    },
-    template: `
+  data() {
+    return {
+      ...mock,
+    };
+  },
+  template: `
         <block-card-vertical
-        icon-name="teaching-icon"
-        to="/visit/foo/bar"
-        :title="title"
-        :text="text" />`,
-})
+            icon-name="illustration-teaching"
+            to="/visit/foo/bar"
+            :title="title"
+            :text="text"
+        />
+    `,
+});
