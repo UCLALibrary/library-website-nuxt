c1fecc9afa17a5315fad1d8e165e2d3f36215626
darrowcoucla <darrowco@library.ucla.edu>
2021-05-05 20:40:10 -0700

    added styles

--- components/SiteNotificationAlert.vue
+++ components/SiteNotificationAlert.vue
@@ -76,4 +76,94 @@ export default {
 }
 </script>
 
+<style lang="scss" scoped>
+.site-notification-alert {
+    width: 410px;
+    .container {
+        display: flex;
+        align-items: flex-end;
+        flex-direction: column;
+        flex-wrap: nowrap;
+    }
+
+    .alert {
+        display: flex;
+        justify-content: center;
+
+        background-color: var(--color-yellow);
+        border-radius: 4px 4px 0 0;
+        height: 48px;
+        max-width: 196px;
+    }
+
+    .alert:hover {
+        cursor: pointer;
+    }
+
+    .alertBoxIcon {
+        align-self: center;
+
+        padding: 0 0 0 20px;
+    }
+
+    .alertText {
+        align-self: center;
+        flex-wrap: nowrap;
+
+        color: var(--color-black);
+        font-family: var(--font-primary);
+        font-size: 14px;
+        font-weight: 500;
+        letter-spacing: 0.01em;
+        line-height: 14px;
+        overflow: hidden;
+        padding: 0 20px 3px 0;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+
+    .message {
+        box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.04),
+            0px 6px 32px rgba(0, 0, 0, 0.08);
+        transition-property: background-color, box-shadow;
+        height: 190px;
+        width: 410px;
+        transition-duration: 400ms;
+        transition-timing-function: ease-in-out;
+    }
+
+    .messageIcon {
+        display: flex;
+        align-self: flex-start;
+        justify-content: flex-end;
+
+        padding: 24px 24px 0 0;
+    }
+
+    .messageIcon:hover {
+        cursor: pointer;
+    }
+
+    .messageText {
+        align-self: flex-end;
+
+        color: var(--color-black);
+        font-family: var(--font-primary);
+        font-size: 16px;
+        font-weight: 400;
+        letter-spacing: 0.01em;
+        line-height: 22px;
+        max-height: 128px;
+        overflow-y: auto;
+        padding: 10px 32px 0 32px;
+    }
+
+    // Breakpoints
+    @media #{$lte-phone} {
+        width: 300px;
+        .message {
+            width: 300px;
+        }
+    }
+}
 </style>
