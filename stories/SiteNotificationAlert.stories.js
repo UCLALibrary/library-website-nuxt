3666be4e2569694937978b86daaf8b8fc7e5d30b
darrowcoucla <darrowco@library.ucla.edu>
2021-05-05 20:41:17 -0700

    inserted default story

--- stories/SiteNotificationAlert.stories.js
+++ stories/SiteNotificationAlert.stories.js
@@ -1,15 +1,19 @@
 // Import mock api data
-import * as API from "~/stories/mock-api.json"
+import * as API from "~/stories/mock-api.json"4
 
 // Storybook default settings
 export default {
+    title: "Global / SiteNotificationAlert",
 }
 
 export const Default = () => ({
     data() {
         return {
+            title: API.alert.title,
+            text: API.alert.text
         }
     },
+    template: `<site-notification-alert :title="title" :text="text" />`,
 })
 
 // Variations of stories below
