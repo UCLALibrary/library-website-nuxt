47b41d2368247acb1ec7d6e3fe2a0826e7add127
darrowcoucla <darrowco@library.ucla.edu>
2021-05-05 20:35:30 -0700

    added html

--- components/SiteNotificationAlert.vue
+++ components/SiteNotificationAlert.vue
@@ -1,4 +1,37 @@
-<template lang="html" />
+<template lang="html">
+    <section class="site-notification-alert">
+        <div class="container">
+            <div
+                class="alert"
+                @click="toggleAlert()"
+            >
+                <div class="alertBoxIcon">
+                    <svg-alert-circle />
+                </div>
+                <div
+                    class="alertText"
+                    v-html="title"
+                />
+            </div>
+
+            <div
+                v-if="message"
+                class="message"
+            >
+                <div class="messageIcon">
+                    <svg-glyph-close
+                        class="svg"
+                        @click="toggleAlert()"
+                    />
+                </div>
+                <div
+                    class="messageText"
+                    v-html="text"
+                />
+            </div>
+        </div>
+    </section>
+</template>
 
 <script></script>
 
