d69778ba09418e7965c3ddb68e39ddae07818507
Casey Grzecka <cgrzecka@library.ucla.edu>
2021-05-21 18:22:22 -0700

    add links below search box and styles

--- stories/SearchHome.stories.js
+++ stories/SearchHome.stories.js
@@ -19,3 +19,28 @@ const Template = (args, { argTypes }) => ({
 })
 
 export const Default = Template.bind({})
+
+Default.args = {
+    linkItems: [
+        {
+            text: 'Course Reserves',
+            url: 'https://catalog.library.ucla.edu/vwebv/enterCourseReserve.do',
+            target: '_blank',
+        },
+        {
+            text: 'UCLA Research Guides',
+            url: 'https://guides.library.ucla.edu/',
+            target: ''
+        },
+        {
+            text: 'Databases A-Z',
+            url: 'https://guides.library.ucla.edu/az.php',
+            target: '_blank',
+        },
+    ],
+    advancedSearchLink: {
+        text: 'Advanced Search',
+        url: 'https://www.library.ucla.edu/search',
+        target: '_blank',
+    }
+}
