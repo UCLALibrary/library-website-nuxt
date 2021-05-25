cf5cac0b11a96367e12a1b61bc3b02831ff206aa
Ashton Prigge <34147754+aprigge@users.noreply.github.com>
2021-05-25 08:10:16 -0700

    Updated BlockCardVertical hover colors. (#64)

--- components/BlockCardVertical.vue
+++ components/BlockCardVertical.vue
@@ -97,13 +97,13 @@ export default {
     // Themes
     --color-theme: var(--color-primary-light-blue);
     &.color-visit {
-        --color-theme: var(--color-visit);
+        --color-theme: var(--color-fushia-03);
     }
     &.color-help {
-        --color-theme: var(--color-help);
+        --color-theme: var(--color-green-03);
     }
     &.color-about {
-        --color-theme: var(--color-about);
+        --color-theme: var(--color-purple-03);
     }
 
     .svg {
