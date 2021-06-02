e9deb2d68d65281e9f2928bada79ed5c9a5232a4
Ashton Prigge <ashton.prigge@gmail.com>
2021-06-02 12:51:42 -0700

    BlockTeaserHorizontal.

---
+++ components/BlockTeaserHorizontal.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+    <section class="block-teaser-horizontal">
+        <responsive-image
+            :image="item.image"
+            :aspect-ratio="60"
+            class="image"
+        />
+        <block-teaser-meta
+            v-bind="item"
+            view="gallery"
+            class="meta"
+        />
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        item: {
+            type: Object,
+            default: () => {},
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.block-teaser-horizontal {
+    // display: inline;
+    max-width: 930px;
+    max-height: 360px;
+    // background-color: var(--color-white);
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    .image {
+        width: 456px;
+        height: 274px;
+
+        margin-right: 56px;
+    }
+    .meta {
+        width: 412px;
+        max-height: 180px;
+    }
+}
+</style>
---
+++ stories/BlockTeaserHorizontal.stories.js
@@ -0,0 +1,61 @@
+// Import mock api data
+import * as API from "~/stories/mock-api.json"
+
+// Storybook default settings
+export default {
+    title: "BLOCK / Teaser / Horizontal",
+}
+
+const mock = {
+    image: API.image,
+    to: "/visit/foo/bar/",
+    category: "Ullamco",
+    title: "Fames ac turpis egestas sed tempus",
+    date: "Pempti, Ocavia 14, 2021",
+    time: "3 - 5 pm",
+}
+
+// Variations of stories below
+export const Default = () => ({
+    data() {
+        return { item: mock }
+    },
+    template: `
+      <block-teaser-horizontal
+        :item="item"
+      />
+  `,
+})
+
+export const ShortTitle = () => ({
+    data() {
+        return {
+            item: {
+                ...mock,
+                title: "Minim",
+            },
+        }
+    },
+    template: `
+      <block-teaser-horizontal
+        :item="item"
+      />
+  `,
+})
+
+export const LongTitle = () => ({
+    data() {
+        return {
+            item: {
+                ...mock,
+                title:
+                    "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            },
+        }
+    },
+    template: `
+      <block-teaser-horizontal
+        :item="item"
+      />
+  `,
+})
