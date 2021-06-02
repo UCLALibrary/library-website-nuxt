1ddfddaa6f6934a18ba20d75c6d311be17398fc4
jendiamond <thejendiamond@gmail.com>
2021-06-02 11:31:21 -0700

    Build BlockTeaserVertical component
    Changes to be committed:
    new file:   components/BlockTeaserVertical.vue
    new file:   stories/BlockTeaserVertical.stories.js
    modified:   components/BlockTeaserMeta.vue

---
+++ components/BlockTeaserVertical.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+    <section class="block-teaser-vertical">
+        <responsive-image
+            :image="item.image"
+            :aspect-ratio="60"
+        />
+        <block-teaser-meta
+            v-bind="item"
+            view="gallery"
+            class="meta"
+        />
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        item: {
+            type: Object,
+            default: () => {},
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.block-teaser-vertical {
+    display: block;
+    max-width: 300px;
+    background-color: var(--color-white);
+    max-height: 360px;
+
+    .meta {
+        margin-top: 16px;
+        max-height: 180px;
+    }
+}
+</style>
