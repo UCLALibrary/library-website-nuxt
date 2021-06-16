62e1c4598d550f57e3681d51f499cafdfc1b9c6f
pghorpade <pghorpade@library.ucla.edu>
2021-06-15 19:36:35 -0700

    Set the margin left and right to auto for Featured Image on the index Page

--- pages/index.vue
+++ pages/index.vue
@@ -82,7 +82,8 @@ export default {
         const mockCard = {
             to: "/help/foo/bar/",
             title: "Example Service",
-            text: "Here is a decent amount of text to explain this get help with.",
+            text:
+                "Here is a decent amount of text to explain this get help with.",
         }
         const sectionCardsData = {
             items: [
@@ -93,7 +94,8 @@ export default {
                 { ...mockCard, to: "/help/foo/fred/" },
             ],
             title: "Get Help with",
-            text: "Need guidance on how to make the most of UCLA Libraries? Below are common areas for which we offer services, resources, workshops and more.",
+            text:
+                "Need guidance on how to make the most of UCLA Libraries? Below are common areas for which we offer services, resources, workshops and more.",
             to: "/help/foo/bar",
         }
 
@@ -177,7 +179,7 @@ export default {
 <style lang="scss" scoped>
 .page-home {
     .banner {
-        margin: var(--unit-vertical-gap) 0;
+        margin: var(--unit-vertical-gap) auto;
     }
 }
 </style>
