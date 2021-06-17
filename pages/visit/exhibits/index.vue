35ee3977ce0623342ced570446c3320a247bc1bc
drewbaker <drew@funkhaus.us>
2021-06-17 10:13:03 -0700

    Added example of exhibits page routes

---
+++ pages/visit/exhibits/index.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+    <div class="page page-exhibits">
+        exhibits components go here
+
+        <section-list v-if="pageType" />
+        <section-calender v-if="isCalenderView" />
+    </div>
+</template>
+
+<script>
+export default {
+    async asyncData() {},
+    data() {
+        return {
+            pageType: "list",
+        }
+    },
+    computed: {
+        isListView() {
+            return this.pageType == "list"
+        },
+        isCalenderView() {
+            return this.pageType == "calender"
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.page-exhibits {
+}
+</style>
