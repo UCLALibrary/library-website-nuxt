1d349810e569cc96f5c19a6b0e2f159ac083acde
Ashton Prigge <ashton.prigge@gmail.com>
2021-06-21 14:34:32 -0700

    Rebase and refactor.

--- components/BlockCallToAction.vue
+++ components/BlockCallToAction.vue
@@ -1,32 +1,34 @@
 <template lang="html">
-    <footer class="block-call-to-action">
-        <div class="container">
-            <svg-call-to-action1 class="svg" />
-            <h3
-                class="title"
-                v-html="title"
-            />
-            <div
-                class="text"
-                v-html="text"
-            />
-            <button-link
-                class="button"
-                label="Ask a Librarian"
-            />
-        </div>
-    </footer>
+    <div class="block-call-to-action">
+        <svg-call-to-action class="svg" />
+        <div
+            class="title"
+            v-html="title"
+        />
+        <div
+            class="text"
+            v-html="text"
+        />
+        <button-link
+            :label="name"
+            :to="to"
+            class="button-link"
+        />
+    </div>
 </template>
 
 <script>
-import SvgCallToAction1 from "~/assets/svg/call-to-action1"
-import SvgCallToAction3 from "~/assets/svg/call-to-action3"
+import SvgCallToAction from "~/assets/svg/call-to-action"
+
 export default {
     components: {
-        SvgCallToAction1,
-        SvgCallToAction3,
+        SvgCallToAction,
     },
     props: {
+        iconName: {
+            type: String,
+            default: "",
+        },
         title: {
             type: String,
             default: "",
@@ -35,11 +37,18 @@ export default {
             type: String,
             default: "",
         },
+        name: {
+            type: String,
+            default: "",
+        },
+        to: {
+            type: String,
+            default: "",
+        },
     },
     computed: {
-        year() {
-            const current_year = new Date().getFullYear()
-            return current_year
+        parsedSvgName() {
+            return `${this.iconName}`
         },
     },
 }
@@ -47,115 +56,41 @@ export default {
 
 <style lang="scss" scoped>
 .block-call-to-action {
-    background-color: var(--color-white);
+    max-width: 1046px;
+    max-height: 467px;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: var(--color-primary-lightest-blue);
 
-    .container {
-        background-color: var(--color-lightest-blue);
-        max-width: var(--unit-content-width);
-        min-height: 468px;
-        margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-content: center;
+    align-items: center;
 
-        display: flex;
-        flex-direction: column;
-        flex-wrap: wrap;
-        align-content: center;
-        align-items: center;
-
-        .svg {
-            padding-top: 60px;
-        }
-        .title {
-            position: static;
-            max-width: 630px;
-            min-height: 40px;
-
-            font-family: Karbon;
-            font-style: normal;
-            font-weight: 500;
-            font-size: 40px;
-            line-height: 100%;
-            text-align: center;
-            letter-spacing: 0.01em;
-            padding-top: 20px;
-            color: var(--color-primary-blue);
-        }
-
-        .text {
-            position: static;
-            max-width: 638px;
-            min-height: 50px;
-            left: 0px;
-            top: 0px;
-
-            font-family: Karbon;
-            font-style: normal;
-            font-weight: 400;
-            font-size: 18px;
-            line-height: 140%;
-
-            text-align: center;
-            letter-spacing: 0.02em;
-            padding: 16px 0 46px 0;
-            color: var(--color-dark-blue);
-        }
-
-        .links {
-            display: flex;
-            flex-direction: row;
-            flex-wrap: nowrap;
-            justify-content: flex-start;
-            align-content: flex-start;
-            align-items: flex-start;
-
-            list-style-type: none;
-            margin: 15px 0;
-        }
-        .button {
-            width: 199px;
-            height: 50px;
-            background: #0b6ab7;
-            padding: 0;
-
-            font-family: Proxima Nova;
-            font-style: normal;
-            font-weight: normal;
-            font-size: 18px;
-            line-height: 100%;
-            color: var(--color-white);
-        }
+    .svg {
+        margin-bottom: 35px;
+        margin-top: 60px;
     }
-
-    // Hover states
-    @media #{$has-hover} {
-        &:hover {
-            .link:hover {
-                color: var(--color-primary-blue);
-            }
-        }
+    .title {
+        font-family: var(--font-primary);
+        font-style: normal;
+        font-weight: 500;
+        font-size: 40px;
+        line-height: 100%;
+        color: var(--color-primary-blue);
+        text-align: center;
+        letter-spacing: 0.01em;
+        max-width: 630px;
+        margin-bottom: 16px;
     }
-
-    // Breakpoints
-    @media #{$lte-tablet} {
-        padding-top: 40px;
-        padding-bottom: 40px;
-
-        .container {
-            display: block;
-
-            .links {
-                display: block;
-                margin: 0;
-            }
-            .item {
-                display: block;
-                line-height: 2em;
-            }
-
-            .regents {
-                display: block;
-                margin: 50px 0 0 0;
-            }
-        }
+    .text {
+        margin-bottom: 35px;
+    }
+    .button-link {
+        width: 280px;
+        margin-bottom: 60px;
     }
 }
 </style>
--- stories/BlockCallToAction.stories.js
+++ stories/BlockCallToAction.stories.js
@@ -3,12 +3,15 @@ import * as API from "~/stories/mock-api.json"
 
 // Storybook default settings
 export default {
-    title: "BLOCK / Call to Action",}
+    title: "BLOCK / Call to Action",
+}
 
 const mock = {
     to: "/help/foo/bar/",
-    title: "Not finding what you are looking for?",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    name: "Lorem ipsum dolor",
+    title: "Lorem ipsum dolor sit amet?",
+    text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 }
 
 // Variations of stories below
@@ -20,34 +23,11 @@ export const Default = () => ({
     },
     template: `
         <block-call-to-action
+
             :to="to"
+            :name="name"
             :title="title"
             :text="text"
         />
     `,
-})
-export const LongText = () => ({
-    data() {
-        return {
-            items: [...API.links],
-        }
-    },
-    computed: {
-        parsedItems() {
-            // Restructuring item to support text key
-            return this.items.map((obj, i) => {
-                let text = obj.name
-
-                if (i == 0) {
-                    text = "UCLA Library Copyright Policies"
-                }
-
-                return {
-                    ...obj,
-                    text,
-                }
-            })
-        },
-    },
-    template: `<footer-sock :items="parsedItems"/>`,
 })
