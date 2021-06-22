6707c3397a70d53d95f1017426f1ad55914d646c
Ashton Prigge <ashton.prigge@gmail.com>
2021-06-22 16:46:22 -0700

    Update svgs, update styling

--- components/BlockCallToAction.vue
+++ components/BlockCallToAction.vue
@@ -1,28 +1,35 @@
 <template lang="html">
-    <div class="block-call-to-action">
-        <svg-call-to-action class="svg" />
-        <div
-            class="title"
-            v-html="title"
-        />
-        <div
-            class="text"
-            v-html="text"
-        />
-        <button-link
-            :label="name"
-            :to="to"
-            class="button-link"
-        />
+    <div :class="sizeClasses">
+        <div :class="classes">
+            <component
+                :is="parsedSvgName"
+                class="svg"
+            />
+            <div
+                class="title"
+                v-html="title"
+            />
+            <div
+                class="text"
+                v-html="text"
+            />
+            <button-link
+                :label="name"
+                :to="to"
+                class="button-link"
+                :icon-name="iconName"
+            />
+        </div>
     </div>
 </template>
 
 <script>
-import SvgCallToAction from "~/assets/svg/call-to-action"
-
 export default {
     components: {
-        SvgCallToAction,
+        SvgCallToActionMoney: () => import("~/assets/svg/call-to-action-money"),
+        SvgCallToActionChat: () => import("~/assets/svg/call-to-action-chat"),
+        SvgCallToActionMail: () => import("~/assets/svg/call-to-action-mail"),
+        SvgCallToActionFind: () => import("~/assets/svg/call-to-action-find"),
     },
     props: {
         iconName: {
@@ -45,10 +52,34 @@ export default {
             type: String,
             default: "",
         },
+        isLightBlue: {
+            type: Boolean,
+            default: true,
+        },
+        isFullSize: {
+            type: Boolean,
+            default: true,
+        },
     },
     computed: {
         parsedSvgName() {
             return `${this.iconName}`
+        },
+        classes() {
+            if (this.isLightBlue !== true) {
+                return [
+                    "block-call-to-action",
+                    "color-dark-blue",
+                    "color-light",
+                ]
+            } else {
+                return ["block-call-to-action"]
+            }
+        },
+        sizeClasses() {
+            if (this.isFullsize !== true) {
+                return ["block-call-to-action-small"]
+            }
         },
     },
 }
@@ -57,10 +88,10 @@ export default {
 <style lang="scss" scoped>
 .block-call-to-action {
     max-width: 1046px;
-    max-height: 467px;
+    max-height: 700px;
     margin-left: auto;
     margin-right: auto;
-    background-color: var(--color-primary-lightest-blue);
+    background-color: var(--color-theme);
 
     display: flex;
     flex-direction: column;
@@ -69,28 +100,63 @@ export default {
     align-content: center;
     align-items: center;
 
+    // Themes
+    --color-theme: var(--color-primary-lightest-blue);
+    &.color-dark-blue {
+        --color-theme: var(--color-secondary-blue-02);
+    }
+    .block-call-to-action-small {
+        max-width: 500px;
+        max-height: 520px;
+    }
+
     .svg {
         margin-bottom: 35px;
         margin-top: 60px;
+        flex-grow: 0;
+        flex-shrink: 0;
+    }
+    // Themes
+    --color-text: var(--color-primary-blue);
+    &.color-light {
+        --color-text: var(--color-white);
     }
     .title {
         font-family: var(--font-primary);
-        font-style: normal;
         font-weight: 500;
         font-size: 40px;
         line-height: 100%;
-        color: var(--color-primary-blue);
+        color: var(--color-text);
         text-align: center;
-        letter-spacing: 0.01em;
+        padding-left: 70px;
+        padding-right: 70px;
         max-width: 630px;
         margin-bottom: 16px;
     }
+
     .text {
         margin-bottom: 35px;
+        font-size: 18px;
+        text-align: center;
+        line-height: 140%;
+        padding-left: 10px;
+        padding-right: 10px;
+        max-width: 630px;
+        color: var(--color-text);
     }
+
     .button-link {
         width: 280px;
         margin-bottom: 60px;
+        background-color: var(--color-primary-blue);
+        color: var(--color-white);
+    }
+    /deep/ {
+        .button-link {
+            .arrow-right {
+                stroke: var(--color-white);
+            }
+        }
     }
 }
 </style>
--- stories/BlockCallToAction.stories.js
+++ stories/BlockCallToAction.stories.js
@@ -8,6 +8,7 @@ export default {
 
 const mock = {
     to: "/help/foo/bar/",
+
     name: "Lorem ipsum dolor",
     title: "Lorem ipsum dolor sit amet?",
     text:
@@ -28,6 +29,45 @@ export const Default = () => ({
             :name="name"
             :title="title"
             :text="text"
+            icon-name= "svg-call-to-action-chat"
+        />
+    `,
+})
+
+// Variations of stories below
+export const DarkBlue = () => ({
+    data() {
+        return {
+            ...mock,
+        }
+    },
+    template: `
+        <block-call-to-action
+            is-light-blue="false"
+            :to="to"
+            :name="name"
+            :title="title"
+            :text="text"
+            icon-name= "svg-call-to-action-chat"
+        />
+    `,
+})
+
+// Variations of stories below
+export const SmallCTA = () => ({
+    data() {
+        return {
+            ...mock,
+        }
+    },
+    template: `
+        <block-call-to-action
+            is-full-size="false"
+            :to="to"
+            :name="name"
+            :title="title"
+            :text="text"
+            icon-name= "svg-call-to-action-chat"
         />
     `,
 })
