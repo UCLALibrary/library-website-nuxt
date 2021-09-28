caa71242319884e909aabbf44cd5d0d004303408
jendiamond <thejendiamond@gmail.com>
2021-09-28 15:25:36 -0700

    Add Divider Flexible Page Block

---
+++ components/Flexible/Divider.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+    <divider-general />
+</template>
+
+<script>
+export default {
+    props: {
+        block: {
+            type: Object,
+            default: () => {},
+        },
+    },
+}
+</script>
