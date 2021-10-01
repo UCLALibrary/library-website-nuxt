ebfc532cc647a1a793d236df707d251703ea6d81
Ashton Prigge <aprigge@library.ucla.edu>
2021-09-30 17:14:57 -0700

    Help topic card FPB

---
+++ components/Flexible/HelpTopicCard3Up.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+    <div class="help-cards">
+        <block-card-vertical v-for="card in block" />
+        <nuxt-link
+            v-if="to"
+            class="card card-more"
+            :to="to"
+        >
+            <button-more class="button" />
+        </nuxt-link>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        block: {
+            type: Object,
+            default: () => {},
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
