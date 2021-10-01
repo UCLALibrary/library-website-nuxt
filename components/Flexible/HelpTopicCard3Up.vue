9081416aae5cbbc0bb43bf916de077a73ab96ca7
Ashton Prigge <aprigge@library.ucla.edu>
2021-10-01 09:37:39 -0700

    update fpb

--- components/Flexible/HelpTopicCard3Up.vue
+++ components/Flexible/HelpTopicCard3Up.vue
@@ -1,6 +1,14 @@
 <template lang="html">
-    <div class="help-cards">
-        <block-card-vertical v-for="card in block" />
+    <div class="cards">
+        <block-card-vertical
+            v-for="item in items"
+            :key="item.to"
+            class="card"
+            :icon-name="item.iconName"
+            :to="item.to"
+            :title="item.title"
+            :text="item.text"
+        />
         <nuxt-link
             v-if="to"
             class="card card-more"
@@ -22,4 +30,93 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.cards {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-content: flex-start;
+    align-items: flex-start;
+}
+.card {
+    margin: 0 30px 0 0;
+    width: 281px;
+
+    &:nth-child(3n) {
+        margin-right: 0;
+    }
+    &:nth-child(n + 4) {
+        margin-top: 40px;
+    }
+}
+.card-more {
+    border: 2px solid var(--color-primary-blue-01);
+    width: 281px;
+    height: 400px;
+    border-radius: var(--rounded-slightly-all);
+    box-sizing: border-box;
+
+    transition-property: box-shadow, transform;
+    transition-duration: 400ms;
+    transition-timing-function: ease-in-out;
+
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-content: center;
+    align-items: center;
+
+    .button {
+        width: 100%;
+        max-width: 100%;
+        height: 100%;
+    }
+}
+
+// Hovers
+.card-more {
+    @media #{$has-hover} {
+        &:hover {
+            transform: scale(1.1);
+            box-shadow: 0px 10px 17px rgba(0, 0, 0, 0.04);
+        }
+    }
+}
+
+// Breakpoints
+@media #{$lte-tablet} {
+    .meta {
+        padding: 0 var(--unit-gutter);
+        margin-bottom: 40px;
+
+        .title {
+            font-size: 40px;
+        }
+        .text {
+            max-width: 600px;
+            font-size: 22px;
+        }
+    }
+    .cards {
+        width: auto;
+        overflow-x: auto;
+        overflow-y: visible;
+        flex-wrap: nowrap;
+        min-height: 420px;
+        margin-right: var(--unit-gutter);
+        .card {
+            margin-top: 0;
+            margin-right: 0;
+            margin-left: 30px;
+            flex-shrink: 0;
+
+            &:first-child {
+                margin-left: var(--unit-gutter);
+            }
+        }
+    }
+}
+}
+</style>
