c3e51460715c688e8657b2b5574660ef9a5116ba
Ashton Prigge <aprigge@library.ucla.edu>
2021-10-08 13:46:35 -0700

    rebase

---
+++ components/Flexible/BannerFeatured.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+  <banner-featured
+    :image="block.contentLink[0].image"
+    :to="block.contentLink[0].to"
+    :title="block.contentLink[0].title"
+    :category="category"
+    :breadcrumb="breadcrumb"
+    :dates="dates"
+    :is-online="isOnline"
+    :prompt="block.prompt"
+    :align-right="parsedAlignment"
+    :location="parsedLocations"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    block: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  computed: {
+    parsedAlignment() {
+      return this.block.alignment === "right" ? true : false;
+    },
+    parsedIsOnline() {},
+    parsedLocations() {
+      locations = [];
+      for (let location in block.contentLink[0].associatedLocations) {
+        locations.push(
+          block.contentLink[0].associatedLocations[location].title
+        );
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
