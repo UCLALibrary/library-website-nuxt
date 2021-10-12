26a22ba21ba9080895cbfd4869313e97df810bac
pghorpade <pghorpade@library.ucla.edu>
2021-10-12 11:21:49 -0700

    add functionality to open and close the share links

--- components/HeaderSticky.vue
+++ components/HeaderSticky.vue
@@ -1,13 +1,14 @@
 <template lang="html">
-    <header class="header-sticky">
+    <header :class="classes">
         <div class="bread-crumb">
             <!--TODO add the right chevron between breadcrumb -->
             <nuxt-link
-                v-for="item in crumbs"
-                :key="item.path"
-                :to="item.path"
-                v-html="item.title"
+                :key="crumbs[0].path"
+                :to="crumbs[0].path"
+                v-html="crumbs[0].title"
             />
+            <svg-chevron-left class="chevron-left" />
+            <span>{{ crumbs[1].title }}</span>
         </div>
         <div v-if="hasCallToAction">
             <smart-link
@@ -25,27 +26,67 @@
             v-if="hasShareLinks"
             class="social-shares"
         >
-            <span class="share-text"> Share </span>
-            <a
-                v-for="item in mainShareLinks"
-                :key="item.url"
-                :href="item.url"
-                target="_blank"
-                v-html="item.name"
-            />
-            <span> svg for 3 blue dots</span>
+            <div class="main-shares">
+                <span class="share-text"> Share </span>
+                <a
+                    v-for="item in mainShareLinks"
+                    :key="item.name"
+                    :href="item.url"
+                    target="_blank"
+                >
+                    <component :is="item.icon" />
+                </a>
+                <span @click="toggleShare()">
+                    <svg-icon-ellipsis class="svg-ellipsis" />
+                    <svg-glyph-close class="svg-close" />
+                </span>
+            </div>
+            <div class="other-shares">
+                <a
+                    v-for="item in otherShareLinks"
+                    :key="item.name"
+                    :href="item.url"
+                    target="_blank"
+                    class="other-link"
+                >
+                    <component :is="item.icon" />
+                    <span class="share-name">{{ item.name }}</span>
+                </a>
+            </div>
         </div>
     </header>
 </template>
 
 <script>
+//lodash
 import _get from "lodash/get"
-
+//helpers
 import getShareLinks from "~/utils/getShareLinks"
 
 // TODO Share Links design
+//svgs
+import SvgIconSocialTwitter from "~/assets/svg/icon-social-twitter"
+import SvgIconSocialLinkedin from "~/assets/svg/icon-social-linkedin"
+import SvgIconSocialFacebook from "~/assets/svg/icon-social-facebook"
+import SvgIconSocialWhatsapp from "~/assets/svg/icon-social-whatsapp"
+import SvgIconSharePrinter from "~/assets/svg/icon-share-printer"
+import SvgIconShareEmail from "~/assets/svg/icon-share-email"
+import SvgIconEllipsis from "~/assets/svg/icon-ellipsis"
+import SvgGlyphClose from "~/assets/svg/glyph-close"
+import SvgChevronLeft from "~/assets/svg/chevron-left"
 
 export default {
+    components: {
+        SvgIconSocialTwitter,
+        SvgIconSocialLinkedin,
+        SvgIconSocialFacebook,
+        SvgIconSocialWhatsapp,
+        SvgIconSharePrinter,
+        SvgIconShareEmail,
+        SvgIconEllipsis,
+        SvgGlyphClose,
+        SvgChevronLeft,
+    },
     props: {
         crumbs: {
             type: Array,
@@ -84,7 +125,15 @@ export default {
             default: "",
         },
     },
+    data() {
+        return {
+            isOpened: false,
+        }
+    },
     computed: {
+        classes() {
+            return ["header-sticky", { "is-opened": this.isOpened }]
+        },
         parseTarget() {
             return this.callToActionIconName.includes("arrow-diagonal")
                 ? "_blank"
@@ -106,10 +155,46 @@ export default {
         },
         mainShareLinks() {
             return [
-                { name: "Twitter", url: this.allShareLinks.twitter },
-                { name: "LinkedIn", url: this.allShareLinks.linkedin },
-                { name: "Facebook", url: this.allShareLinks.facebook },
+                {
+                    name: "Twitter",
+                    url: this.allShareLinks.twitter,
+                    icon: "svg-icon-social-twitter",
+                },
+                {
+                    name: "LinkedIn",
+                    url: this.allShareLinks.linkedin,
+                    icon: "svg-icon-social-linkedin",
+                },
+                {
+                    name: "Facebook",
+                    url: this.allShareLinks.facebook,
+                    icon: "svg-icon-social-facebook",
+                },
             ]
+        },
+        otherShareLinks() {
+            return [
+                {
+                    name: "Email",
+                    url: this.allShareLinks.email,
+                    icon: "svg-icon-share-email",
+                },
+                {
+                    name: "WhatsApp",
+                    url: this.allShareLinks.whatsapp,
+                    icon: "svg-icon-social-whatsapp",
+                },
+                {
+                    name: "Print",
+                    url: this.allShareLinks.printer,
+                    icon: "svg-icon-share-printer",
+                },
+            ]
+        },
+    },
+    methods: {
+        toggleShare() {
+            this.isOpened = !this.isOpened
         },
     },
 }
@@ -117,13 +202,84 @@ export default {
 
 <style lang="scss" scoped>
 .header-sticky {
-    background-color: red;
+    background-color: var(--color-white);
+    width: 100%;
+    padding: 15px 200px;
 
     display: flex;
     flex-direction: row;
     flex-wrap: nowrap;
-    justify-content: space-between;
+    justify-content: flex-start;
     align-items: center;
+    .bread-crumb {
+        width: 816px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        align-content: center;
+    }
+    .social-shares {
+        width: 240px;
+        .svg-close {
+            display: none;
+        }
+        .main-shares {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-evenly;
+            align-items: center;
+            align-content: center;
+            // padding: 20px 47px;
+        }
+        .other-shares {
+            background-color: var(--color-white);
+            opacity: 0;
+            max-height: 0;
+            overflow: hidden;
+            padding: 10px 22px;
+            position: absolute;
+            max-width: 240px;
+            transition-property: opacity, max-height;
+            transition-duration: 400ms;
+            transition-timing-function: ease-in-out;
+
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            justify-content: flex-start;
+            align-items: center;
+            align-content: center;
+
+            .other-link {
+                &:not(last-child) {
+                    padding-right: 30px;
+                }
+                margin-top: 16px;
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                align-content: center;
+            }
+        }
+    }
+
+    // States
+    &.is-opened {
+        .svg-close {
+            display: inline;
+        }
+        .svg-ellipsis {
+            display: none;
+        }
+        .other-shares {
+            max-height: 120px;
+            opacity: 1;
+        }
+    }
+
+    // Breakpoints
+    @media #{$lte-phone} {
+    }
 }
 </style>
 
