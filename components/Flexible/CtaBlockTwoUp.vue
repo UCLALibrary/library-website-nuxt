fce6c679c3df2aa595a16d2191b4f590d795222c
jendiamond <thejendiamond@gmail.com>
2021-10-19 13:24:06 -0700

    aAd cta files

---
+++ components/Flexible/CtaBlockTwoUp.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+    <block-call-to-action
+        v-for="item in parsedItems"
+        :key="item.id"
+        :is-small-size="true"
+        :name="item.buttonText"
+        :title="item.title"
+        :text="item.description"
+        :icon-name="item.icon"
+    />
+</template>
+
+<script>
+export default {
+    props: {
+        block: {
+            type: Object,
+            default: () => {},
+        },
+    },
+    computed: {
+        parsedItems() {
+            let items = []
+            for (let item in this.block.ctaBlock2Up) {
+                items.push(this.block.ctaBlock2Up[0])
+            }
+            return items
+        },
+    },
+}
+</script>
+
+<style lang="css" scoped></style>
