d8fc4546fc0684ab54f888c9484c187c0affa5df
jendiamond <thejendiamond@gmail.com>
2021-10-20 11:07:18 -0700

    Loop

--- components/Flexible/CtaBlockTwoUp.vue
+++ components/Flexible/CtaBlockTwoUp.vue
@@ -1,11 +1,11 @@
 <template lang="html">
     <block-call-to-action
         v-for="item in parsedItems"
-        :key="item.id"
+        class="cta-block-two-up"
         :is-small-size="true"
-        :name="item.buttonText"
-        :title="item.title"
-        :text="item.description"
+        :name="item.urlButtonText"
+        :title="item.titleCTA2Up"
+        :text="item.shortDescription"
         :icon-name="item.icon"
     />
 </template>
@@ -20,14 +20,16 @@ export default {
     },
     computed: {
         parsedItems() {
-            let items = []
-            for (let item in this.block.ctaBlock2Up) {
-                items.push(this.block.ctaBlock2Up[0])
-            }
-            return items
+            return this.block.ctaBlock2Up[0]
         },
     },
 }
 </script>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.cta-block-two-up {
+    max-width: 924px;
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
--- stories/Flexible_CtaBlockTwoUp.stories.js
+++ stories/Flexible_CtaBlockTwoUp.stories.js
@@ -3,23 +3,24 @@ export default {
 }
 
 const mock = {
-    "ctaBlock2Up": [
+    ctaBlock2Up: [
         {
-          "id": "3036",
-          "shortDescription": "Cat",
-          "urlButtonText": "Cat",
-          "urlButtonUrl": "http://cat.com",
-          "titleCTA2Up": "Cat",
-          "icon": "https://img.icons8.com/fluent/2x/doge.png"
+            "id": "3036",
+            "shortDescription": "Cat",
+            "urlButtonText": "Cat",
+            "urlButtonUrl": "http://cat.com",
+            "titleCTA2Up": "Cat",
+            "icon": "https://img.icons8.com/fluent/2x/doge.png"
         },
         {
-          "id": "3037",
-          "shortDescription": "Dog",
-          "urlButtonText": "Dog",
-          "urlButtonUrl": "http://dog.com",
-          "titleCTA2Up": "Dog",
-          "icon": "https://img.icons8.com/fluent/2x/doge.png"
+            "id": "3037",
+            "shortDescription": "Dog",
+            "urlButtonText": "Dog",
+            "urlButtonUrl": "http://dog.com",
+            "titleCTA2Up": "Dog",
+            "icon": "https://img.icons8.com/fluent/2x/doge.png"
         },
+    ],
 }
 
 export const Default = () => ({
