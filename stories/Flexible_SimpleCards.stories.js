96faaafad5bea2500b9169b224e4a284d4f98184
Ashton Prigge <aprigge@library.ucla.edu>
2021-10-20 15:02:04 -0700

    simplecard

--- stories/Flexible_SimpleCards.stories.js
+++ stories/Flexible_SimpleCards.stories.js
@@ -1,5 +1,5 @@
 export default {
-    title: "FLEXIBLE / Help Topic Card",
+    title: "FLEXIBLE / Simple Card",
 }
 
 const mock = {
@@ -12,8 +12,7 @@ const mock = {
                     url: "https://test-craft.library.ucla.edu/help/services-resources/borrowing-books-and-equipment",
                     to: "/help/services-resources/borrowing-books-and-equipment",
                     title: "Borrowing Books and Equipment",
-                    text: "Kogi ut austin snackwave try-hard. Labore excepteur activated charcoal trust fund. Gastropub in asymmetrical eu, thundercats flexitarian truffaut waistcoat aliquip. Pour-over irure ex, pinterest et taiyaki incididunt. Craft beer cronut knausgaard, biodiesel poke beard slow-carb ullamco umami semiotics prism taxidermy hoodie meh. Et ugh small batch food truck twee bespoke, laboris roof party knausgaard try-hard tumblr swag cupidatat kombucha eu. Semiotics vegan officia in, pork belly chillwave vape plaid ex affogato veniam shaman brooklyn artisan aliqua.",
-                    iconName: "illustration-find-space",
+                    text: "Pour-over irure ex, pinterest et taiyaki incididunt. Craft beer cronut knausgaard, biodiesel poke beard slow-carb ullamco umami semiotics prism taxidermy hoodie meh. Et ugh small batch food truck twee bespoke, laboris roof party knausgaard try-hard tumblr swag cupidatat kombucha eu. Semiotics vegan officia in, pork belly chillwave vape plaid ex affogato veniam shaman brooklyn artisan aliqua.",
                 },
             ],
         },
@@ -26,7 +25,10 @@ export const Default = () => ({
     },
     template: `
         <flexible-simple-card
-            :block="block"
+            :title="block.simpleCard[0].contentLink[0].title"
+            :text="block.simpleCard[0].contentLink[0].text"
+            :to="block.simpleCard[0].contentLink[0].to"
+
         />
     `,
 })
