780073fbe3ece86d2f88ef2b6331b46f2c39a135
JenDiamond <thejendiamond@gmail.com>
2021-10-28 14:58:32 -0700

    Add Flexible Divider story (#204)
    * aAd cta files
    
    * Loop
    
    * Less broken
    
    * Add CTA Block Content Width story
    
    * Add Divider story
    
    * Update npm
    
    * Delete white space
    
    * Strip down commit to only the DFlexible-ivider story
    
    * Delete BlockVtaContentWIdth from fragments
    
    Co-authored-by: Ashton Prigge <34147754+aprigge@users.noreply.github.com>

---
+++ stories/Flexible_Divider.stories.js
@@ -0,0 +1,33 @@
+export default {
+    title: "FLEXIBLE / Divider",
+}
+
+const mock = {
+    divider: "default"
+}
+
+export const Default = () => ({
+    data() {
+        return { block: mock }
+    },
+    template: `
+        <flexible-divider
+            :block="block"
+        />
+    `,
+})
+
+const mockBold = {
+    divider: "bold"
+}
+
+export const Bold= () => ({
+    data() {
+        return { block: mockBold }
+    },
+    template: `
+            <flexible-divider
+                :block="block"
+            />
+        `,
+})
