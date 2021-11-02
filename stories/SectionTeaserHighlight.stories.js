55137d93554520c225a1d20a6b1c255b5d02195b
Ashton Prigge <34147754+aprigge@users.noreply.github.com>
2021-11-02 11:17:34 -0700

    Create Flexible-Highlight component (#207)
    * Highlight FPB
    
    * refactor block highlight
    
    * refactor block highlight
    
    * Update highlight fpb
    
    * Update highlight fpb
    
    * Update story data
    
    * remove computed property
    
    * Update parsed time and date
    
    * Update parsed time and date
    
    * Update parsed time and date
    
    * Update parsed time and date
    
    * update css

--- stories/SectionTeaserHighlight.stories.js
+++ stories/SectionTeaserHighlight.stories.js
@@ -11,8 +11,8 @@ const mock = [
         to: "/visit/foo/bar/",
         category: "Ullamco",
         title: "Fames ac turpis egestas sed tempus lorem ipsum",
-        date: "Pempti, Ocavia 14, 2021",
-        time: "3 - 5 pm",
+        startDate: "2021-09-03T08:00:00+00:00",
+        endDate: "2021-10-08T07:10:00+00:00",
         text: "Ultricies leo integer malesuada nunc vel risus commodo viverra.",
     },
     {
@@ -20,30 +20,41 @@ const mock = [
         to: "/visit/foo/bar/",
         category: "Aliquam Varius",
         title: "Pretium magna eget dignissim placerat arcu velit et metus",
-        date: "Pempti, Ocavia 14, 2021",
-        time: "1 - 3 pm",
-        text:
-            "Pellentesque molestie, est ut vestibulum convallis, velit ante auctor mauris, sed mattis dolor ante ac purus. Nullam egestas neque sed ullamcorper suscipit.",
+        startDate: "2021-09-03T08:00:00+00:00",
+        endDate: "2021-10-08T07:10:00+00:00",
+        locations: [
+            { title: "Powellarium", to: "/location/bar" },
+            {
+                title: "Research Library (Charles E. Young)",
+                to: "/location/baz",
+            },
+        ],
+        text: "Pellentesque molestie, est ut vestibulum convallis, velit ante auctor mauris, sed mattis dolor ante ac purus. Nullam egestas neque sed ullamcorper suscipit.",
     },
     {
         image: API.image,
         to: "/visit/foo/baz/",
         category: "Sagittis",
         title: "Auctor Ligula",
-        date: "Mercury, Ocavia 16, 2021",
-        time: "4 - 6 pm",
-        text:
-            "Proin sit amet leo eros. Pellentesque luctus tortor eu aliquet tristique. Proin fringilla enim eget sagittis mattis. Praesent faucibus libero ipsum.",
+        startDate: "2021-01-03T08:00:00+00:00",
+        endDate: "2021-02-08T07:10:00+00:00",
+        locations: [
+            { title: "Online", to: "/location/bar" },
+            {
+                title: "Research Library (Charles E. Young)",
+                to: "/location/baz",
+            },
+        ],
+        text: "Proin sit amet leo eros. Pellentesque luctus tortor eu aliquet tristique. Proin fringilla enim eget sagittis mattis. Praesent faucibus libero ipsum.",
     },
     {
         image: API.image,
         to: "/visit/foo/baz/",
         category: "Tristique",
         title: "Amet nisl suscipit adipiscing bibendum lectus sed",
-        date: "Mars, Ocavia 17, 2021",
-        time: "6 - 8 pm",
-        text:
-            "A diam maecenas sed enim. Tristique senectus et netus et malesuada fames. Nibh nisl condimentum id venenatis. Mi bibendum neque egestas congue. Placerat duis ultricies lacus sed turpis. Massa enim nec dui nunc mattis. Dolor morbi non arcu risus quis varius vestibulum sed.",
+        startDate: "2021-09-03T08:00:00+00:00",
+        endDate: "2021-10-08T07:10:00+00:00",
+        text: "A diam maecenas sed enim. Tristique senectus et netus et malesuada fames. Nibh nisl condimentum id venenatis. Mi bibendum neque egestas congue. Placerat duis ultricies lacus sed turpis. Massa enim nec dui nunc mattis. Dolor morbi non arcu risus quis varius vestibulum sed.",
     },
 ]
 
