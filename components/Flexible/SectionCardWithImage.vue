ee267ccefea919ea6e142b6fabc82a516cc30af6
Ashton Prigge <aprigge@library.ucla.edu>
2021-11-10 15:45:34 -0800

    Card with Image

---
+++ components/Flexible/SectionCardWithImage.vue
@@ -0,0 +1,63 @@
+<template lang="html">
+    <div class="section-card-with-image">
+        <block-highlight
+            v-for="item in parsedItems"
+            :key="item.to"
+            :to="item.to"
+            :image="item.image[0].image[0]"
+            :category="item.category"
+            :title="item.title"
+            :start-date="item.date[0].startDate"
+            :end-date="item.date[0].endDate"
+            :text="item.text"
+            :is-vertical="true"
+            image-aspect-ratio="60"
+            class="block"
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        block: {
+            type: Object,
+            default: () => {},
+        },
+    },
+    computed: {
+        parsedItems() {
+            let items = []
+            for (let item in this.block.cardWithImage) {
+                items.push(this.block.cardWithImage[item].contentLink[0])
+            }
+            console.log(items)
+            return items
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.section-card-with-image {
+    max-width: 924px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-content: flex-start;
+    align-items: flex-start;
+
+    .block {
+        margin: 0 30px 0 0;
+        width: 281px;
+
+        &:nth-child(3n) {
+            margin-right: 0;
+        }
+        &:nth-child(n + 4) {
+            margin-top: 40px;
+        }
+    }
+}
+</style>
