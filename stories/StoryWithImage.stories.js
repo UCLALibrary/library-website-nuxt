1d4ca1fa5a15601740c2b26fbf71a4848f3f08d0
JenDiamond <thejendiamond@gmail.com>
2022-01-10 09:22:57 -0800

    APPS-1245_story-with-image (#232)
    * APPS-1245_story-with-image
    
    Changes to be committed:
    new file:   components/StoryWithImage.vue
    modified:   stories/Flexible_SimpleCards.stories.js
    new file:   stories/StoryWithImage.stories.js
    new file:   stories/impact-report.json
    
    * Style caption
    
    Changes to be committed:
    modified:   components/StoryWithImage.vue
    modified:   stories/StoryWithImage.stories.js
    modified:   stories/impact-report.json
    
    * Add all missing v-ifs

---
+++ stories/StoryWithImage.stories.js
@@ -0,0 +1,26 @@
+// Import mock api data
+import * as API from "~/stories/impact-report.json"
+
+// Storybook default settings
+export default {
+    title: "IMPACT REPORT / Story with Image",
+}
+
+// Variations of stories below
+export const Default = () => ({
+    data() {
+        return {
+            item: { ...API.story[0] },
+        }
+    },
+    template: `
+      <story-with-image
+          :image="item.image"
+          :caption="item.image.caption"
+          :title="item.title"
+          :text="item.text"
+          :footnote="item.footnote"
+          :calltoaction="item.calltoaction"
+      />
+  `,
+})
