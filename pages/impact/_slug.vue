5e574bdecfd6874dd2286c506afb4c8a26510513
Parinita Mulak <pghorpade@library.ucla.edu>
2022-01-24 11:54:47 -0800

    Adds S3 Bucket URL (#250)

--- pages/impact/_slug.vue
+++ pages/impact/_slug.vue
@@ -3,8 +3,7 @@
         <banner-header
             class="section banner-header"
             :title="bannerHeader.title"
-            :image="bannerHeader.image"
-            :video="bannerHeader.video"
+            :video="parseVideo"
             :to="bannerHeader.to"
             :align-right="true"
         />
@@ -41,6 +40,9 @@
 // TODO replace this file with actual json data from google sheet
 import * as MOCK_IMPACT_API from "~/data/mock-impact-report.json"
 
+// Utilities
+import getS3Bucket from "~/utils/getS3Bucket"
+
 export default {
     layout: "impact",
     data() {
@@ -50,7 +52,35 @@ export default {
             mainStory: MOCK_IMPACT_API.mainStory,
         }
     },
-    computed: {},
+    computed: {
+        parseVideo() {
+            let video = {
+                sourceUrl:
+                    "https://mock-api.funkhaus.us/wp-content/uploads/2021/01/Screen-Shot-2018-04-05-at-3.43.34-PM_res-4-1-2560x1442.jpg",
+                sizes: "(max-width: 2560px) 100vw, 2560px",
+                srcSet: "https://mock-api.funkhaus.us/wp-content/uploads/2021/01/Screen-Shot-2018-04-05-at-3.43.34-PM_res-4-1-2560x1442.jpg 2560w, https://mock-api.funkhaus.us/wp-content/uploads/2021/01/Screen-Shot-2018-04-05-at-3.43.34-PM_res-4-1-300x169.jpg 300w, https://mock-api.funkhaus.us/wp-content/uploads/2021/01/Screen-Shot-2018-04-05-at-3.43.34-PM_res-4-1-1024x577.jpg 1024w, https://mock-api.funkhaus.us/wp-content/uploads/2021/01/Screen-Shot-2018-04-05-at-3.43.34-PM_res-4-1-768x432.jpg 768w, https://mock-api.funkhaus.us/wp-content/uploads/2021/01/Screen-Shot-2018-04-05-at-3.43.34-PM_res-4-1-1536x865.jpg 1536w, https://mock-api.funkhaus.us/wp-content/uploads/2021/01/Screen-Shot-2018-04-05-at-3.43.34-PM_res-4-1-2048x1153.jpg 2048w, https://mock-api.funkhaus.us/wp-content/uploads/2021/01/Screen-Shot-2018-04-05-at-3.43.34-PM_res-4-1-960x541.jpg 960w, https://mock-api.funkhaus.us/wp-content/uploads/2021/01/Screen-Shot-2018-04-05-at-3.43.34-PM_res-4-1-375x211.jpg 375w, https://mock-api.funkhaus.us/wp-content/uploads/2021/01/Screen-Shot-2018-04-05-at-3.43.34-PM_res-4-1-1280x721.jpg 1280w, https://mock-api.funkhaus.us/wp-content/uploads/2021/01/Screen-Shot-2018-04-05-at-3.43.34-PM_res-4-1-1920x1081.jpg 1920w",
+                id: "cG9zdDo2NQ==",
+                databaseId: 65,
+                title: "Screen-Shot-2018-04-05-at-3.43.34-PM_res-4-1",
+                altText: "",
+                caption: null,
+                mediaDetails: {
+                    height: 1892,
+                    width: 3360,
+                },
+                imageMeta: {
+                    videoUrl: getS3Bucket(
+                        this.$config,
+                        "ucla-impact-report-animation.mp4"
+                    ),
+                    primaryColor: null,
+                    focalPointX: null,
+                    focalPointY: null,
+                },
+            }
+            return video
+        },
+    },
 }
 </script>
 
