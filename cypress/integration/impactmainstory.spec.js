7a42d8bc1f002179e857d11102b89f0da4e32514
pghorpade <pghorpade@library.ucla.edu>
2022-02-04 11:13:09 -0800

    Fix title

--- cypress/integration/impactmainstory.spec.js
+++ cypress/integration/impactmainstory.spec.js
@@ -4,7 +4,7 @@ describe('Impact Main Story page', () => {
 
         // UCLA Library brand
         cy.get('.logo-ucla').should('be.visible')
-        cy.get('h1.title').should('contain','After Covid Triggered a Campus Closure, UCLA Library Reimagined its Services')
+        cy.get('h1.title').should('contain','After Covid Triggered a Campus Closure, UCLA Library Reimagined Its Services')
         cy.get('.media-video').should('have.attr','src','https://static.library.ucla.edu/ucla-impact-report-feature-story-animation.mp4')
         cy.percySnapshot({ widths: [768, 992, 1200] })
 
