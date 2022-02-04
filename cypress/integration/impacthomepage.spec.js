f9e2c01af4d708a0c8da2a6e53668492ebb1b02c
pghorpade <pghorpade@library.ucla.edu>
2022-02-03 16:17:23 -0800

    Add h1 to class selector

--- cypress/integration/impacthomepage.spec.js
+++ cypress/integration/impacthomepage.spec.js
@@ -7,7 +7,7 @@ describe('Impact Homepage', () => {
 
 
         // Ginny's Letter
-        cy.get('.intro')
+        cy.get('h1.intro')
             .should('contain', 'Impact Report')
         cy.get('.signature').should('contain','Virginia Steel')
 
