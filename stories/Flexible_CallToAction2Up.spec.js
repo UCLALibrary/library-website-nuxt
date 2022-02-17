1bef357616a8bc247a0ca1fd1d8c9c4318f8793c
pghorpade <pghorpade@library.ucla.edu>
2022-02-16 19:00:17 -0800

    Fix call to action 2 up spec

--- stories/Flexible_CallToAction2Up.spec.js
+++ stories/Flexible_CallToAction2Up.spec.js
@@ -1,7 +1,7 @@
 describe('FLEXIBLE / Call To Action 2 Up', () => {
     it('Default', () => {
         cy.visit('/iframe.html?id=flexible-call-to-action-2-up--default&args=&viewMode=story')
-        cy.get('.block-call-to-action-2-up').should('exist')
+        cy.get('.block-call-to-action-two-up').should('exist')
 
         cy.percySnapshot('FLEXIBLE / Call To Action 2 Up: Default')
     })
