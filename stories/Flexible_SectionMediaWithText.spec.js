0c57ac9362032c34b24f70c5e3c562e25ed3e23d
Ashton Prigge <aprigge@library.ucla.edu>
2022-02-22 12:47:48 -0800

    Rebase

--- stories/Flexible_SectionMediaWithText.spec.js
+++ stories/Flexible_SectionMediaWithText.spec.js
@@ -8,12 +8,14 @@ describe("FLEXIBLE / Section Media With Text", () => {
         cy.percySnapshot("FLEXIBLE / Section Media With Text: Default")
     })
 
-    // it("Grey", () => {
-    //     cy.visit(
-    //         "/iframe.html?id=flexible-section-media-with-text--default&args=&viewMode=story"
-    //     )
-    //     cy.get(".section-media-with-text").should("exist")
-    //
-    //     cy.percySnapshot("FLEXIBLE / Section Media With Text: Default")
-    // })
+    it("Grey", () => {
+        cy.visit(
+            "/iframe.html?id=flexible-section-media-with-text--is-grey-background&args=&viewMode=story"
+        )
+        cy.get(".section-media-with-text").should("exist")
+
+        cy.percySnapshot(
+            "FLEXIBLE / Section Media With Text: Is Grey Background"
+        )
+    })
 })
