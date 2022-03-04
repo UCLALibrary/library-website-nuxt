b192f31eac5a700f2cf47450264a3334ca20666a
Axa Liauw <aliauw@library.ucla.edu>
2022-03-04 10:38:17 -0500

    Adjust width of simple card to be fluid

--- components/SimpleCards.vue
+++ components/SimpleCards.vue
@@ -54,8 +54,7 @@ export default {
 
 <style lang="scss" scoped>
 .simple-cards {
-    max-width: 960px;
-    height: 100%;
+    max-width: $container-l-main + px;
 
     display: flex;
     flex-direction: row;
@@ -68,10 +67,10 @@ export default {
         margin: 12px 16px 0 0;
     }
     .card-large {
-        width: 100%;
+        width: calc(50% - 16px)
     }
     .card-small {
-        width: 300px;
+        width: calc(33.33% - 16px);
     }
     .card-five {
         &:nth-child(-n + 2) {
