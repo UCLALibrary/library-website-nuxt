e2296568e8877360fb4c4f3016f33626be323cd0
JenDiamond <thejendiamond@gmail.com>
2022-03-07 09:40:43 -0800

    Apps 1432 Add ORCID iD to staff detail page (#327)
    * Add orrcid data' to page
    
    * Add styles

---
+++ components/SectionStaffOrcidPublications.vue
@@ -0,0 +1,69 @@
+<template lang="html">
+    <section class="section-staff-orcid-publications">
+        <divider-way-finder
+            class="divider divider-first"
+            color="about"
+        />
+        <div
+            v-if="sectionTitle"
+            class="section-title"
+            v-html="sectionTitle"
+        />
+        <div
+            v-if="orcid"
+            class="orcid"
+            v-html="orcid"
+        />
+        <div
+            v-if="publications"
+            class="publications"
+            v-html="publications"
+        />
+        <divider-way-finder
+            class="divider divider-first"
+            color="about"
+        />
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        sectionTitle: {
+            type: String,
+            default: "",
+            required: true,
+        },
+        orcid: {
+            type: String,
+            default: "",
+        },
+        publications: {
+            type: String,
+            default: "",
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.section-staff-orcid-publications {
+    max-width: 100%;
+    margin: auto;
+
+    .section-title {
+        @include step-3;
+        text-transform: capitalize;
+        color: var(--color-primary-blue-03);
+        padding-top: 100px;
+        padding-bottom: 10px;
+    }
+
+    .orcid {
+        color: forestgreen;
+    }
+    .publications {
+        color: coral;
+    }
+}
+</style>
--- pages/about/staff/_slug.vue
+++ pages/about/staff/_slug.vue
@@ -4,24 +4,51 @@
         <!-- no search on this page -->
         <!-- TODO: Add Breadcrumb Component -->
         <block-staff-detail
+            class="staff-detail-block"
             :image="parsedImage"
             :staff-name="parsedStaffName"
-            :pronouns="page.entry.pronouns"
             :job-title="page.entry.jobTitle"
+            :departments="page.entry.departments"
             :locations="page.entry.locations"
+            :pronouns="page.entry.pronouns"
             :email="page.entry.email"
             :phone="page.entry.phone"
-            :departments="page.entry.departments"
+            :consultation="page.entry.consultation"
             :topics="page.entry.topics"
             :biography="page.entry.biography"
-            class="staff-detail-block"
         />
+
+        <section
+            v-if="page.entry.orcid || page.entry.publications"
+            class="staff-orcid-publications"
+        >
+            <divider-way-finder
+                class="divider divider-first"
+                color="visit"
+            />
+            <h2 class="secondary-header">
+                Publications
+            </h2>
+            <smart-link
+                :to="page.entry.orcid"
+                target="_blank"
+                class="orcid"
+                v-html="`ORCID: <span style='font-size: 20px'>${page.entry.orcid}</span>`"
+            />
+            <rich-text
+                v-if="page.entry.publications"
+                class="publications"
+                :rich-text-content="page.entry.publications"
+            />
+        </section>
+
         <section-staff-article-list
             v-if="parsedItems.length"
-            :items="parsedItems"
-            section-title="Articles"
             class="staff-article-list-section"
+            section-title="Articles"
+            :items="parsedItems"
         />
+
         <!-- this is different from flexible page blocks ctacontentwidth and will be hardcoded where required -->
         <block-call-to-action
             class="section block-call-to-action"
@@ -38,7 +65,7 @@
 // Helpers
 import _get from "lodash/get"
 
-// gql
+// GQL
 import STAFF_DETAIL from "~/gql/queries/StaffDetail"
 
 export default {
@@ -56,6 +83,7 @@ export default {
             return _get(this.page.entry, "image[0]staffMemberImage[0]", null)
         },
         parsedStaffName() {
+            console.log(this.page.entry)
             return `${this.page.entry.nameFirst} ${this.page.entry.nameLast}`
         },
         parsedItems() {
@@ -79,8 +107,44 @@ export default {
         margin-right: auto;
         margin-top: 70px;
     }
-    .staff-article-list-section {
-        background-color: var(--color-secondary-grey-01);
+
+    .divider {
+        margin: 45px 0px;
+        width: 100%;
+    }
+
+    .staff-orcid-publications {
+        max-width: $container-l-main + px;
+        padding: 0 var(--unit-gutter) 46px;
+        margin: auto;
+        .secondary-header {
+            margin-bottom: 48px;
+            @include step-3;
+            color: var(--color-primary-blue-03);
+        }
+        .orcid {
+            font-size: 26px;
+            font-weight: 500;
+            color: var(--color-primary-blue-03);
+        }
+        .publications {
+            font-size: 20px;
+        }
+        .rich-text {
+            margin: 20px auto;
+            margin-left: -20px;
+        }
+        ul, li {
+            margin-bottom: 24px;
+        }
+            // Hover states
+        @media #{$has-hover} {
+            .orcid:hover {
+                text-decoration: underline;
+                text-decoration-color: var(--color-primary-blue-03);
+                text-decoration-thickness: 1.5px;
+            }
+        }
     }
 }
 </style>
