98e38a59bd4ac0e2bac391990739db30cc8fc7b2
JenDiamond <thejendiamond@gmail.com>
2022-03-22 13:23:36 -0700

    Update the styling on the Reserve button' (#341)
    * Update the styling og the Reserve button'
    
    * Identify issues with the buttons and links, add styling
    
    * Add logic for Non-Reservable Spaces
    
    * Finish button icon link
    
    * Fix spec

--- stories/BlockSpaces.spec.js
+++ stories/BlockSpaces.spec.js
@@ -1,8 +1,8 @@
-describe('BLOCK / Staff / List', () => {
+describe('BLOCK / Spaces / List', () => {
     it('Default', () => {
         cy.visit('/iframe.html?id=block-spaces--default&args=&viewMode=story')
         cy.get('.block-spaces').should('exist')
 
-        cy.percySnapshot('BLOCK / Staff / Spaces: Default')
+        cy.percySnapshot('BLOCK / Spaces: Default')
     })
 })
