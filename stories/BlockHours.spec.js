df7fc9c1c92f804995321186481ec970f331a774
jendiamond <thejendiamond@gmail.com>
2022-03-22 12:47:53 -0700

    Delete Hours from cleanup branch

--- stories/BlockHours.spec.js
+++
@@ -1,8 +0,0 @@
-describe('BLOCK / Hours', () => {
-    it('Default', () => {
-        cy.visit('/iframe.html?id=block-hours--default&args=&viewMode=story')
-        cy.get('.block-hours').should('exist')
-
-        cy.percySnapshot('BLOCK / Hours: Default')
-    })
-})
