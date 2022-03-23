fe018809b9b48ce5ea7ea48b2f2f90d610330f4b
Andrew Wallace <andrewwallace@library.ucla.edu>
2022-03-23 13:00:24 -0700

    Edits per review

--- components/MediaGallery/ThumbnailCard.vue
+++ components/MediaGallery/ThumbnailCard.vue
@@ -45,15 +45,11 @@ export default
     padding: 0;
 
     .caption-title {
+        @include step-1;
+
         margin: 0;
         padding: 0;
-
         position: static;
-        font-family: var(--font-primary);
-        font-style: normal;
-        font-weight: normal;
-        font-size: 20px;
-        line-height: 130%;
         color: var(--color-black);
 
         /* Inside auto layout */
@@ -61,24 +57,15 @@ export default
         flex: none;
         order: 0;
         flex-grow: 0;
-        margin: 4px 0px;
+        margin: 16px 0px 8px;
     }
 
     .caption-text {
+        @include step-0;
+
+
         padding: 0;
-
-
-        /* proxima-nova/proxima-nova-regular-16-140-1.5-0 */
-
-        font-family: var(--font-secondary);
-        font-style: normal;
-        font-weight: normal;
-        font-size: 16px;
         line-height: 140%;
-        /* or 22px */
-
-        letter-spacing: 0.015em;
-
         color: var(--color-secondary-grey-05);
 
         /* Inside auto layout */
