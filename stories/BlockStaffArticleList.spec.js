3e02e4617581a0ac56e3d6c9422891575762f8d7
Ashton Prigge <aprigge@library.ucla.edu>
2022-03-23 10:54:19 -0700

    Update spec

--- stories/BlockStaffArticleList.spec.js
+++ stories/BlockStaffArticleList.spec.js
@@ -1,8 +1,10 @@
-describe('BLOCK / Staff / Article / List', () => {
-    it('Default', () => {
-        cy.visit('/iframe.html?id=block-staff-article-list--default&args=&viewMode=story')
-        cy.get('.block-staff-article-list').should('exist')
+describe("BLOCK / Staff / Article / List", () => {
+    it("Default", () => {
+        cy.visit(
+            "/iframe.html?id=block-staff-article-list--default&args=&viewMode=story"
+        )
+        cy.get(".block-staff-article-item").should("exist")
 
-        cy.percySnapshot('BLOCK / Staff / Article / List: Default')
+        cy.percySnapshot("BLOCK / Staff / Article / List: Default")
     })
 })
