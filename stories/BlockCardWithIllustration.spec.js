20dd34070f0b3104422b502d12db01134c60989b
jendiamond <thejendiamond@gmail.com>
2022-03-29 19:29:24 -0700

    Fix spec

--- stories/BlockCardWithIllustration.spec.js
+++ stories/BlockCardWithIllustration.spec.js
@@ -1,6 +1,6 @@
 describe('BLOCK / Card With Illustration', () => {
     it('Default', () => {
-        cy.visit('/iframe.html?id=block-card-with-illustration --default&args=&viewMode=story')
+        cy.visit('/iframe.html?id=block-card-with-illustration--default&args=&viewMode=story')
         cy.get('.block-card-with-illustration ').should('exist')
 
         cy.percySnapshot('BLOCK / Card With Illustration: Default')
