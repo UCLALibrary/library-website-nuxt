ed482b38f842ca87ad8c8413518c39e29890e7eb
JenDiamond <thejendiamond@gmail.com>
2022-04-07 10:28:03 -0700

    APPS-1476 Create IconWithLink Component (#359)
    * Set up files
    
    * Start of IconWithLink
    
    * Just the beginning
    
    * More set up
    
    * Update icon with link
    
    * Add icons and rebase
    
    * Update story
    
    * Update story
    
    * Update story
    
    * Update css
    
    * Update css
    
    * Add social share icons
    
    Co-authored-by: Ashton Prigge <aprigge@library.ucla.edu>
    Co-authored-by: Ashton Prigge <34147754+aprigge@users.noreply.github.com>

---
+++ stories/IconWithLink.stories.js
@@ -0,0 +1,54 @@
+// Storybook default settings
+export default {
+    title: "Icon With Link",
+}
+
+export const Default = () => ({
+    template: `
+        <icon-with-link
+            text="Powell"
+            icon-name="svg-icon-location"
+            to="/visit/library/powell"
+        />
+    `,
+})
+
+export const WithExternalLink = () => ({
+    template: `
+        <icon-with-link
+            text="See More"
+            icon-name="svg-icon-search"
+            to="https://www.google.com/"
+        />
+    `,
+})
+
+export const InternalLink = () => ({
+    template: `
+        <icon-with-link
+            text="Emergency Phone Booth"
+            icon-name="svg-icon-phone"
+            to= "/help/services-and-resources"
+        />
+    `,
+})
+
+export const ConsultationIconLink = () => ({
+    template: `
+        <icon-with-link
+            text="Book a Consultation"
+            icon-name="svg-icon-consultation"
+            to= "/help/services-and-resources"
+        />
+    `,
+})
+
+export const LongText = () => ({
+    template: `
+        <icon-with-link
+            text="A Icon with Link with Very Very Long Text What Happens?"
+            icon-name="svg-icon-share-twitter"
+            to= "/help/services-and-resources"
+        />
+    `,
+})
