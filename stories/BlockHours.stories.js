7309324798a1633b4975b6d9c939821aee9a5aaf
pghorpade <pghorpade@library.ucla.edu>
2022-04-12 14:03:13 -0700

    Remove data from story

--- stories/BlockHours.stories.js
+++ stories/BlockHours.stories.js
@@ -2,16 +2,9 @@ export default {
     title: "block / Hours",
 }
 
-const mock = {
-    campusLocationId: "280",
-    locationName: "YRL Library",
-    buildingAccess:
-        "<p>Park at parking structure 2. You cannot park in the round about. Powell is the second building in on the quad. Walk straight ahead for about 15 minutes. Wear your walking shoes. Finally when you are there walk up the stairs to the rotunda. You are at the library now.</p>",
-}
-
 export const Default = () => ({
     data() {
-        return { ...mock }
+        return{}
     },
     template: `
         <block-hours lid="4690"/>
@@ -20,7 +13,7 @@ export const Default = () => ({
 
 export const NoInternalLocations = () => ({
     data() {
-        return { ...mock }
+        return{}
     },
     template: `
         <block-hours lid="4693"/>
@@ -29,7 +22,7 @@ export const NoInternalLocations = () => ({
 
 export const MoreInternalLocations = () => ({
     data() {
-        return { ...mock }
+        return{}
     },
     template: `
         <block-hours lid="2081"/>
