2ef34fe32c22703bea826f19a5c1e56b30666061
Matheus Gomes <47605309+matheusgomes062@users.noreply.github.com>
2022-04-28 16:24:08 -0300

    APPS-1498 Create BlockLocationListItem Component (#371)
    * feat: inital commit and few minor changes to a new component
    
    * feat: minor improvements on the component and storybook
    
    * feat: improvements on BlockLocationListItem component. Now featuring mobile and tablet versions, also a storybook version with and without image (all with controls to mess around).
    
    * Comment out BlockStaffList line that broke storybook build
    
    export const Default = () => ({
        data() {
            return { ...mock }
        },
        template: `
            <block-location-list-item
                :title="title"
                :to="to"
                :image="image"
                :day="day"
                :startTime="startTime"
                :endTime="endTime"
                :address="address"
                :addressLink="addressLink"
                :amenities="amenities"
                :reserveSeat="reserveSeat"
                :isUclaLibrary="isUclaLibrary"
            />
        `,
    })
    
    * Updated design tokens to version 3
    
    * Fixed molecule placeholder
    
    * Updated package-lock
    
    * Minor changes to the component. Changed date to single string, also changed icon and background-position
    
    * Fixes the background-position-x and the Title link problem
    
    * Change title hover
    
    * Add link to image and refactor the image-container class
    
    * Fixes design styles and remove important from css

---
+++ stories/BlockLocationListItem.stories.js
@@ -0,0 +1,65 @@
+// Import mock api data
+import * as API from "~/stories/mock-api.json"
+import BlockLocationListItem from "~/components/BlockLocationListItem"
+
+const mock = {
+    isUclaLibrary: false,
+    title: "Arts Library",
+    image:  API.image,
+    day: "Today",
+    hour: "8am - 5pm",
+    reserveSeat: "http://google.com/reserveSeat",
+    address: "1400 Public Affairs Building Los Angeles, CA 90095-1392",
+    addressLink: "http://google.com/address",
+    amenities: [
+        "SvgIconEmail",
+        "SvgIconPhone",
+        "SvgIconVirtual",
+        "SvgIconChair",
+        "SvgIconVirtual",
+        "SvgIconSharePrinter",
+        "SvgIconShareBook",
+        "SvgIconShareBook",
+        "SvgIconShareBook",
+    ],
+    to: "http://google.com/title",
+}
+
+export default {
+    title: "BLOCK / Location List Item",
+    component: BlockLocationListItem
+}
+
+const Template = (args, { argTypes }) => ({
+    components: { BlockLocationListItem },
+    props: Object.keys(argTypes),
+    template: `<block-location-list-item v-bind="$props"/>`,
+})
+
+export const Default = () => ({
+    data() {
+        return { ...mock }
+    },
+    template: `
+        <block-location-list-item
+            :title="title"
+            :to="to"
+            :image="image"
+            :day="day"
+            :hour="hour"
+            :address="address"
+            :addressLink="addressLink"
+            :amenities="amenities"
+            :reserveSeat="reserveSeat"
+            :isUclaLibrary="isUclaLibrary"
+        />
+    `,
+})
+
+export const WithControls = Template.bind({})
+WithControls.args = { 
+    ...mock
+}
+
+export const WithControlsAndNoImage = Template.bind({})
+WithControlsAndNoImage.args = { ...mock, image: "" }
