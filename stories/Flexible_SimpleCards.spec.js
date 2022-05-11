575f5493e14b34de358c4065cd1a537bdccf88d2
jendiamond <thejendiamond@gmail.com>
2022-05-11 10:58:47 -0700

    Update flexible simple card spec

--- stories/Flexible_SimpleCards.spec.js
+++ stories/Flexible_SimpleCards.spec.js
@@ -1,8 +1,8 @@
-describe("SimpleCards", () => {
-    it("Default", () => {
-        cy.visit("/iframe.html?id=section-simple-cards--default&args=&viewMode=story")
-        cy.get(".simple-cards").should("exist")
+describe('FLEXIBLE / Simple Cards', () => {
+    it('Default', () => {
+        cy.visit('/iframe.html?id=flexible-simple-cards--default&args=&viewMode=story')
+        cy.get('.simple-cards').should('exist')
 
-        cy.percySnapshot("SimpleCards: Default")
+        cy.percySnapshot('FLEXIBLE / Simple Cards: Default')
     })
 })
