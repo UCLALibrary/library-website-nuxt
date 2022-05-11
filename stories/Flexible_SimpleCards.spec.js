1f91e2b5774347f276d3c89fb39c50aaa60312de
jendiamond <thejendiamond@gmail.com>
2022-05-11 09:15:41 -0700

    Update gql

--- stories/Flexible_SimpleCards.spec.js
+++ stories/Flexible_SimpleCards.spec.js
@@ -1,8 +1,8 @@
-describe('FLEXIBLE / Simple Cards', () => {
-    it('Default', () => {
-        cy.visit('/iframe.html?id=flexible-simple-cards--default&args=&viewMode=story')
-        cy.get('.simple-cards').should('exist')
+describe("SimpleCards", () => {
+    it("Default", () => {
+        cy.visit("/iframe.html?id=section-simple-cards--default&args=&viewMode=story")
+        cy.get(".simple-cards").should("exist")
 
-        cy.percySnapshot('FLEXIBLE / Simple Cards: Default')
+        cy.percySnapshot("SimpleCards: Default")
     })
 })
