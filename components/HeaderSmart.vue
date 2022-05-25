8de4d9e61bc06454140233a43ef19fb170454b3a
Ashton Prigge <aprigge@library.ucla.edu>
2022-05-25 13:19:40 -0700

    wip

---
+++ components/HeaderSmart.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+    <component
+        :is="whichHeader"
+        :class="isMobile ? 'mobile-header' : 'desktop-header'"
+    />
+</template>
+
+<script>
+export default {
+    computed: {
+        isMobile() {
+            return this.$store.state.winWidth <= 1200 ? true : false
+        },
+        whichHeader() {
+            console.log(this.$store.state.winWidth)
+            return this.isMobile ? "header-main-responsive" : "header-main"
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.mobile-header {
+    top: 0;
+    right: 0;
+}
+
+.desktop-header {
+    top: 0;
+    left: 0;
+}
+</style>
