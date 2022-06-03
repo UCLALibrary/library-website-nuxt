ce3f09856e6cdf6668136aad14a2a15d46b65af2
Ashton Prigge <aprigge@library.ucla.edu>
2022-06-03 10:56:40 -0700

    fix: rebsae and update test

--- cypress/integration/locationdetailpage.spec.js
+++ cypress/integration/locationdetailpage.spec.js
@@ -1,10 +1,13 @@
 describe("Location Detail page", () => {
     it("Visit a Location Detail Page", () => {
-        cy.visit("/visit/locations/arts-library")
+        cy.visit("/visit/locations/biomed")
 
         // UCLA Library brand
         cy.get(".logo-ucla").should("be.visible")
-        cy.get("h1.title").should("contain", "Arts Library")
+        cy.get("h1.title").should(
+            "contain",
+            "Louise M. Darling Biomedical Library"
+        )
         cy.percySnapshot({ widths: [768, 992, 1200] })
     })
 })
