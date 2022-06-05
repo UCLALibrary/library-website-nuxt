8c655ff3d8447d97429ecd9ba496c325354ba81a
jendiamond <thejendiamond@gmail.com>
2022-06-05 00:02:42 -0700

    feat: detail page

--- pages/about/news/_slug.vue
+++ pages/about/news/_slug.vue
@@ -1,11 +1,13 @@
-// articleType
+// articleType - nowhere
+// summary - nowhere
+// associatedLocations - nowhere
+// department - nowhere
+
 // title
 // heroImage banner Image
 // articleCategories (library news)
 // staffMember
-// summary - nowhere
-// associatedLocations - nowhere
-// department - nowhere
+
 // allFpb
 // bannerheader
 // date published
@@ -14,8 +16,11 @@
 // pipe instead of purple dot
 // fancy wayfinder divider
 
+// {"entry":{"id":"4416","articleType":"news","title":"I Like Turtles","heroImage":[{"id":"5227","image":[{"id":"5225","src":"https://static.library.ucla.edu/craftassetstest/_fullscreen/I-like-turtles.png","height":2560,"width":2560,"srcset":"https://static.library.ucla.edu/craftassetstest/_375xAUTO_crop_center-center_none/I-like-turtles.png 375w, https://static.library.ucla.edu/craftassetstest/_960xAUTO_crop_center-center_none/I-like-turtles.png 960w, https://static.library.ucla.edu/craftassetstest/_1280xAUTO_crop_center-center_none/I-like-turtles.png 1280w, https://static.library.ucla.edu/craftassetstest/_1920xAUTO_crop_center-center_none/I-like-turtles.png 1920w, https://static.library.ucla.edu/craftassetstest/_2560xAUTO_crop_center-center_none/I-like-turtles.png 2560w","alt":"I like turtles","focalPoint":[0.5,0.5]}]}],"articleCategories":[],"staffMember":[{"id":"4169","nameFirst":"Courtney","nameLast":"Hoffner"}],"associatedLocations":[{"id":"523","title":"Powell Library","uri":"visit/locations/powell-library"}],"department":[{"id":"7248","title":"Information Technology Services","uri":"organization/information-technology-services"}],"description":"<p>A FACE-painted boy who went viral for his bizarre response to a TV reporter's question looks unrecognisable 15 years later.</p>\n<p>Jonathan Ware, 10, shot to fame in 2007 after a clip of him went viral on YouTube.</p>","blocks":[]}}
+
 <template lang="html">
     <section class="page-news-detail">
+        <h2>{{ page }}</h2>
         <nav-breadcrumb :title="page.title" />
         <banner-text
             v-if="!page.heroImage || page.heroImage.length == 0"
@@ -29,8 +34,6 @@
         <banner-header
             v-if="page.heroImage && page.heroImage.length == 1"
             :image="page.heroImage[0].image[0]"
-            :to="parsedButtonTo"
-            :prompt="parsedButtonText"
             :title="page.title"
             :category="page.type"
             :text="page.text"
@@ -40,19 +43,19 @@
             class="divider-way-finder"
         />
 
-        <flexible-blocks
+        <!-- <flexible-blocks
             class="content"
             :blocks="page.blocks"
-        />
+        /> -->
     </section>
 </template>
 
 <script>
-// GQL
-import ARTICLE_NEWS_DETAIL from "~/gql/queries/ArticleNewsDetail"
-
 // Helpers
 import _get from "lodash/get"
+
+// GQL
+import ARTICLE_NEWS_DETAIL from "~/gql/queries/ArticleNewsDetail"
 
 export default {
     async asyncData({ $graphql, params, store }) {
@@ -78,22 +81,16 @@ export default {
         }
     },
     computed: {
-        parsedAssociatedTopics() {
-            return this.page.associatedTopics.map((obj) => {
-                return {
-                    ...obj,
-                    to: obj.researchGuideUrl ? obj.researchGuideUrl : obj.uri,
-                }
-            })
-        },
-        parsedButtonText() {
-            return _get(this.page, "button[0].buttonText", "")
-        },
-        parsedButtonTo() {
-            return _get(this.page, "button[0].buttonUrl", "")
-        },
-    },
+        parsedItems() {
+            return this.page.entry
+            //     to: `/news/${obj.to}`,
+            //     image: _get(obj, "heroImage[0].image[0]", null),
+            //     staffName: `${obj.nameFirst} ${obj.nameLast}`,
+            // }
+        }
+    }
 }
+
 </script>
 
 <style lang="scss" scoped>
