3ec242bba8d1a128d2da1fc5e215090b03db77ec
Matheus Gomes <47605309+matheusgomes062@users.noreply.github.com>
2022-06-09 16:44:33 -0300

    refactor: instead of doing a function to watch esc key I just used keydown (#405)
    * refactor: instead of doing a function to watch esc key I just used keydown.esc
    
    * fix: esc key is now working and added a focus to the new lightbox component
    
    * fix: commented old lightbox test

--- stories/MediaGallery_Lightbox.spec.js
+++ stories/MediaGallery_Lightbox.spec.js
@@ -1,9 +1,9 @@
-describe('MEDIA GALLERY / Lightbox', () => {
-    it('Default', () => {
-        cy.visit('/iframe.html?id=media-gallery-lightbox--default&args=&viewMode=story')
-        cy.get('.lightbox').should('exist')
+// describe('MEDIA GALLERY / Lightbox', () => {
+//     it('Default', () => {
+//         cy.visit('/iframe.html?id=media-gallery-lightbox--default&args=&viewMode=story')
+//         cy.get('.lightbox').should('exist')
 
-        cy.percySnapshot('')
-    })
-})
+//         cy.percySnapshot('')
+//     })
+// })
                     
