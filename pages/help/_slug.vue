a1a62466c238439a17f24cf49e693a380665e705
Andrew Wallace <andrewwallace@library.ucla.edu>
2022-06-10 13:43:07 -0700

    Help Topic Detail Page

---
+++ pages/help/_slug.vue
@@ -0,0 +1,72 @@
+<template lang="html">
+    <main class="page-help-topic">
+        <MastheadSecondary
+            :title="page.title"
+            :text="page.summary"
+        />
+        <RichText
+            v-if="page.richText"
+            class="content"
+            :rich-text-content="page.richText"
+        />
+        <DividerWayFinder color="help" />
+        <flexible-blocks
+            class="content"
+            :blocks="allBlocks"
+        />
+    </main>
+</template>
+
+<script>
+// GQL
+import HELP_TOPIC_DETAIL from "~/gql/queries/HelpTopicDetail"
+
+// Helpers
+import _get from "lodash/get"
+import RichText from "../../components/RichText.vue"
+import DividerGeneral from "../../components/DividerGeneral.vue"
+
+export default {
+    components: { RichText, DividerGeneral },
+    async asyncData({ $graphql, params, store }) {
+        // Do not remove testing live preview
+        const data = await $graphql.default.request(HELP_TOPIC_DETAIL, {
+            slug: params.slug,
+        })
+        return {
+            page: _get(data, "entry", {}),
+        }
+    },
+    head() {
+        let title = this.page ? this.page.title : "... loading"
+        return {
+            title: title,
+        }
+    },
+    computed: {
+        allBlocks() {
+            return this.page.helpTopicBlocks.map(
+                (block) => Object.assign(block, {
+                    typeHandle: 'simpleCards',
+                    title: block.sectionTitle,
+                    text: block.sectionSummary,
+                    cards: block.associatedEntries,
+                })
+            ).concat(this.page.blocks)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.page-help-topic {
+    .content {
+        margin: 0 auto;
+        max-width: $container-l-main + px;
+    }
+
+    .help-topic-block {
+
+    }
+}
+</style>
--- utils/isInternalLink.js
+++ utils/isInternalLink.js
@@ -7,6 +7,10 @@
 function isInternalLink(uri = "") {
     let output = false
 
+    if (uri == null) {
+        return output // don't try string method .includes
+    }
+
     switch (true) {
         case uri.includes("library.ucla.edu") && !uri.includes("mailto:"):
         case String(uri).indexOf("/") === 0:
