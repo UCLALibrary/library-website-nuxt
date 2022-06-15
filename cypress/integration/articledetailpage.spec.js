9560f6b808c8b1ae404b90b4445ff8743edfb36a
JenDiamond <thejendiamond@gmail.com>
2022-06-15 15:27:27 -0700

    feat: assemble article detail page (#408)
    * feat: assemble article detail page
    
    * feat: add news-article list and query
    
    * feat: udate list page - working
    
    * feat: article-list-page working
    
    * feat: work on detail gql
    
    * feat: detail page
    
    * Fix breadcrumb
    
    * feat: fix bannerHeader
    
    * feat: add locations
    
    * feat: fix date format
    
    * feat: fix date
    
    * feat: locations working
    
    * Add spec

---
+++ cypress/integration/articledetailpage.spec.js
@@ -0,0 +1,13 @@
+describe("Article Detail page", () => {
+    it("Visits an Article Detail Page", () => {
+        cy.visit("/about/news/turtles")
+
+        // UCLA Library brand
+        cy.get(".logo-ucla").should("be.visible")
+        cy.get("h1.title").should(
+            "contain",
+            "I Like Turtles"
+        )
+        cy.percySnapshot({ widths: [768, 992, 1200] })
+    })
+})
