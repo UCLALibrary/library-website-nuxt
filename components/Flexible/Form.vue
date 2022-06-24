91adad26dd341d7bde282dca9c06d2b07385aef9
jendiamond <thejendiamond@gmail.com>
2022-06-24 16:09:13 -0700

    feat: add story

--- components/Flexible/Form.vue
+++ components/Flexible/Form.vue
@@ -44,7 +44,6 @@ export default {
                     testiframe.style.height = testiframe.contentDocument.body.scrollHeight + form_iframe.height
                 })
 
-
                 switch (eventName) {
                     case "setHeight":
                         form_iframe.height = testiframe.style.height
@@ -53,17 +52,6 @@ export default {
             },
             false
         )
-    },
-    methods: {
-        resizeIframe(obj) {
-            // TODO why is this not working?
-            console.log(obj.target.contentWindow.document.body.scrollHeight)
-
-            obj.target.style.height = "524px"
-            console.log(obj.target.contentWindow.document.body.scrollHeight)
-            /*obj.style.height =
-                obj.contentWindow.document.body.scrollHeight + "px"*/
-        },
     },
 }
 </script>
--- stories/Flexible_Form.stories.js
+++ stories/Flexible_Form.stories.js
@@ -1,46 +1,20 @@
 // Storybook default settings
+/* eslint-disable */
 export default {
     title: "FLEXIBLE / Form",
 }
 
 const mock = {
-    "id": "20792",
-    "form": '<div id=\"form_95580ed5e8d04c56e4afbf9e91781b28\"></div><script type=\"text/javascript\" src=\"https://ucla.libwizard.com/form_loader.php?id=95580ed5e8d04c56e4afbf9e91781b28&noheader=0\"></script>',
+    form: '<div id=\"form_95580ed5e8d04c56e4afbf9e91781b28\"></div><script type=\"text/javascript\" src=\"https://ucla.libwizard.com/form_loader.php?id=95580ed5e8d04c56e4afbf9e91781b28&noheader=0\"></script>',
 }
-
-// Variations of stories below
-// export const Default = () => ({
-//     data() {
-//         return {
-//             items: {
-//                 ...mock,
-//             }
-//         }
-//     },
-//     template: `<flexible-form :items="items" />`,
-// })
-
 
 export const Default = () => ({
     data() {
         return {
-            item: {
-                ...mock,
-            },
+            block: mock,
         }
     },
     template: `<flexible-form
-        :form = "<div id=\"form_95580ed5e8d04c56e4afbf9e91781b28\"></div><script type=\"text/javascript\" src=\"https://ucla.libwizard.com/form_loader.php?id=95580ed5e8d04c56e4afbf9e91781b28&noheader=0\"></script>"
-    />`,
-})
-
-export const Form2 = () => ({
-    data() {
-        return {
-            ...mock,
-        }
-    },
-    template: `<flexible-form
-    :form = 'form'
+        :block= "block"
     />`,
 })
