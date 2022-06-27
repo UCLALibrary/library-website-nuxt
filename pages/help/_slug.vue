615c24d96689f21cdd9c3e61c31074e0c00551fc
Andy Wallace <andrewwallace@library.ucla.edu>
2022-06-27 11:13:57 -0700

    feat: Help Topic Detail Page (#409)
    * Help Topic Detail Page
    
    * update page to use simple-cards regular component
    
    * add divider at bottom of block
    
    * add cypress test and adjust margin on page
    
    * update internal link logic
    
    * fix: Add / to BlockCardIllustration component so that  nuxt generate will find help topic cards
    
    Co-authored-by: Ashton Prigge <34147754+aprigge@users.noreply.github.com>
    Co-authored-by: Ashton Prigge <aprigge@library.ucla.edu>
    Co-authored-by: Parinita Mulak <pghorpade@library.ucla.edu>

---
+++ pages/help/_slug.vue
@@ -0,0 +1,83 @@
+<template lang="html">
+    <main class="page page-help-topic">
+        <MastheadSecondary
+            :title="page.title"
+            :text="page.summary"
+        />
+        <RichText
+            v-if="page.richText"
+            class="content"
+            :rich-text-content="page.richText"
+        />
+        <DividerWayFinder color="help" />
+        <div
+            v-for="(block, index) in page.helpTopicBlocks"
+            :key="`HelpTopicBlocksKey${index}`"
+            class="help-topic-section"
+        >
+            <simple-cards
+                class="help-topic-block"
+                :section-title="block.sectionTitle"
+                :section-summary="block.sectionSummary"
+                :items="block.associatedEntries"
+            />
+            <DividerWayFinder
+                color="help"
+                class="help-topic-divider"
+            />
+        </div>
+
+        <flexible-blocks
+            class="content"
+            :blocks="page.blocks"
+        />
+    </main>
+</template>
+
+<script>
+// GQL
+import HELP_TOPIC_DETAIL from "~/gql/queries/HelpTopicDetail"
+
+// Helpers
+import _get from "lodash/get"
+import RichText from "../../components/RichText.vue"
+import DividerGeneral from "../../components/DividerGeneral.vue"
+
+export default {
+    components: { RichText, DividerGeneral },
+    async asyncData({ $graphql, params, store }) {
+        // Do not remove testing live preview
+        const data = await $graphql.default.request(HELP_TOPIC_DETAIL, {
+            slug: params.slug,
+        })
+        return {
+            page: _get(data, "entry", {}),
+        }
+    },
+    head() {
+        let title = this.page ? this.page.title : "... loading"
+        return {
+            title: title,
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.page-help-topic {
+    .content {
+        margin: 0 auto;
+        max-width: $container-l-main + px;
+    }
+
+    .help-topic-section {
+        margin: var(--space-2xl) auto;
+    }
+
+    @media #{$medium} {
+        .help-topic-section {
+            padding: 0 var(--unit-gutter);
+        }
+    }
+}
+</style>
