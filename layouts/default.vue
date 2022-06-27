1c40ebf31396530ca65db9ecc5fa684d6c6e8813
Axa Mei Liauw <aliauw@library.ucla.edu>
2022-06-27 17:02:08 -0400

    feat: adds skip link and main landmark (#429)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <template lang="html">
     <div :class="classes">
+        <a href="#main" ref="skipLink" class="skip-link">Skip to main content</a>
+        
         <header-smart />
-
-        <nuxt class="page" />
-
+        <main id="main">
+            <nuxt class="page" />
+        </main>
         <footer-primary :form="true" />
         <footer-sock />
     </div>
@@ -50,6 +52,11 @@ export default {
             ]
         },
     },
+    watch: {
+        $route() {
+            this.$refs.skipLink.focus()
+        }
+    }
     // meta: [
     //     {
     //         hid: "description",
@@ -102,5 +109,22 @@ export default {
     .page {
         flex: 1 1 auto;
     }
+}
+
+.skip-link {
+    position: absolute;
+    transform: translateY(-100%);
+    display: inline-block;
+    background: var(--color-primary-yellow-01);
+    color: var(--color-black);
+    @include step-0;
+    padding: 4px 16px;
+    transition: transform 0.3s;
+    width: auto;
+    left: 0;
+}
+
+.skip-link:focus {
+    transform: translateY(0%);
 }
 </style>
