cf6ef33b9a3eaf41b98a4ec79cace93917176716
Ashton Prigge <34147754+aprigge@users.noreply.github.com>
2022-06-27 17:07:07 -0700

    fix: add help topics to services listing page (#431)

--- pages/help/services-resources/index.vue
+++ pages/help/services-resources/index.vue
@@ -1,7 +1,20 @@
 <template lang="html">
     <div class="page page-help">
+        <h3>Services and Resources</h3>
         <nuxt-link
             v-for="item in parsedServiceAndResourceList"
+            :key="item.to"
+            :to="item.to"
+        >
+            <div
+                class="text"
+                v-html="item.title"
+            />
+        </nuxt-link>
+        <br>
+        <h3>Help Topics</h3>
+        <nuxt-link
+            v-for="item in parsedHelpTopicList"
             :key="item.to"
             :to="item.to"
         >
@@ -16,15 +29,19 @@
 <script>
 // gql
 import SERVICE_AND_RESOURCE_LIST from "~/gql/queries/ServiceAndResourceList"
+import HELP_TOPIC_LIST from "~/gql/queries/HelpTopicList"
 
 export default {
     async asyncData({ $graphql, params }) {
         const data = await $graphql.default.request(SERVICE_AND_RESOURCE_LIST, {
             uri: params.path,
         })
-
+        const helpTopicData = await $graphql.default.request(HELP_TOPIC_LIST, {
+            uri: params.path,
+        })
         return {
             page: data,
+            helpTopic: helpTopicData,
         }
     },
     computed: {
@@ -33,6 +50,14 @@ export default {
                 return {
                     ...obj,
                     to: `/help/services-resources/${obj.to}`,
+                }
+            })
+        },
+        parsedHelpTopicList() {
+            return this.helpTopic.entries.map((obj) => {
+                return {
+                    ...obj,
+                    to: `/help/${obj.to}`,
                 }
             })
         },
