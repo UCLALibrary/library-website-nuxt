941cca2a7443520de7f5ab0b90770453001baa03
Ashton Prigge <aprigge@library.ucla.edu>
2022-06-30 14:23:04 -0700

    comment out local only components

--- pages/test_library/index.vue
+++ pages/test_library/index.vue
@@ -29,7 +29,7 @@
             text="Featured "
             to="/visit/"
         />
-        <banner-text
+        <!-- <banner-text
             category="Featured"
             title="New Exhibition"
             text="This is a New Exhibition"
@@ -54,7 +54,7 @@
             :start-date="bannerFeaturedData.startDate"
             :end-date="bannerFeaturedData.endDate"
             :prompt="bannerFeaturedData.prompt"
-        />
+        /> -->
         <!--block-call-to-action
             svg-name="svg-call-to-action-chat"
             title="test block call to action"
@@ -69,63 +69,63 @@
 <script>
 export default {
     name: "Home",
-    computed: {
-        bannerHeaderData() {
-            return {
-                image: {
-                    src: "https://via.placeholder.com/1920x1080",
-                    srcset: "https://via.placeholder.com/960x540 960w, https://via.placeholder.com/1280x720 1280w, https://via.placeholder.com/1920x1080 1920w",
-                    sizes: "100vw",
-                    alt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                    title: "Lorem ipsum",
-                    caption: "Lorem ipsum",
-                    height: 1080,
-                    width: 1920,
-                },
-                to: "/help/foo/bar/",
-                title: "Curabitur Tortor Pellentesque Nibh Aenean",
-                category: "Lectus",
-                startDate: "1995-12-17T03:24:00",
-                endDate: "1995-12-17T03:24:00",
-                byline: ["Cursus Quis"],
-                locations: [
-                    {
-                        id: "523",
-                        title: "Powell Library",
-                        to: "visit/locations/powell-library",
-                    },
-                    {
-                        id: "3062",
-                        title: "Online",
-                        to: "visit/locations/online",
-                    },
-                ],
-                prompt: "Cursus Quis",
-                alignRight: true,
-            }
-        },
-        bannerFeaturedData() {
-            return {
-                image: {
-                    src: "https://via.placeholder.com/1920x1080",
-                    srcset: "https://via.placeholder.com/960x540 960w, https://via.placeholder.com/1280x720 1280w, https://via.placeholder.com/1920x1080 1920w",
-                    sizes: "100vw",
-                    alt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                    title: "Lorem ipsum",
-                    caption: "Lorem ipsum",
-                    height: 1080,
-                    width: 1920,
-                },
-                to: "/visit/foo/bar/",
-                title: "Sed Lectus Inceptos: Suspendisse in Justo eu Magna Luctus Suscipit",
-                category: "Torquent",
-                breadcrumb: "Torquent",
-                startDate: "1995-12-17T03:24:00",
-                endDate: "1995-12-17T03:24:00",
-                prompt: "Cursus Quis",
-                alignRight: false,
-            }
-        },
-    },
+    // computed: {
+    //     bannerHeaderData() {
+    //         return {
+    //             image: {
+    //                 src: "https://via.placeholder.com/1920x1080",
+    //                 srcset: "https://via.placeholder.com/960x540 960w, https://via.placeholder.com/1280x720 1280w, https://via.placeholder.com/1920x1080 1920w",
+    //                 sizes: "100vw",
+    //                 alt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    //                 title: "Lorem ipsum",
+    //                 caption: "Lorem ipsum",
+    //                 height: 1080,
+    //                 width: 1920,
+    //             },
+    //             to: "/help/foo/bar/",
+    //             title: "Curabitur Tortor Pellentesque Nibh Aenean",
+    //             category: "Lectus",
+    //             startDate: "1995-12-17T03:24:00",
+    //             endDate: "1995-12-17T03:24:00",
+    //             byline: ["Cursus Quis"],
+    //             locations: [
+    //                 {
+    //                     id: "523",
+    //                     title: "Powell Library",
+    //                     to: "visit/locations/powell-library",
+    //                 },
+    //                 {
+    //                     id: "3062",
+    //                     title: "Online",
+    //                     to: "visit/locations/online",
+    //                 },
+    //             ],
+    //             prompt: "Cursus Quis",
+    //             alignRight: true,
+    //         }
+    //     },
+    //     bannerFeaturedData() {
+    //         return {
+    //             image: {
+    //                 src: "https://via.placeholder.com/1920x1080",
+    //                 srcset: "https://via.placeholder.com/960x540 960w, https://via.placeholder.com/1280x720 1280w, https://via.placeholder.com/1920x1080 1920w",
+    //                 sizes: "100vw",
+    //                 alt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    //                 title: "Lorem ipsum",
+    //                 caption: "Lorem ipsum",
+    //                 height: 1080,
+    //                 width: 1920,
+    //             },
+    //             to: "/visit/foo/bar/",
+    //             title: "Sed Lectus Inceptos: Suspendisse in Justo eu Magna Luctus Suscipit",
+    //             category: "Torquent",
+    //             breadcrumb: "Torquent",
+    //             startDate: "1995-12-17T03:24:00",
+    //             endDate: "1995-12-17T03:24:00",
+    //             prompt: "Cursus Quis",
+    //             alignRight: false,
+    //         }
+    //     },
+    // },
 }
 </script>
