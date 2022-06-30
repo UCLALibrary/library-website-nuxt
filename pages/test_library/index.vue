be98db9b0cd5bf09fe992d0eb2b632d249443761
Ashton Prigge <aprigge@library.ucla.edu>
2022-06-30 13:32:47 -0700

    Add Banner components to index.vue

--- pages/test_library/index.vue
+++ pages/test_library/index.vue
@@ -16,10 +16,7 @@
         <br>
         <divider-general />
         <br>
-        <divider-general
-            
-            :is-bold="true"
-        />
+        <divider-general :is-bold="true" />
         <br>
         <alphabetical-browse-by />
         <br>
@@ -31,6 +28,32 @@
         <heading-arrow
             text="Featured "
             to="/visit/"
+        />
+        <banner-text
+            category="Featured"
+            title="New Exhibition"
+            text="This is a New Exhibition"
+            button-text="See More"
+            to="/visit/exhibition"
+        />
+        <banner-header
+            :image="bannerHeaderData.image"
+            :to="bannerHeaderData.to"
+            :title="bannerHeaderData.title"
+            :category="bannerHeaderData.category"
+            :start-date="bannerHeaderData.startDate"
+            :end-date="bannerHeaderData.endDate"
+            :prompt="bannerHeaderData.prompt"
+        />
+        <banner-featured
+            :image="bannerFeaturedData.image"
+            :to="bannerFeaturedData.to"
+            :title="bannerFeaturedData.title"
+            :category="bannerFeaturedData.category"
+            :breadcrumb="bannerFeaturedData.breadcrumb"
+            :start-date="bannerFeaturedData.startDate"
+            :end-date="bannerFeaturedData.endDate"
+            :prompt="bannerFeaturedData.prompt"
         />
         <!--block-call-to-action
             svg-name="svg-call-to-action-chat"
@@ -44,9 +67,65 @@
     </div>
 </template>
 <script>
-
-
 export default {
-    name: 'Home',
+    name: "Home",
+    computed: {
+        bannerHeaderData() {
+            return {
+                image: {
+                    src: "https://via.placeholder.com/1920x1080",
+                    srcset: "https://via.placeholder.com/960x540 960w, https://via.placeholder.com/1280x720 1280w, https://via.placeholder.com/1920x1080 1920w",
+                    sizes: "100vw",
+                    alt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    title: "Lorem ipsum",
+                    caption: "Lorem ipsum",
+                    height: 1080,
+                    width: 1920,
+                },
+                to: "/help/foo/bar/",
+                title: "Curabitur Tortor Pellentesque Nibh Aenean",
+                category: "Lectus",
+                startDate: "1995-12-17T03:24:00",
+                endDate: "1995-12-17T03:24:00",
+                byline: ["Cursus Quis"],
+                locations: [
+                    {
+                        id: "523",
+                        title: "Powell Library",
+                        to: "visit/locations/powell-library",
+                    },
+                    {
+                        id: "3062",
+                        title: "Online",
+                        to: "visit/locations/online",
+                    },
+                ],
+                prompt: "Cursus Quis",
+                alignRight: true,
+            }
+        },
+        bannerFeaturedData() {
+            return {
+                image: {
+                    src: "https://via.placeholder.com/1920x1080",
+                    srcset: "https://via.placeholder.com/960x540 960w, https://via.placeholder.com/1280x720 1280w, https://via.placeholder.com/1920x1080 1920w",
+                    sizes: "100vw",
+                    alt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    title: "Lorem ipsum",
+                    caption: "Lorem ipsum",
+                    height: 1080,
+                    width: 1920,
+                },
+                to: "/visit/foo/bar/",
+                title: "Sed Lectus Inceptos: Suspendisse in Justo eu Magna Luctus Suscipit",
+                category: "Torquent",
+                breadcrumb: "Torquent",
+                startDate: "1995-12-17T03:24:00",
+                endDate: "1995-12-17T03:24:00",
+                prompt: "Cursus Quis",
+                alignRight: false,
+            }
+        },
+    },
 }
 </script>
