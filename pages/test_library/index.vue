5ca766f39a6b0bf335ab220f7ef447e75379175a
Ashton Prigge <aprigge@library.ucla.edu>
2022-07-13 16:10:06 -0700

    add impact report

--- pages/test_library/index.vue
+++ pages/test_library/index.vue
@@ -234,11 +234,34 @@
 
         <h2>Flexible Blocks</h2>
         <flexible-blocks :blocks="flexibleBlocks" />
+
+        <h2>Impact report</h2>
+        <story-with-image
+            :image="mockStoryWithImage.image"
+            :caption="mockStoryWithImage.image.caption"
+            :title="mockStoryWithImage.title"
+            :text="mockStoryWithImage.text"
+            :footnote="mockStoryWithImage.footnote"
+            :calltoaction="mockStoryWithImage.calltoaction"
+        />
+        <hr>
+        <hr>
+
+        <grid-gallery :items="mockGridGallery" />
+        <hr>
+        <hr>
+
+        <impact-rich-text
+            :text-blocks="mockImpactRichText.textBlocks"
+            :pull-quote="mockImpactRichText.pullQuote"
+            :images="mockImpactRichText.images"
+        />
     </div>
 </template>
 
 <script>
 import * as API from "~/stories/mock-api.json"
+import * as IMPACT_API from "~/stories/impact-report_index.json"
 
 export default {
     name: "Home",
@@ -436,6 +459,106 @@ export default {
         }
     },
     computed: {
+        mockStoryWithImage() {
+            return { ...IMPACT_API.story[0] }
+        },
+        mockGridGallery() {
+            return [
+                {
+                    image: API.image,
+                    monthYear: "March 2020",
+                    to: "/visit/foo/bar/",
+                    headlineText: "Vel Quam Elementum",
+                    snippet:
+                        "Vel eros donec ac odio tempor orci dapibus. Ante metus dictum at tempor. ",
+                },
+                {
+                    image: API.image,
+                    monthYear: "March 2020",
+                    to: "/visit/foo/baz/",
+                    headlineText:
+                        "Mauris pellentesque pulvinar pellentesque habitant morbi tristique",
+                    snippet:
+                        "Ante metus dictum at tempor. Pretium nibh ipsum consequat nisl vel pretium. Amet consectetur adipiscing elit ut aliquam purus sit. Diam quis enim lobortis scelerisque fermentum dui faucibus. Hac habitasse platea dictumst quisque.",
+                },
+                {
+                    image: API.image,
+                    monthYear: "March 2020",
+                    to: "/visit/foo/bat/",
+                    headlineText: "Adipiscing Tristique",
+                    snippet:
+                        "Vel eros donec ac odio  nisl vel pretium. Amet consectetur adipiscing elit ut aliquam purus sit. ",
+                },
+                {
+                    image: API.image,
+                    monthYear: "March 2020",
+                    to: "/visit/foo/ban/",
+                    headlineText:
+                        "Fames ac turpis egestas sed tempus lorem ipsum",
+                    snippet: "Vel eros donec ac .",
+                },
+                {
+                    image: API.image,
+                    monthYear: "March 2020",
+                    to: "/visit/foo/barf/",
+                    headlineText: "Vel Quam Elementum",
+                },
+                {
+                    image: API.image,
+                    to: "/visit/foo/bad/",
+                    headlineText: "Aenean Lectus Elit",
+                    snippet:
+                        "Vel eros donec ac odio tempor orci dapibus. Ante metus dictum at tempor. Pretium nibh ipsum consequat nisl vel pretium. Amet consectetur adipiscing elit ut aliquam purus sit. Diam quis enim lobortis scelerisque fermentum dui faucibus. Hac habitasse platea dictumst quisque.",
+                },
+            ]
+        },
+        mockImpactRichText() {
+            return {
+                textBlocks: [
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nulla urna, euismod at sagittis ac, aliquam ut sem. Phasellus varius lacinia nisl nec fermentum. Sed dignissim metus quis consequat pretium. In hac habitasse platea dictumst. Ut vel dignissim eros. Aliquam lacus tellus, molestie a nisi non, suscipit consectetur dolor. Donec sit amet venenatis ante. Nullam blandit rhoncus massa, ac consequat ante egestas in. Pellentesque lobortis sed ex ac viverra. Proin et semper mauris. Pellentesque pulvinar cursus ipsum sit amet scelerisque. Praesent mollis fringilla est, quis rutrum sapien viverra eu.<br><br> Sed vitae lectus tristique, consequat mauris sed, commodo metus. Sed sit amet turpis urna. Donec nisl felis, imperdiet a nisi et, commodo imperdiet magna. Phasellus sed nisi a nulla vehicula volutpat. Nulla est justo, gravida et dui laoreet, viverra suscipit odio. Sed blandit leo elit, at eleifend neque facilisis in. Fusce congue dapibus vestibulum. Aliquam eget erat sed magna elementum maximus vitae ac quam. Cras massa tellus, pulvinar non nibh a, gravida vulputate ante. Pellentesque pharetra luctus turpis, et rutrum leo. Pellentesque commodo, sem id placerat mattis, elit dui vehicula ipsum, in vulputate dui augue a metus. Proin nec mi ut orci laoreet gravida id non leo. Vestibulum sodales tincidunt libero, vitae bibendum urna gravida ullamcorper. Morbi sagittis id justo nec condimentum. Aenean at ex nec sapien semper viverra eget sed erat.",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nulla urna, euismod at sagittis ac, aliquam ut sem. Phasellus varius lacinia nisl nec fermentum. Sed dignissim metus quis consequat pretium. In hac habitasse platea dictumst. .<br><br>Ut vel dignissim eros. Aliquam lacus tellus, molestie a nisi non, suscipit consectetur dolor. Donec sit amet venenatis ante. Nullam blandit rhoncus massa, ac consequat ante egestas in. Pellentesque lobortis sed ex ac viverra. Proin et semper mauris. Pellentesque pulvinar cursus ipsum sit amet scelerisque. Praesent mollis fringilla est, quis rutrum sapien viverra eu. Sed vitae lectus tristique, consequat mauris sed, commodo metus. Sed sit amet turpis urna. Donec nisl felis, imperdiet a nisi et, commodo imperdiet magna. Phasellus sed nisi a nulla vehicula volutpat. Nulla est justo, gravida et dui laoreet, viverra suscipit odio. Sed blandit leo elit, at eleifend neque facilisis in. Donec accumsan, metus in aliquet venenatis, mi lectus placerat leo, congue gravida mi quam sit amet neque.",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan, metus in aliquet venenatis, mi lectus placerat leo, congue gravida mi quam sit amet neque.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan, metus in aliquet venenatis, mi lectus placerat leo, congue gravida mi quam sit amet neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan, metus in aliquet venenatis, mi lectus placerat leo, congue gravida mi quam sit amet neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan, metus in aliquet venenatis, mi lectus placerat leo, congue gravida mi quam sit amet neque.",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan, metus in aliquet venenatis, mi lectus placerat leo, congue gravida mi quam sit amet neque.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan, metus in aliquet venenatis, mi lectus placerat leo, congue gravida mi quam sit amet neque.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan, metus in aliquet venenatis, mi lectus placerat leo, congue gravida mi quam sit amet neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan, metus in aliquet venenatis, mi lectus placerat leo, congue gravida mi quam sit amet neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan, metus in aliquet venenatis, mi lectus placerat leo, congue gravida mi quam sit amet neque.",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan, metus in aliquet venenatis, mi lectus placerat leo, congue gravida mi quam sit amet neque.",
+                ],
+                images: [
+                    {
+                        src: "https://static.library.ucla.edu/remote-vpn-setup-ucla.jpg",
+                        sizes: "100vw",
+                        alt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                        caption:
+                            "Device loans from the Campus Library Instructional Computing Commons (CLICC) provided a lifeline for many Bruins during remote teaching and learning. CLICC coordinated nearly 3,000 long-term loans of laptops, Chrombooks, iPads, and MiFi units, picked up on campus and shipped to users’ residences, between March 2020 and September 2021.",
+                        height: 1080,
+                        width: 1920,
+                    },
+                    {
+                        src: "https://static.library.ucla.edu/remote-vpn-setup-ucla.jpg",
+                        sizes: "100vw",
+                        alt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                        caption:
+                            "Device loans from the Campus Library Instructional Computing Commons (CLICC) provided a lifeline for many Bruins during remote teaching and learning. CLICC coordinated nearly 3,000 long-term loans of laptops, Chrombooks, iPads, and MiFi units, picked up on campus and shipped to users’ residences, between March 2020 and September 2021.",
+                        height: 1080,
+                        width: 1920,
+                    },
+                    {
+                        src: "https://static.library.ucla.edu/remote-vpn-setup-ucla.jpg",
+                        sizes: "100vw",
+                        alt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                        caption:
+                            "Device loans from the Campus Library Instructional Computing Commons (CLICC) provided a lifeline for many Bruins during remote teaching and learning. CLICC coordinated nearly 3,000 long-term loans of laptops, Chrombooks, iPads, and MiFi units, picked up on campus and shipped to users’ residences, between March 2020 and September 2021.",
+                        height: 1080,
+                        width: 1920,
+                    },
+                ],
+
+                pullQuote: [
+                    {
+                        text: "Vestibulum sodales tincidunt libero, vitae bibendum urna gravida ullamcorper. Morbi sagittis id justo nec condimentum. Aenean at ex nec sapien semper viverra eget sed erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nulla urna, euismod at sagittis ac, aliquam ut sem. Phasellus varius lacinia nisl nec fermentum.",
+                        attribution: "Ashton Prigge",
+                    },
+                ],
+            }
+        },
         flexibleBlocks() {
             return [
                 {
