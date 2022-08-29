1f938c6dba82751a5014a8dbe93c850ac7ae77e8
dianneweinthal <44713143+dianneweinthal@users.noreply.github.com>
2022-08-29 15:23:17 -0700

    feat: adds favicon (#58)
    * feat: adds favicon
    
    * fix: updates indentation
    
    * fix: updates indentation
    
    * fix: updates indentation

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,10 @@ export default {
         meta: [
             { charset: "utf-8" },
             { name: "viewport", content: "width=device-width, initial-scale=1" },
-        ]
+        ],
+        link: [
+            { rel: 'icon', type: 'image/svg', href: '/icon-ucla-favicon.svg' }
+        ],
     },
     /*
      ** Global CSS
