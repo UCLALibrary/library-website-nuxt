52f171ba42d0a0ec5a4c4e9dee55f8e4c0861b6e
pghorpade <pghorpade@library.ucla.edu>
2022-09-08 16:14:03 -0700

    fix: trying preview url

--- cypress/integration/GeneralContent.spec.js
+++ cypress/integration/GeneralContent.spec.js
@@ -1,13 +1,13 @@
 describe("General Content page", () => {
     it("Visits a General Content Page", () => {
-        cy.visit("/privacy-terms-of-use")
+        cy.visit("/privacy-terms-of-use?preview=true")
         cy.get(".logo-ucla").should("be.visible")
         cy.get("h1.title").should("contain", "Privacy & Terms of Use")
         cy.percySnapshot({ widths: [768, 992, 1200] })
     })
 
     it("Visits a Nested General Content Page", () => {
-        cy.visit("/accessibility/emergency")
+        cy.visit("/accessibility/emergency?preview=true")
         cy.get(".logo-ucla").should("be.visible")
         cy.get("h1.title").should("contain", "Emergency")
         cy.percySnapshot({ widths: [768, 992, 1200] })
