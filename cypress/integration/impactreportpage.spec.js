06afdc041bd52e04045c3f4c8f257b05f7e18b3e
Parinita Mulak <pghorpade@library.ucla.edu>
2022-09-23 09:19:58 -0700

    feat: APPS-902 impact report (#485)
    * feat: initial commit
    
    * feat: add craft data  to title, letter, portrait and flexible page blocks
    
    * fix: Add block fragment for gridgallerycards flexible blocks to impact report FBP query
    
    * test: fix impact report test
    
    * fix: package-lock.json merge conflict
    
    * fix: update vue component library package version
    
    * fix: remove icon from query

---
+++ cypress/integration/impactreportpage.spec.js
@@ -0,0 +1,34 @@
+describe('Impact Report Page', () => {
+    it('Visit the Impact Report Page', () => {
+        cy.visit('/impact')
+
+        // UCLA Library brand
+        cy.get('.logo-ucla').should('be.visible')
+
+
+        // Ginny's Letter
+        cy.get('h1')
+            .should('have.class', 'intro')
+        // cy.get('.signature').should('contain','Virginia Steel')
+
+        // // Main story/ banner data
+        // cy.get('.banner').should('contain','Read full story')
+        // cy.get('.media-video').should('be.visible')
+
+        // // featurettes
+        // cy.get('.block-highlight').should('have.length','9')
+            
+        // cy.get('.divider').should('have.length','2')
+       
+        // cy.get('h2.title')
+        //     .should('contain', '2020-21: An Academic Year Like No Other')
+        // cy.get('.sub-section-grid')
+        //     .should('have.descendants','.grid-gallery-subtitle')
+        //     .and('have.descendants','.grid-gallery')
+        // cy.get('.grid-gallery').find('img').last().should('have.attr','src','https://static.library.ucla.edu/ucla-library_nakatani-bookplate-crop.jpg')
+        // cy.get('.credits').should('be.visible')
+
+        cy.percySnapshot({ widths: [768, 992, 1200] })
+
+    })
+})
