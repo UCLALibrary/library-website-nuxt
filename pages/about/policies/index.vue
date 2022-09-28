f8a8f545b2610df907298195ac6d053f7ff86733
jendiamond <thejendiamond@gmail.com>
2022-09-28 11:12:00 -0700

    feat: check data

--- pages/about/policies/index.vue
+++ pages/about/policies/index.vue
@@ -1,25 +1,24 @@
 <template>
-    <main class="page page-news">
-        <h2>{{ page }}</h2>
-        <!-- <ul>
-            <li
-                v-for="(item, index) in page.entries"
-                :key="`impact-${index}`"
-            >
-                <nuxt-link :to="`/${item.to}`">
-                    {{ item.title }}
-                </nuxt-link>
-            </li>
-        </ul> -->
+    <main class="page page-policies">
+        <section-wrapper
+            v-if="page.entries"
+            class="section"
+        >
+            <ul class="policies">
+                <li
+                    v-for="(item, index) in parsedPolicies"
+                    :key="`impact-${index}`"
+                >
+                    <nuxt-link :to="item.to">
+                        {{ item.title }}
+                    </nuxt-link>
+                </li>
+            </ul>
+        </section-wrapper>
     </main>
 </template>
 
 <script>
-// Helpers
-// Helpers
-import _get from "lodash/get"
-import format from "date-fns/format"
-
 // GQL
 import POLICIES_LIST from "~/gql/queries/PoliciesList"
 
@@ -33,31 +32,22 @@ export default {
             page: data,
         }
     },
+    computed: {
+        parsedPolicies() {
+            return this.page.entries.map((obj) => {
+                return {
+                    ...obj,
+                    to: `/about/policies/${obj.to}`,
+                }
+            })
+        },
+    },
 }
 </script>
 
 <style lang="scss" scoped>
-.page-news {
-    padding-left: 50px;
-
-    .entry-count {
-        @include step-2;
-        color: var(--color-primary-blue-03);
-        margin: var(--space-m);
-    }
-
-    .section-heading {
-        @include step-2;
-        color: var(--color-primary-blue-03);
-        margin-bottom: var(--space-m);
-    }
-
-    .all-news-heading {
-        @include step-1;
-        color: var(--color-primary-blue-03);
-    }
-
-    .news-item-link {
+.page-policies {
+    .policies {
         list-style: none;
         display: flex;
         justify-content: space-between;
