c96096243a6d5b024c6f8dd353f1d489d5e0f7c9
Parinita Mulak <pghorpade@library.ucla.edu>
2022-09-28 11:54:13 -0700

    feat: Test elastic search api key (#487)
    * initial commit
    
    * feature: Sample elastic search queries
    
    * fix: add env variables
    
    * fix: lint errors
    
    * chore: fix .env example file
    
    * ci: Add ES secrets
    
    * ci: add es secrets to percy workflow
    
    * ci: Add ES keys to on publish workflow

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template lang="html">
     <div class="page page-home">
         <masthead-primary />
+        <!-- TODO elastis search testing -->
+        <!--h4>Mapping:</h4>
+        <p>{{ mapping }}</p>
+        <h4>Search Responsee</h4>
+        <p>{{ searchResponse }}</p-->
         <divider-way-finder
             class="divider divider-first"
             color="help"
@@ -84,9 +89,13 @@ import * as MOCK_API from "~/data/mock-api.json"
 
 export default {
     components: {},
-    async asyncData() {
+    async asyncData({ $dataApi }) {
         //const data = await this.$graphql(QUERY);
 
+        const mapping = await $dataApi.getMapping()
+        // console.log(JSON.stringify(mapping))
+        const searchResponse = await $dataApi.keywordSearch("*")
+        // console.log("Search Response: " + JSON.stringify(searchResponse))
         const mockCard = {
             to: "/help/foo/bar/",
             title: "Example Service",
@@ -152,6 +161,8 @@ export default {
 
         return {
             page: data,
+            mapping: mapping,
+            searchResponse: searchResponse,
         }
     },
     computed: {
