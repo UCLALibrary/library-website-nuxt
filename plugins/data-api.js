89f97a412a3e2db48011cfb7756c816cba1964c4
Ashton Prigge <aprigge@library.ucla.edu>
2022-10-04 12:24:03 -0700

    rebase

--- plugins/data-api.js
+++ plugins/data-api.js
@@ -1,9 +1,9 @@
-export default function({$config}, inject) {
+export default function ({ $config }, inject) {
     /*
     {"id":"j6tUj4IBzYVXfPB9-JvU","name":"dev-es-key","api_key":"N_f0_5WSSae3QOvm4hlq0g","encoded":"ajZ0VWo0SUJ6WVZYZlBCOS1KdlU6Tl9mMF81V1NTYWUzUU92bTRobHEwZw=="}
     */
 
-    async function keywordSearch(keyword){
+    async function keywordSearch(keyword) {
         //var data_url = new URL(`${ES_URL}/apps-dev-library-website/_search`)
         if($config.esApiKey === "" || !$config.esURL === "") return
         console.log("keyword:"+keyword)
@@ -14,36 +14,39 @@ export default function({$config}, inject) {
         //         }
         //     }
         // }
-        
+
         // const urlParams = new URLSearchParams(params).toString()
         // console.log("paranaters: "+urlParams)
         // `${ES_URL}/apps-dev-library-website/_search?q=*:*` GET request
-        const response = await fetch(`${$config.esURL}/apps-dev-library-website/_search`, {
-            headers: {
-                'Authorization': `ApiKey ${$config.esApiKey}`,
-                'Content-Type': 'application/json',
-            },
-            method: 'POST',
-            body: JSON.stringify({
-                /* "query": {
+        const response = await fetch(
+            `${$config.esURL}/apps-dev-library-website/_search`,
+            {
+                headers: {
+                    Authorization: `ApiKey ${$config.esApiKey}`,
+                    "Content-Type": "application/json",
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    /* "query": {
                     "match_all": {}
                 } */
-                "query": {
-                    "query_string" : {
-                        "query" : "*:*"
-                    }
-                }
-            })
-        })
+                    query: {
+                        query_string: {
+                            query: "*:*",
+                        },
+                    },
+                }),
+            }
+        )
         const data = await response.json()
         return data
     }
-    inject('dataApi', {
+    inject("dataApi", {
         getMapping,
         keywordSearch,
-        
     })
 
+<<<<<<< HEAD
 
     async function getMapping(){
         if($config.esApiKey === "" || !$config.esURL === "") return
@@ -53,6 +56,18 @@ export default function({$config}, inject) {
                 // 'Content-Type': 'application/x-www-form-urlencoded',
             },
         })
+=======
+    async function getMapping() {
+        const response = await fetch(
+            `${$config.esURL}/apps-dev-library-website/_mapping`,
+            {
+                headers: {
+                    Authorization: `ApiKey ${$config.esApiKey}`,
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            }
+        )
+>>>>>>> fa12a4c (wip)
         const data = await response.json()
         return data
     }
