16a44e4fe3fc423cca240b14ecf6f24de9874407
matheusgomes062 <matheusgomes062@gmail.com>
2022-10-27 15:50:55 -0300

    fix: fixed graphql data and commented code

--- pages/visit/events-exhibits/_slug.vue
+++ pages/visit/events-exhibits/_slug.vue
@@ -17,11 +17,6 @@
         />
 
         <!-- if theres an image -->
-        <divider-general
-            v-if="page.image[0].image[0]" 
-            class="section divider divider-general"
-        />
-
         <section-wrapper
             v-if="page.image[0].image[0]"
             class="section-banner"
@@ -39,37 +34,40 @@
         </p> -->
         
         <section-wrapper theme="divider">
-            <divider-general />
+            <divider-way-finder
+                v-if="page.image[0].image[0]"
+                color="visit"
+            />
         </section-wrapper>
 
         <section-wrapper
-            v-if="mockRichText || mockRichText.eventDescription"
+            v-if="page || page.eventDescription"
         >
             <rich-text
                 :rich-text-content="page.eventDescription"
             />
         </section-wrapper>
 
-        <section-wrapper theme="divider">
+        <!-- <section-wrapper theme="divider">
             <divider-general />
-        </section-wrapper>
+        </section-wrapper> -->
         
 
         <section-wrapper 
+            theme="gray"
             section-title="More Upcoming Events"
         >
             <!-- TODO List of events go here -->
             <section-teaser-list
-                theme="gray"
                 :items="listEvents"
                 class="section section-list"
             />
         </section-wrapper>
 
 
-        <section-wrapper theme="divider">
+        <!-- <section-wrapper theme="divider">
             <divider-general />
-        </section-wrapper>
+        </section-wrapper> -->
 
         <!-- <section-wrapper> -->
         <block-call-to-action
@@ -107,7 +105,6 @@ export default {
     },
     async asyncData({ $graphql }) {
         const data = await $graphql.default.request(EVENT_DETAIL)
-        console.log("Data fetched: " + JSON.stringify(data))
         return {
             page: _get(data, "entry", {}),
         }
@@ -134,11 +131,6 @@ export default {
                 buttonText: "Curabitur",
                 byline: ["Cursus Quis", "Charles E. Young"],
                 to: "/visit/foo/bar/",
-            },
-            mockRichText: {
-                richText:
-                    '<h3>Here is a title</h3><figure class="figure image-right"><img src="https://test-craft.library.ucla.edu/assets/images/zooland.jpeg" alt="ZooLand" /><figcaption>Zoo</figcaption></figure><p>Could you do an actual logo instead of a font I cant pay you? Can we try some other colors maybe? I cant pay you. You might wanna give it another shot, so make it pop and this is just a 5 minutes job the target audience makes and families aged zero and up will royalties in the company do instead of cash.</p>\n<p>Jazz it up a little I was wondering if my cat could be placed over the logo in the flyer I have printed it out, but the animated gif is not moving I have printed it out, but the animated gif is not moving make it original. Can you make it stand out more? Make it original.</p>\n<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><figure><iframe width="560" height="315" src="https://www.youtube.com/embed/wn7s7podZWI" title="YouTube video player" frameborder="0"></iframe></figure><h2>\n The Outer Space Treaty\n</h2>\n<p>\n Hearts of the stars something incredible is waiting to be known corpus callosum rich in mystery two ghostly white figures in coveralls and helmets are soflty dancing a very small stage in a vast cosmic arena. Are creatures of the cosmos across the centuries courage of our questions inconspicuous motes of rock and gas hundreds of thousands Orion\'s sword.\n</p>\n<blockquote class="twitter-tweet">\n <p lang="en" dir="ltr" xml:lang="en">\n Crew Dragon docked to the <a href="https://twitter.com/Space_Station?ref_src=twsrc%5Etfw">@Space_Station</a> <a href="https://t.co/JG5e2XjZ4n">pic.twitter.com/JG5e2XjZ4n</a>\n </p>\n — SpaceX (@SpaceX) <a href="https://twitter.com/SpaceX/status/1278799363804811264?ref_src=twsrc%5Etfw">July 2, 2020</a>\n </blockquote>\n <p>\n Star stuff harvesting star light kindling the energy hidden in matter a mote of dust suspended in a sunbeam bits of moving fluff another world brain is the seed of intelligence. Take root and flourish kindling the energy hidden in matter the carbon in our apple pies kindling the energy hidden in matter citizens of distant epochs bits of moving fluff.\n</p>\n<p>Could you do an actual logo instead of a font I cant pay you? Can we try some other colors maybe? I cant pay you. You might wanna give it another shot, so make it pop and this is just a 5 minutes job the target audience makes and families aged zero and up will royalties in the company do instead of cash.</p>\n<p>Jazz it up a little I was wondering if my cat could be placed over the logo in the flyer I have printed it out, but the animated gif is not moving I have printed it out, but the animated gif is not moving make it original. Can you make it stand out more? Make it original.</p>\n<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></figure>',
-
             },
             listEvents: [
                 {
@@ -220,26 +212,6 @@ export default {
             ]
         }
     },
-    // async fetch() {
-    //     console.log("In fetch start")
-    //     const navData = await this.$graphql.default.request(
-    //         EVENT_DETAIL
-    //     )
-    //     console.log(navData)
-    //     // sample event id = 9383207
-    //     this.primaryItems = _get(navData, "primary", [])
-    //     this.secondaryItems = _get(navData, "secondary", [])
-    //     console.log("params " + this.$route.params.slug)
-    //     const data = await this.$axios.$get(
-    //         `/1.1/events/${this.$route.params.slug}`
-    //     )
-    //     // TODO get event data from Craft
-    //     // return {
-    //     //     page: {},
-    //     // }
-    //     this.allEvents = [...this.allEvents, ...data.events]
-    //     console.log(data.events)
-    // },
     computed: {
         // listEvents() {
         //     const items = this.parsedEvents.slice(2)
@@ -281,15 +253,6 @@ export default {
         //         }
         //     })
         // },
-    },
-    created() {
-        console.log("AAAAAAAAAAA")
-        this.libcalData = this.$graphql.default.request(
-            EVENT_DETAIL
-        )
-
-        // const events = libcalData.events
-        console.log("LIBCAL: " + this.libcalData.data)
     },
 }
 </script>
