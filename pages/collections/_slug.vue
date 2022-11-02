5701c47eecb441f9bda78798bfe7b6f719ce9a75
jendiamond <thejendiamond@gmail.com>
2022-11-01 21:33:41 -0700

    feat: parsedDonor

--- pages/collections/_slug.vue
+++ pages/collections/_slug.vue
@@ -5,8 +5,8 @@
             :title="page.title"
             parent-title="Collections"
         />
-        <!-- <h3>endowment: {{ page.endowment }}</h3> -->
-        <h3>parsedEndowments: {{ parsedEndowments }}</h3>
+        <h3>page.associatedStaffMember: {{ page.associatedStaffMember }}</h3>
+        <h3>parsedDonors: {{ parsedDonors }}</h3>
 
         <banner-text
             v-if="!page.heroImage || page.heroImage.length == 0"
@@ -40,6 +40,25 @@
             />
         </section-wrapper>
 
+        <!-- Using the Collection -->
+        <section-wrapper
+            v-if="page.resourceServiceWorkshop.length"
+            class=""
+        >
+            <div class="section-header">
+                <simple-cards
+                    section-title="Using the Collection"
+                    :items="parsedServicesAndResources"
+                />
+            </div>
+
+            <divider-way-finder
+                class="divider"
+                color="default"
+            />
+        </section-wrapper>
+
+        <!-- Flexible Page Blocks -->
         <flexible-blocks
             class="flexible-content"
             :blocks="page.blocks"
@@ -81,7 +100,7 @@
 
         <!-- Subject Specialist -->
         <section-wrapper v-if="parsedAssociatedStaffMember.length > 0">
-            <h2 class="section-title">
+            <h2 class="section-title-page">
                 Contact a Subject Specialist
             </h2>
 
@@ -123,13 +142,6 @@ export default {
     },
     computed: {
         parsedPhysicalDigital() {
-            // if (this.page.physicalDigital.length == 1){
-            //     return this.page.physicalDigital[0]
-            // } else  if (this.page.physicalDigital.length > 1){
-            //     return `${this.page.physicalDigital[0]} & ${this.page.physicalDigital[1]}`
-            // } else {
-            //     return ""
-            // }
             return this.page.physicalDigital.length == 1 ?
                 this.page.physicalDigital[0] :
                 `${this.page.physicalDigital[0]} & ${this.page.physicalDigital[1]}`
@@ -140,19 +152,19 @@ export default {
         parsedButtonTo() {
             return _get(this.page, "buttonUrl[0].buttonUrl", "")
         },
-        parsedCollection() {
-            return this.associatedCollection.map((obj) => {
-                return {
-                    ...obj,
-                    to: `/about/${obj.articleType}/${obj.to}`,
-                    image: _get(obj, "heroImage[0].image[0]", {}),
-                    category: _get(obj, "category", ""),
-                    title: _get(obj, "title", ""),
-                    text: _get(obj, "description", ""),
-                    startDate: _get(obj, "startDate", "")
-                }
-            })
-        },
+        // parsedCollection() {
+        //     return this.page.associatedCollection.map((obj) => {
+        //         return {
+        //             ...obj,
+        //             to: `/about/${obj.articleType}/${obj.to}`,
+        //             image: _get(obj, "heroImage[0].image[0]", {}),
+        //             category: _get(obj, "category", ""),
+        //             title: _get(obj, "title", ""),
+        //             text: _get(obj, "description", ""),
+        //             startDate: _get(obj, "startDate", "")
+        //         }
+        //     })
+        // },
         parsedServicesAndResources() {
             let services = this.page.resourceServiceWorkshop
             return services.map((obj) => {
@@ -164,19 +176,33 @@ export default {
                 }
             })
         },
-        parsedEndowments() {
-            let endowment = this.page.endowment
-            return endowment.map((obj) => {
-                return {
-                    ...obj,
-                    to: `${obj.uri}`,
-                    image: _get(obj, "image[0]", null),
-                    title: _get(obj, "title", ""),
-                    description: _get(obj, "summary", ""),
-                    category: _get(obj, "donors.firstname", null),
-                }
+        parsedDonors() {
+            let donors = this.page.endowment[0].donors
+            let donorNames = []
+            donors.map((donor) => {
+                donorNames.push(`${donor.firstName} ${donor.lastName}`)
             })
+            if (donorNames.length == 1) {
+                return `Donor: ${donorNames[0]}`
+            } else {
+                let names = [donorNames.slice(0, -1).join(', '), donorNames.slice(-1)[0]].join(donorNames.length < 2 ? '' : ' and ')
+                return `Donors: ${names}`
+            }
         },
+        // TODO fix this
+        // parsedEndowments() {
+        //     let endowment = this.page.endowment
+        //     let parsedCategory = this.parsedDonors
+        //     return endowment.map((item) => {
+        //         return {
+        //             to: `${item.uri}`,
+        //             image: _get(item, "image[0]", null),
+        //             title: item.endowment[0].title,
+        //             description: _get(item, "summary", ""),
+        //             category: parsedCategory
+        //         }
+        //     })
+        // },
         parsedAssociatedStaffMember() {
             return this.page.associatedStaffMember.map((obj) => {
                 return {
@@ -202,9 +228,10 @@ export default {
         margin: var(--space-xl) auto;
         max-width: $container-l-main + px;
     }
-    .section-title {
+    .section-title-page {
         @include step-4;
         color: var(--color-primary-blue-03);
+        color: green;
     }
 }
 </style>
