a9f4d0f2ad0df1c4465a00727e084f2972894fe7
Ashton Prigge <aprigge@library.ucla.edu>
2022-11-02 18:46:24 -0700

    rebase

--- pages/search_site/index.vue
+++ pages/search_site/index.vue
@@ -1,13 +1,30 @@
 <template lang="html">
     <div>
-        <search-generic />
+        <search-generic search-generic-query="service" />
         <div
             v-for="(result, index) in searchResults"
             :key="`SearchResult${index}`"
         >
-            {{ result }}
+            {{ result._source }}
+            {{ result._source.title }}
+            {{ result._source.summary }}
+            {{ result._source.uri }}
+            {{ result._source.sectionHandle }}
+
             <divider-general />
         </div>
+        <section-wrapper
+            v-for="(result, index) in searchResults"
+            :key="`SearchResultBlock${index}`"
+        >
+            <search-result
+                :title="result._source.title"
+                :category="result._source.sectionHandle"
+                :summary="result._source.summary"
+                to="/"
+            />
+            <divider-general />
+        </section-wrapper>
         <section-wrapper>
             <divider-way-finder />
         </section-wrapper>
@@ -20,7 +37,7 @@ import _get from "lodash/get"
 
 export default {
     async asyncData({ $dataApi }) {
-        const searchResponse = await $dataApi.keywordSearch("*")
+        const searchResponse = await $dataApi.siteSearch()
         const searchJson = JSON.stringify(searchResponse)
         const searchObject = JSON.parse(searchJson)
         return { searchResults: _get(searchObject, "hits.hits", []) }
--- plugins/data-api.js
+++ plugins/data-api.js
@@ -3,52 +3,13 @@ export default function ({ $config }, inject) {
     {"id":"j6tUj4IBzYVXfPB9-JvU","name":"dev-es-key","api_key":"N_f0_5WSSae3QOvm4hlq0g","encoded":"ajZ0VWo0SUJ6WVZYZlBCOS1KdlU6Tl9mMF81V1NTYWUzUU92bTRobHEwZw=="}
     */
 
-<<<<<<< HEAD
-    async function siteSearch(keyword="*:*"){
-        //var data_url = new URL(`${ES_URL}/apps-dev-library-website/_search`)
-        if($config.esApiKey === "" || !$config.esURL === "") return
-        console.log("keyword:"+keyword)
-    
-        const response = await fetch(`${$config.esURL}/apps-craft-test/_search`, {
-            headers: {
-                'Authorization': `ApiKey ${$config.esApiKey}`,
-                'Content-Type': 'application/json',
-            },
-            method: 'POST',
-            body: JSON.stringify({
-                "query": {
-                    "query_string" : {
-                        "query" : keyword,
-                        "fields": [
-                            "*",
-                            "title^4",
-                            "summary^3",
-                            "text^3",
-                            "richText^2"
-                        ],
-                        "fuzziness":"auto"
-                    }
-                }
-            })
-        })
-=======
-    async function keywordSearch(keyword) {
+    async function siteSearch(keyword = "*:*") {
         //var data_url = new URL(`${ES_URL}/apps-dev-library-website/_search`)
         if ($config.esApiKey === "" || !$config.esURL === "") return
         console.log("keyword:" + keyword)
-        // var params = {
-        //     query: {
-        //         query_string: {
-        //             query: "*"
-        //         }
-        //     }
-        // }
 
-        // const urlParams = new URLSearchParams(params).toString()
-        // console.log("paranaters: "+urlParams)
-        // `${ES_URL}/apps-dev-library-website/_search?q=*:*` GET request
         const response = await fetch(
-            `${$config.esURL}/apps-dev-library-website/_search`,
+            `${$config.esURL}/apps-craft-test/_search`,
             {
                 headers: {
                     Authorization: `ApiKey ${$config.esApiKey}`,
@@ -56,75 +17,71 @@ export default function ({ $config }, inject) {
                 },
                 method: "POST",
                 body: JSON.stringify({
-                    /* "query": {
-                    "match_all": {}
-                } */
                     query: {
                         query_string: {
-                            query: "*:*",
+                            query: keyword,
+                            fields: [
+                                "*",
+                                "title^4",
+                                "summary^3",
+                                "text^3",
+                                "richText^2",
+                            ],
+                            fuzziness: "auto",
                         },
                     },
                 }),
             }
         )
->>>>>>> dc7923b (add search-site route)
         const data = await response.json()
-        console.log(data)
         return data
     }
-<<<<<<< HEAD
 
+<<<<<<< HEAD
     async function keywordSearchWithFilters(keyword="*:*", sectionHandle, filters, sort, source=["*"], aggFields=[]){
+=======
+    async function keywordSearchWithFilters(
+        keyword = "*:*",
+        sectionHandle,
+        filters,
+        sort,
+        source = ["*"]
+    ) {
+>>>>>>> 937359b (search results page)
         //var data_url = new URL(`${ES_URL}/apps-dev-library-website/_search`)
-        if($config.esApiKey === "" || !$config.esURL === "") return
-        console.log("keyword:"+keyword)
-        console.log("filters:"+filters)
-        console.log("sort:"+sort)
+        if ($config.esApiKey === "" || !$config.esURL === "") return
+        console.log("keyword:" + keyword)
+        console.log("filters:" + filters)
+        console.log("sort:" + sort)
         let testquery = JSON.stringify({
-            "_source": [ ...source ],
-            "query": {
-                "bool": {
-                    "must": [
+            _source: [...source],
+            query: {
+                bool: {
+                    must: [
                         {
-                            "query_string" : {
-                                "query" : keyword
-                            }
+                            query_string: {
+                                query: keyword,
+                            },
                         },
                         ...parseSectionHandle(sectionHandle),
-                        ...parseFilterQuery(filters)
-                    ]
-                }
+                        ...parseFilterQuery(filters),
+                    ],
+                },
             },
-            ...parseSort(sort)
-            
+            ...parseSort(sort),
         })
-        console.log("this is the query: "+testquery)
+        console.log("this is the query: " + testquery)
 
         // need to know fields to boost on for listing pages when searching like title etc
-    
-        const response = await fetch(`${$config.esURL}/apps-craft-test/_search`, {
-            headers: {
-                'Authorization': `ApiKey ${$config.esApiKey}`,
-                'Content-Type': 'application/json',
-            },
-            method: 'POST',
-            body: JSON.stringify({
-                "size":"1000",
-                "_source": [ ...source ],
-                "query": {
-                    "bool": {
-                        "must": [
-                            {
-                                "query_string" : {
-                                    "query" : keyword,
-                                    "fuzziness":"auto"
-                                }
-                            },
-                            ...parseSectionHandle(sectionHandle),
-                            ...parseFilterQuery(filters)
-                        ]
-                    }
+
+        const response = await fetch(
+            `${$config.esURL}/apps-craft-test/_search`,
+            {
+                headers: {
+                    Authorization: `ApiKey ${$config.esApiKey}`,
+                    "Content-Type": "application/json",
                 },
+<<<<<<< HEAD
                 ...parseSort(sort),
                 "aggs": {
                     ...parseFieldNames(aggFields)
@@ -132,35 +89,44 @@ export default function ({ $config }, inject) {
                 
             })
         })
+=======
+                method: "POST",
+                body: JSON.stringify({
+                    size: "1000",
+                    _source: [...source],
+                    query: {
+                        bool: {
+                            must: [
+                                {
+                                    query_string: {
+                                        query: keyword,
+                                        fuzziness: "auto",
+                                    },
+                                },
+                                ...parseSectionHandle(sectionHandle),
+                                ...parseFilterQuery(filters),
+                            ],
+                        },
+                    },
+                    ...parseSort(sort),
+                }),
+            }
+        )
+>>>>>>> 937359b (search results page)
         const data = await response.json()
         return data
     }
-    inject('dataApi', {
+    inject("dataApi", {
         getMapping,
         siteSearch,
         keywordSearchWithFilters,
-        getAggregations
-    })
-
-
-    async function getMapping(){
-        if($config.esApiKey === "" || !$config.esURL === "") return
-        const response = await fetch(`${$config.esURL}/apps-craft-test/_mapping`, {
-            headers: {
-                'Authorization': `ApiKey ${$config.esApiKey}`,
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-        })
-=======
-    inject("dataApi", {
-        getMapping,
-        keywordSearch,
+        getAggregations,
     })
 
     async function getMapping() {
         if ($config.esApiKey === "" || !$config.esURL === "") return
         const response = await fetch(
-            `${$config.esURL}/apps-dev-library-website/_mapping`,
+            `${$config.esURL}/apps-craft-test/_mapping`,
             {
                 headers: {
                     Authorization: `ApiKey ${$config.esApiKey}`,
@@ -168,62 +134,63 @@ export default function ({ $config }, inject) {
                 },
             }
         )
->>>>>>> dc7923b (add search-site route)
         const data = await response.json()
         return data
     }
 
-    async function getAggregations(fields, sectionHandle){
-        console.log("search text: "+fields)
-        if(!fields || fields.length == 0 ) return
-        const response = await fetch(`${$config.esURL}/apps-craft-test/_search`, {
-            headers: {
-                'Authorization': `ApiKey ${$config.esApiKey}`,
-                'Content-Type': 'application/json',
-            },
-            method: 'POST',
-            body: JSON.stringify({
-                "size": 0,
-                "query": {
-                    "match": {
-                        "sectionHandle":{
-                            "query":sectionHandle
-                        }
-                    }
+    async function getAggregations(fields, sectionHandle) {
+        console.log("search text: " + fields)
+        if (!fields || fields.length == 0) return
+        const response = await fetch(
+            `${$config.esURL}/apps-craft-test/_search`,
+            {
+                headers: {
+                    Authorization: `ApiKey ${$config.esApiKey}`,
+                    "Content-Type": "application/json",
                 },
-                "aggs": {
-                    ...parseFieldNames(fields)
-                }
-            })
-        })
+                method: "POST",
+                body: JSON.stringify({
+                    size: 0,
+                    query: {
+                        match: {
+                            sectionHandle: {
+                                query: sectionHandle,
+                            },
+                        },
+                    },
+                    aggs: {
+                        ...parseFieldNames(fields),
+                    },
+                }),
+            }
+        )
         const data = await response.json()
         return data.aggregations
     }
 
-    function parseSort(sortField){
-        if(!sortField || sortField == "") return {}
-        let parseQuery= {}
-        parseQuery["sort"]=[]
-        parseQuery["sort"][0]= {}
-        parseQuery["sort"][0][sortField] = {"order":"asc"}
-            
-        
+    function parseSort(sortField) {
+        if (!sortField || sortField == "") return {}
+        let parseQuery = {}
+        parseQuery["sort"] = []
+        parseQuery["sort"][0] = {}
+        parseQuery["sort"][0][sortField] = { order: "asc" }
+
         return parseQuery
     }
-    function parseSectionHandle(sectionHandle){
+    function parseSectionHandle(sectionHandle) {
         console.log(sectionHandle)
-        if(sectionHandle && sectionHandle === "") return []
+        if (sectionHandle && sectionHandle === "") return []
         console.log("where is the execution")
         let boolQuery = []
         let sectionHandleTermQueryObj = {}
         sectionHandleTermQueryObj["term"] = {}
         sectionHandleTermQueryObj["term"]["sectionHandle"] = sectionHandle
         boolQuery.push(sectionHandleTermQueryObj)
-        console.log("query:"+boolQuery)
+        console.log("query:" + boolQuery)
         return boolQuery
     }
-    function parseFilterQuery(filters){
-        if(!filters || filters.length == 0) return []
+    function parseFilterQuery(filters) {
+        if (!filters || filters.length == 0) return []
         let boolQuery = []
         /*
         [
@@ -232,34 +199,32 @@ export default function ({ $config }, inject) {
                         "locations.title.keyword":"Powell"
                     }
                 }
-
             ]
-            
-        
+
+
         */
         for (const filter of filters) {
             console.log(filter)
-            if(!filter.value) continue
-            let filterObj = {term:{}}
+            if (!filter.value) continue
+            let filterObj = { term: {} }
             filterObj.term[filter.esFieldName] = filter.value
             boolQuery.push(filterObj)
         }
         return boolQuery
     }
 
-    function parseFieldNames(fields){
+    function parseFieldNames(fields) {
         let aggsFields = {}
         for (const element of fields) {
             console.log(element)
             aggsFields[element.label] = {
-                terms:{
-                    field:  element.esFieldName,
-                    size: 15
-                }
+                terms: {
+                    field: element.esFieldName,
+                    size: 15,
+                },
             }
-            
-        } 
-        console.log("aggsFields:"+JSON.stringify(aggsFields))
+        }
+        console.log("aggsFields:" + JSON.stringify(aggsFields))
         return aggsFields
     }
 }
