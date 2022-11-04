fa413917ad728e6c6ddb229b52325e03219e9773
Ashton Prigge <aprigge@library.ucla.edu>
2022-11-04 11:49:50 -0700

    reactive search working

--- pages/search_site/index.vue
+++ pages/search_site/index.vue
@@ -2,17 +2,18 @@
     <div>
         <search-generic @search-ready="getSearchData" />
         <section-wrapper
-            v-for="(result, index) in searchResults"
+            v-for="(result, index) in page"
             :key="`SearchResultBlock${index}`"
         >
             <search-result
-                :title="result._source.title"
-                :category="result._source.sectionHandle"
-                :summary="result._source.summary"
+                :title="result.title"
+                :category="result.sectionHandle"
+                :summary="result.summary"
                 to="/"
             />
             <divider-general />
         </section-wrapper>
+        {{ page }}
 
         <section-wrapper>
             <divider-way-finder />
@@ -25,15 +26,38 @@
 import _get from "lodash/get"
 
 export default {
+    async asyncData({ $dataApi }) {
+        let data = await $dataApi.siteSearch()
+        console.log(data)
+        return {
+            page: data,
+        }
+    },
     methods: {
         async getSearchData(data) {
             // console.log("from search-generic: " + JSON.stringify(data.text))
+            // let keyword =
+            //     data && data.text && Object.keys(data).length != 0
+            //         ? data.text
+            //         : "*"
+
+            // console.log(Object.keys(data).length)
             const results = await this.$dataApi.siteSearch(data.text)
-            // console.log(JSON.stringify(results))
-            const searchJson = JSON.stringify(results)
-            const searchObject = JSON.parse(searchJson)
-            console.log(searchObject.hits.hits)
-            return { searchResults: _get(searchObject, "hits.hits", []) }
+            if (results && results.hits && results.hits.total.value > 0)
+                this.page = this.parseResults(results.hits.hits)
+        },
+        parseResults(hits = []) {
+            // console.log("checking results data:" + JSON.stringify(hits[0]))
+
+            return hits.map((obj) => {
+                return {
+                    ...obj["_source"],
+                    to: `${obj["_source"].to}`,
+                    title: obj["_source"]["title"],
+                    category: obj["_source"]["category"],
+                    summary: obj["_source"]["summary"],
+                }
+            })
         },
     },
 }
