80fd3a784a6c98ae11182946b631e37416524e27
Ashton Prigge <aprigge@library.ucla.edu>
2022-11-10 09:13:54 -0800

    update test

--- cypress/integration/eventseriesdetail.spec.js
+++ cypress/integration/eventseriesdetail.spec.js
@@ -2,7 +2,7 @@ describe("Event Series Detail Page", () => {
     it("Visits an Event Series Detail Page", () => {
         cy.visit("/visit/events-exhibitions/test-event-series")
         cy.get(".logo-ucla").should("be.visible")
-        cy.get("h1.title").should("contain", "Test Event Series: Side Pie")
+        cy.get("h1.title").should("contain", "Test Event Series")
         cy.percySnapshot({ widths: [768, 992, 1200] })
     })
 })
