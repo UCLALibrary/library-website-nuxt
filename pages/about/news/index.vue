72a38ebc105f933e3ba20b6c218e29d2d5dd722b
Axa Mei Liauw <aliauw@library.ucla.edu>
2022-11-14 14:44:32 -0500

    update query and template for news listing

--- pages/about/news/index.vue
+++ pages/about/news/index.vue
@@ -1,14 +1,51 @@
 <template>
     <main class="page page-news">
         <masthead-secondary
-            :title="summaryData.articleListTitle"
-            :text="summaryData.articleListSummary"
+            :title="summaryData.title"
+            :text="summaryData.summary"
         />
+
+        <section-wrapper section-title="Highlighted News">
+            <banner-featured
+                :image="parsedBannerHeader.image"
+                :title="parsedBannerHeader.title"
+                breadcrumb="Featured"
+                :byline="parsedByline"
+                :locations="parsedBannerHeader.locations"
+                :description="parsedBannerHeader.text"
+                :date-created="parsedBannerHeader.dateCreated"
+                :to="parsedBannerHeader.to"
+                :align-right="true"
+                prompt="Read More"
+                class="banner"
+            />
+        </section-wrapper>
+
+        <section-wrapper>
+            <section-teaser-highlight
+                class="section"
+                :items="parsedSectionHighlight"
+            />
+        </section-wrapper>
+
+        <section-wrapper theme="divider">
+            <divider-way-finder
+                class="divider"
+                color="about"
+            />
+        </section-wrapper>
 
         <section-wrapper>
             <section-staff-article-list
                 :items="parsedNewsList"
                 section-title="All News"
+            />
+        </section-wrapper>
+
+        <section-wrapper>
+            <block-call-to-action
+                class="block-call-to-action"
+                :is-global="true"
             />
         </section-wrapper>
     </main>
@@ -35,6 +72,34 @@ export default {
         }
     },
     computed: {
+        parsedFeaturedNews() {
+            return this.summaryData.featuredNews.map((obj) => {
+                return {
+                    ...obj,
+                    to: `/${obj.to}`,
+                    image: _get(obj, "heroImage[0].image[0]", null),
+                    category: _get(obj, "category[0].title", ""),
+                    dateCreated: _get(obj, "postDate", ""),
+                    byline: _get(obj, "articleStaff", []),
+                    bylineOne: _get(obj, "articleStaff[0].title", ""),
+                    bylineTwo: _get(obj, "postDate", ""),
+                }
+            })
+        },
+        parsedBannerHeader() {
+            return this.parsedFeaturedNews[0]
+        },
+        parsedSectionHighlight() {
+            return this.parsedFeaturedNews.slice(1).map((obj) => {
+                return {
+                    ...obj,
+                    bylineTwo:
+                        obj.bylineTwo != null
+                            ? format(new Date(obj.bylineTwo), "MMMM d, yyyy")
+                            : "",
+                }
+            })
+        },
         parsedNewsList() {
             console.log("Entries: " + this.page.entries)
             return this.page.map((obj) => {
