584b253b72db940d08bf69651037de34ad50a898
Ashton Prigge <aprigge@library.ucla.edu>
2022-11-15 14:49:50 -0800

    rebase

--- pages/collections/access/index.vue
+++ pages/collections/access/index.vue
@@ -20,19 +20,6 @@
                 @view-mode-change="viewModeChanger" -->
         </masthead-secondary>
 
-<<<<<<< HEAD
-=======
-        <banner-text
-            v-if="!page.heroImage || page.heroImage.length == 0"
-            class="banner-text"
-            category="Library News"
-            :title="page.title"
-            :text="page.text"
-            :byline="parsedBylineBannerText"
-            :locations="page.locations"
-        />
-
->>>>>>> 16e5842 (feat: add more components)
         <section-wrapper>
             <divider-way-finder class="divider divider-way-finder" />
         </section-wrapper>
@@ -50,7 +37,6 @@
         </section-wrapper>
 
         <section-wrapper>
-<<<<<<< HEAD
             <section-cards-with-illustrations
                 class="section"
                 :items="parsedAssociatedTopics"
@@ -59,95 +45,35 @@
                 button-text="All services & Resources"
                 :is-horizontal="false"
             />
-=======
-            <section-wrapper>
-                <!-- <banner-featured
-                    class="banner banner-about"
-                    :image="bannerAbout.image"
-                    :to="bannerAbout.to"
-                    :title="bannerAbout.title"
-                    :category="bannerAbout.category"
-                    :breadcrumb="bannerAbout.breadcrumb"
-                    :start-date="bannerVisit.startDate"
-                    :end-date="bannerVisit.endDate"
-                    :prompt="bannerAbout.prompt"
-                    :ratio="bannerAbout.ratio"
-                    :locations="bannerAbout.locations"
-                />
-                <block-highlight
-                    :image="image"
-                    :to="to"
-                    :category="category"
-                    :title="title"
-                    :start-date="startDate"
-                    :end-date="endDate"
-                    :text="text"
-                    :has-triangle="true"
-                    :is-vertical="true"
-                    :image-aspect-ratio="60"
-                    :locations="locations"
-                /> -->
-            </section-wrapper>
         </section-wrapper>
-
-        <section-wrapper>
-            <divider-way-finder class="divider divider-way-finder" />
->>>>>>> 16e5842 (feat: add more components)
-        </section-wrapper>
-
-        <!-- cards with images -->
-        <!-- <flexible-card-with-image
-            :block="block"
-        /> -->
-
-        <section-wrapper>
-            <divider-way-finder class="divider divider-way-finder" />
-        </section-wrapper>
-
-        <flexible-blocks
-            v-if="page"
-            class="flexible-content"
-            :blocks="page.blocks"
-        />
     </section>
 </template>
 
 <script>
 // Helpers
 import _get from "lodash/get"
-
 // GQL
 import ACCESS_COLLECTIONS from "~/gql/queries/CollectionsAccessList.gql"
-
 export default {
-    data() {
+    async asyncData({ $graphql }) {
+        const data = await $graphql.default.request(ACCESS_COLLECTIONS)
+        data.entry.accessCollections.forEach((element) => {
+            element.to = element.uri ? element.uri : element.externalResourceUrl
+            element.category =
+                element.workshopOrEventSeriesType === "help/services-resources"
+                    ? "workshop"
+                    : element.serviceOrResourceType
+                        ? element.serviceOrResourceType
+                        : element.typeHandle === "externalResource"
+                            ? "external resource"
+                            : element.typeHandle
+        })
         return {
-            allEvents: [],
-            primaryItems: [],
-            secondaryItems: [],
-            formData: {},
-            formId: "",
-            eventId: "9383207",
-            libcalEndpointProxy: this.$config.libcalProxy,
-            page: {}
+            page: _get(data, "entry", {}),
         }
     },
-    async fetch() {
-        const data = await this.$graphql.default.request(ACCESS_COLLECTIONS)
-
-        data.entry.accessCollections.forEach(element => {
-            element.to = element.uri ? element.uri : element.externalResourceUrl
-
-            element.category = 
-                (element.workshopOrEventSeriesType === "help/services-resources") ? "workshop":
-                    element.serviceOrResourceType ? element.serviceOrResourceType :
-                        (element.typeHandle === "externalResource") ? "external resource":
-                            element.typeHandle
-        })
-        this.page = _get(data, "entry", {})
-    },
     computed: {
-        parsedAccessCollections(){
+        parsedAccessCollections() {
             return this.page.accessCollections.map((obj) => {
                 return {
                     ...obj,
@@ -157,7 +83,7 @@ export default {
                 }
             })
         },
-        parsedAssociatedTopics(){
+        parsedAssociatedTopics() {
             return this.page.associatedTopics.map((obj) => {
                 return {
                     ...obj,
@@ -166,13 +92,12 @@ export default {
                         : `/${obj.to}`,
                 }
             })
-        }
-    }
+        },
+    },
 }
 </script>
 
 <style lang="scss" scoped>
 .page-collections-access {
-
 }
 </style>
