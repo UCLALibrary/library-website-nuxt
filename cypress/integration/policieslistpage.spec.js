2b1c6aee6878b6f6a246d184bfbf89f28aebb677
jendiamond <thejendiamond@gmail.com>
2022-11-15 13:59:10 -0800

    feat: update spec

--- cypress/integration/policieslistpage.spec.js
+++ cypress/integration/policieslistpage.spec.js
@@ -1,11 +1,11 @@
-describe("Policy Detail page", () => {
-    it("Visits a Policy Detail Page", () => {
+describe("Policy List page", () => {
+    it("Visits a Policy List Page", () => {
         cy.visit("about/policies")
         cy.get(".logo-ucla").should("be.visible")
         cy.get(".page-policies").should("be.visible")
         cy.get("h1.title").should(
             "contain",
-            "I Like Turtles"
+            "Policies"
         )
         cy.percySnapshot({ widths: [768, 992, 1200] })
     })
