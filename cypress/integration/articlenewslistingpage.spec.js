33dcee6165196a684b135ec2a9a0544a355e8942
JenDiamond <thejendiamond@gmail.com>
2022-11-16 11:48:10 -0800

    feat: APPS-1969 Create News listing page (#511)
    * update library components
    
    * feat: add masthead to news listing page
    
    * update query and template for news listing
    
    * chore: add spec
    
    * tweak template
    
    * feat: update if statements
    
    * remove duplicate spec
    
    * update test
    
    Co-authored-by: Axa Mei Liauw <aliauw@library.ucla.edu>
    Co-authored-by: Ashton Prigge <34147754+aprigge@users.noreply.github.com>
    Co-authored-by: Ashton Prigge <aprigge@library.ucla.edu>

---
+++ cypress/integration/articlenewslistingpage.spec.js
@@ -0,0 +1,11 @@
+describe("Article News Listing page", () => {
+    it("Visit a Article News Listing Page", () => {
+        cy.visit("/about/news")
+
+        // UCLA Library brand
+        cy.get(".logo-ucla").should("be.visible")
+        cy.get(".page-news").should("be.visible")
+        cy.get("h1.title").should("contain", "Library News")
+        cy.percySnapshot({ widths: [768, 992, 1200] })
+    })
+})
--- pages/about/news/index.vue
+++ pages/about/news/index.vue
@@ -1,29 +1,77 @@
 <template>
-    <main class="page page-news">
-        <banner-header
-            v-if="page.heroImage && page.heroImage.length == 1"
-            :image="page.heroImage[0].image[0]"
+    <main
+        id="main"
+        class="page page-news"
+    >
+        <masthead-secondary
             :title="page.title"
-            category="Library News"
-            :byline="parsedBylines"
-            :locations="locations"
-            :date-created="parsedDate"
-            :to="to"
-            :authors="authors"
-            :align-right="true"
+            :text="page.summary"
         />
+
+        <!-- <search-generic
+            search-type="about"
+            :filters="searchFilters"
+            class="generic-search"
+            @search-ready="getSearchData"
+        /> -->
 
         <section-wrapper theme="divider">
             <divider-way-finder
-                class="divider"
                 color="about"
             />
+        </section-wrapper>
+
+        <section-wrapper
+            v-if="page.featuredNews.length"
+            section-title="Highlighted News"
+        >
+            <banner-featured
+                :image="parsedBannerHeader.image"
+                :title="parsedBannerHeader.title"
+                breadcrumb="Featured"
+                :byline="parsedByline"
+                :locations="parsedBannerHeader.locations"
+                :description="parsedBannerHeader.text"
+                :date-created="parsedBannerHeader.dateCreated"
+                :to="parsedBannerHeader.to"
+                :align-right="true"
+                prompt="Read More"
+                class="banner"
+            />
+
+            <section-wrapper theme="divider">
+                <divider-way-finder
+                    color="about"
+                />
+            </section-wrapper>
+        </section-wrapper>
+
+        <section-wrapper
+            v-if="parsedSectionHighlight.length"
+        >
+            <section-teaser-highlight
+                class="section"
+                :items="parsedSectionHighlight"
+            />
+
+            <section-wrapper theme="divider">
+                <divider-way-finder
+                    color="about"
+                />
+            </section-wrapper>
         </section-wrapper>
 
         <section-wrapper>
             <section-staff-article-list
                 :items="parsedNewsList"
                 section-title="All News"
+            />
+        </section-wrapper>
+
+        <section-wrapper>
+            <block-call-to-action
+                class="block-call-to-action"
+                :is-global="true"
             />
         </section-wrapper>
     </main>
@@ -45,13 +93,47 @@ export default {
         })
         // console.log("data:" + data)
         return {
-            page: data,
+            news: _get(data, "entries", {}),
+            page: _get(data, "entry", {}),
+        }
+    },
+    head() {
+        let title = this.page ? this.page.title : "... loading"
+        return {
+            title: title,
         }
     },
     computed: {
+        parsedFeaturedNews() {
+            return this.page.featuredNews.map((obj) => {
+                return {
+                    ...obj,
+                    to: `/${obj.to}`,
+                    image: _get(obj, "heroImage[0].image[0]", null),
+                    category: _get(obj, "category[0].title", ""),
+                    dateCreated: _get(obj, "postDate", ""),
+                    byline: _get(obj, "articleStaff", []),
+                    bylineOne: _get(obj, "articleStaff[0].title", ""),
+                    bylineTwo: _get(obj, "postDate", ""),
+                }
+            })
+        },
+        parsedBannerHeader() {
+            return this.parsedFeaturedNews[0]
+        },
+        parsedSectionHighlight() {
+            return this.parsedFeaturedNews.slice(1).map((obj) => {
+                return {
+                    ...obj,
+                    bylineTwo:
+                        obj.bylineTwo != null
+                            ? format(new Date(obj.bylineTwo), "MMMM d, yyyy")
+                            : "",
+                }
+            })
+        },
         parsedNewsList() {
-            console.log("Entries: " + this.page.entries)
-            return this.page.entries.map((obj) => {
+            return this.news.map((obj) => {
                 return {
                     ...obj,
                     to: `/${obj.to}`,
@@ -62,10 +144,10 @@ export default {
             })
         },
         parsedDate() {
-            return format(new Date(this.page.dateCreated), "MMMM d, Y")
+            return format(new Date(this.news.postDate), "MMMM d, Y")
         },
         parsedByline() {
-            let byline = (this.page.contributors || []).map((entry) => {
+            let byline = (this.news.contributors || []).map((entry) => {
                 return `${entry.byline} ${
                     entry.title || entry.staffMember[0].title
                 }`
@@ -80,31 +162,5 @@ export default {
 
 <style lang="scss" scoped>
 .page-news {
-    padding-left: 50px;
-
-    .entry-count {
-        @include step-2;
-        color: var(--color-primary-blue-03);
-        margin: var(--space-m);
-    }
-
-    .section-heading {
-        @include step-2;
-        color: var(--color-primary-blue-03);
-        margin-bottom: var(--space-m);
-    }
-
-    .all-news-heading {
-        @include step-1;
-        color: var(--color-primary-blue-03);
-    }
-
-    .news-item-link {
-        list-style: none;
-        display: flex;
-        justify-content: space-between;
-        @include step-1;
-        color: var(--color-primary-blue-03);
-    }
 }
 </style>
