7ceff3688e0e7c6a7bff9c05c5f0e5e51eea7f21
dianneweinthal <44713143+dianneweinthal@users.noreply.github.com>
2022-11-16 15:16:02 -0800

    feat: adds gql and index for endowments

---
+++ pages/support/index.vue
@@ -0,0 +1,61 @@
+<template lang="html">
+    <main
+        class="page page-endowment-listing"
+    >
+        <nav-breadcrumb
+                to="/support"
+                title="Endowments"
+                parent-title="Support Us"
+            />
+
+        <masthead-secondary
+            :title="page.title"
+            :text="page.summary"
+        />
+
+        <!-- <search-generic
+            search-type="about"
+            :filters="searchFilters"
+            class="generic-search"
+            @search-ready="getSearchData"
+        /> -->
+
+        <section-wrapper theme="divider">
+            <divider-way-finder
+                color="about"
+            />
+        </section-wrapper>
+
+        <section-wrapper>
+            <section-staff-article-list
+                :items="parsedEndowmentList"
+                section-title="All Endowments"
+            />
+            <!-- pagination -->
+        </section-wrapper>    
+    </main>
+</template>
+
+<script>
+// Helpers
+import _get from "lodash/get"
+import format from "date-fns/format"
+
+// GQL
+import ENDOWMENT_LIST from "~gql/queries/EndowmentList"
+
+export default {
+    head() {
+        let title = this.page ? this.page.title : "... loading"
+        return {
+            title: title,
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.page-endowment-listing {
+}
+</style>
+
