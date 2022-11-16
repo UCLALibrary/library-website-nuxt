a5a790be8b06457ad523150b9c3692144612c6ba
matheusgomes062 <matheusgomes062@gmail.com>
2022-11-16 17:59:39 -0300

    fix: gql fix

---
+++ cypress/integration/serviceresourcespage.spec.js
@@ -0,0 +1,7 @@
+describe("Services & Resources page", () => {
+    it("Visits the Services & Resources Page", () => {
+        cy.visit(" help/services-resources/a-test-workshop-with-a-pull-quote-fpb")
+        cy.get(".logo-ucla").should("be.visible")
+        cy.percySnapshot({ widths: [768, 992, 1200] })
+    })
+})
