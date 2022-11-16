b3b6b1196c8ccf9a02293eb7ddd446a4409c9fcb
Ashton Prigge <aprigge@library.ucla.edu>
2022-11-16 11:32:36 -0800

    remove duplicate spec

--- cypress/integration/articlenewslistingpage.spec.js
+++ cypress/integration/articlenewslistingpage.spec.js
@@ -1,14 +1,11 @@
-describe("Article News List page", () => {
-    it("Visit a Article News List Page", () => {
+describe("Article News Listing page", () => {
+    it("Visit a Article News Listing Page", () => {
         cy.visit("/about/news")
 
         // UCLA Library brand
         cy.get(".logo-ucla").should("be.visible")
         cy.get(".page-news").should("be.visible")
-        cy.get("h1.title").should(
-            "contain",
-            "Listing - News"
-        )
+        cy.get("h1.title").should("contain", "Listing - News")
         cy.percySnapshot({ widths: [768, 992, 1200] })
     })
 })
