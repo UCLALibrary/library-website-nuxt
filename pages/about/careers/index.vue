c5b3959fae763d689b6bb6c11e56ce8db799c303
jendiamond <thejendiamond@gmail.com>
2022-11-17 16:02:59 -0800

    feat: add sketch components

--- pages/about/careers/index.vue
+++ pages/about/careers/index.vue
@@ -22,6 +22,45 @@
         <h3>jobs ---- {{ jobs }}</h3>
         <hr>
         <h3>associatedTopics ---- {{ associatedTopics }}</h3>
+
+        <!-- <block-highlight
+            :image="associatedTopics.image"
+            :to="associatedTopics.to"
+            :category="associatedTopics.category"
+            :title="associatedTopics.title"
+            :start-date="startDate"
+            :end-date="endDate"
+            :text="text"
+            :has-triangle="true"
+            :is-vertical="true"
+            :image-aspect-ratio="60"
+            :locations="locations"
+        /> -->
+
+        <section-wrapper>
+            <search-result
+                :title="jobs[0].title"
+                :summary="jobs[0].summary"
+                :to="jobs[0].to"
+            />
+            <section-wrapper theme="divider">
+                <divider-way-finder color="about" />
+            </section-wrapper>
+        </section-wrapper>
+
+        <section-wrapper>
+            <section-cards-with-illustrations
+                :items="associatedTopics"
+                section-title="Associated Topics"
+                section-summary="Curabitur aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh."
+                to="/help/foo/bar"
+                :is-horizontal="false"
+                button-text="See More"
+            />
+            <section-wrapper theme="divider">
+                <divider-way-finder color="about" />
+            </section-wrapper>
+        </section-wrapper>
     </main>
 </template>
 
