c071238ad563fd982f40a9bc73e2f2205d769651
Axa Mei Liauw <axa.liauw@gmail.com>
2022-11-21 16:01:19 -0500

    chore: add spec

---
+++ cypress/integration/endowmentsdetail.spec.js
@@ -0,0 +1,11 @@
+describe("Endowments Detail page", () => {
+    it("Visits an Endowments Detail Page", () => {
+        // the following data will not work in production
+        cy.visit("/support/endowments/lifu-wang-chinese-cultural-endowment")
+
+        // UCLA Library brand
+        cy.get(".logo-ucla").should("be.visible")
+        cy.get("h1.title").should("contain", "Lifu Wang")
+        cy.percySnapshot({ widths: [768, 992, 1200] })
+    })
+})
