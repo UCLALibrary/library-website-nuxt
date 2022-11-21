cfd1ff4c9e495075db4f03e44434c5b69cec690d
dianneweinthal <44713143+dianneweinthal@users.noreply.github.com>
2022-11-21 10:51:50 -0800

    feat: updates query and path

--- pages/support/endowments/index.vue
+++ pages/support/endowments/index.vue
@@ -28,7 +28,7 @@
 
         <section-wrapper>
             <section-staff-article-list
-                :items="parsedEndowmentList"
+                :items="parsedEndowmentsList"
                 section-title="All Endowments"
             />
             <!-- pagination -->
@@ -42,7 +42,7 @@ import _get from "lodash/get"
 import format from "date-fns/format"
 
 // GQL
-import ENDOWMENT_LIST from "~gql/queries/EndowmentList"
+import ENDOWMENTS_LIST from "~/gql/queries/EndowmentList"
 
 export default {
     async asyncData({ $graphql, route }) {
@@ -50,24 +50,24 @@ export default {
         const data = await $graphql.default.request(ENDOWMENTS_LIST, {})
         // console.log("data:" + data)
         return {
-            page: _get(data, "entries", {}),
-            entry: _get(data, "entry", {}),
+            endowments: _get(data, "entries", {}),
+            page: _get(data, "entry", {}),
         }
     },
     head() {
-        let title = this.page ? this.page.title : "... loading"
+        let title = this.entry ? this.page.title : "... loading"
         return {
             title: title,
         }
     },
     computed: {
         parsedEndowmentsList() {
-            return this.page.map((obj) => {
+            return this.endowments.map((obj) => {
                 return {
                     ...obj,
                     to: `/${obj.to}`,
                     image: _get(obj, "heroImage[0].image[0]", null),
-                    category: _get(obj, "donors[0].title", null),
+                    category: (obj.donors[0].firstName != null) ? `Donor: ${obj.donors[0].firstName} ${obj.donors[0].lastName}` : `Donors: ${obj.donors[0].lastName}`
                 }
             })
         },
