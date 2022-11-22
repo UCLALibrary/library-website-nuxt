46baa41c529deca59b0d790b229b1191ebf9c17d
matheusgomes062 <matheusgomes062@gmail.com>
2022-11-22 16:55:21 -0300

    fix: added component and made some changes

--- pages/visit/locations/index.vue
+++ pages/visit/locations/index.vue
@@ -4,31 +4,41 @@
             :title="summaryData.title"
             :text="summaryData.summary"
         />
-        <h3>{{ summaryData }}</h3>
+        
+        <section-wrapper theme="divider">
+            <divider-way-finder
+                class="divider-way-finder"
+                color="default"
+            />
+        </section-wrapper>
+        
+        <h2 class="section-heading">
+            {{ summaryData.title }}
+        </h2>
+        <!-- <h3>{{ summaryData }}</h3>
         <hr>
         <h3>{{ uclaLibraries }}</h3>
         <hr>
         <h3>parsedAddress --- {{ parsedAddress }}</h3>
         <hr>
-        <h3>{{ affiliateLibraries }}</h3>
+        <h3>{{ affiliateLibraries }}</h3> -->
 
         <section-wrapper v-if="uclaLibraries">
-        <!-- <section-location-list
+            <section-location-list
                 :items="uclaLibraries"
             />
-        </section-wrapper> -->
-        <!-- 
-        <nuxt-link
-            v-for="item in parsedLocationsList"
-            :key="item.to"
-            :to="item.to"
-        >
-            <div
-                class="text"
-                v-html="item.title"
-            />
-        </nuxt-link> -->
         </section-wrapper>
+        <!-- <section-wrapper v-if="uclaLibraries">
+            <block-location-list-item
+                v-for="item, index in uclaLibraries"
+                :key="`uclaLibraries-${index}`"
+                :title="item.title"
+                :to="item.uri"
+                :image="item.heroImage[0]"
+                :address="item.address"
+                :amenities="item.amenities"
+            />
+        </section-wrapper> -->
     </div>
 </template>
 
@@ -63,19 +73,49 @@ export default {
             return this.page.map((obj) => {
                 return {
                     ...obj,
-                    to: 
+                    to: ""
                 }
             })
         },
-        parsedAffiliateLibraries(){
+        parsedLocationsList() {
+            return this.page.map((obj) => {
+                return {
+                    ...obj,
+                    to: obj.locationType === "uclaLibrary"?`/visit/locations/${obj.to}`:obj.affiliateLibraryUrl,                     
+                    image: _get(obj, "heroImage[0].image[0]", null),                 
+                    address: this.parsedAddress(obj),
+                    addressLink: `https://map.ucla.edu/?id=${obj.campusMapId}&e=true`,
+                    amenities: ""// look for my comment below about amenities
+                }
+            })
+        },
+        // parsedAffiliateLibraries(){
             
-        }
+        // }
         // TODO match on LibCal id for Hours today
     },
+    methods: {
+        parseAddress(obj) {
+            return  obj.address && obj.address[0] ?
+                obj.address[0].addressLine1 + 
+                obj.address[0].addressLine2 + 
+                obj.address[0].addressCity + 
+                obj.address[0].addressState + 
+                obj.address[0].addressZipCode + 
+                obj.address[0].addressCountry : ""
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 .page-help {
+
+    .section-heading {
+        @include step-4;
+        color: var(--color-primary-blue-03);
+        margin: var(--space-xl) auto;
+        max-width: $container-l-main + px;
+    }
 }
 </style>
