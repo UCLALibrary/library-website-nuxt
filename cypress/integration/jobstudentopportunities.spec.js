d25f1d10ebb035afc3b30701a6799a756cc45362
Axa Mei Liauw <axa.liauw@gmail.com>
2022-11-22 16:49:11 -0500

    update path on spec

--- cypress/integration/jobstudentopportunities.spec.js
+++ cypress/integration/jobstudentopportunities.spec.js
@@ -1,7 +1,7 @@
 describe("Student Opportunities List page", () => {
     it("Visits the Student Opportunities List Page", () => {
         // the following data will not work in production
-        cy.visit("/about/jobs/student-opportunities")
+        cy.visit("/about/student-opportunities")
 
         // UCLA Library brand
         cy.get(".logo-ucla").should("be.visible")
