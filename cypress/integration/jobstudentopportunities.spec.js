9394ea7f715d67acf9948090aa52bff3b14e9174
JenDiamond <thejendiamond@gmail.com>
2022-11-29 12:01:42 -0800

    feat: Create listing page for Student Opportunities (#558)
    * update component library
    
    * feat: add files and sketch code
    
    * feat:
    
    * fix dir name typo
    
    * use correct path
    
    * update path on spec
    
    * update template with some data
    
    * feat: update component library
    
    * update links to programs and topics
    
    * feat: fix associatedLocations
    
    Co-authored-by: Axa Mei Liauw <axa.liauw@gmail.com>
    Co-authored-by: Parinita Mulak <pghorpade@library.ucla.edu>

---
+++ cypress/integration/jobstudentopportunities.spec.js
@@ -0,0 +1,12 @@
+describe("Student Opportunities List page", () => {
+    it("Visits the Student Opportunities List Page", () => {
+        // the following data will not work in production
+        cy.visit("/about/student-opportunities")
+
+        // UCLA Library brand
+        cy.get(".logo-ucla").should("be.visible")
+        cy.get(".page-student-opportunities").should("be.visible")
+        cy.get("h1.title").should("contain", "Student Opportunities")
+        cy.percySnapshot({ widths: [768, 992, 1200] })
+    })
+})
---
+++ pages/about/student-opportunities/index.vue
@@ -0,0 +1,188 @@
+<template lang="html">
+    <main
+        id="main"
+        class="page page-student-opportunities"
+    >
+        <nav-breadcrumb
+            to="/about"
+            :title="page.title"
+            parent-title="About"
+        />
+
+        <banner-text
+            class="banner-text"
+            :title="page.title"
+            :text="page.text"
+            :email="page.email"
+            :phone="page.phoneNumber"
+            :button-text="page.buttonUrl[0].buttonText"
+            :to="page.buttonUrl[0].buttonUrl"
+        />
+
+        <section-wrapper theme="divider">
+            <divider-way-finder
+                class="divider"
+                color="about"
+            />
+        </section-wrapper>
+
+        <!-- STUDENT JOBS -->
+        <section-wrapper
+            section-title="Student Jobs"
+        >
+            <section-generic-list
+                v-if="parsedStudentJobs.length > 0"
+                :items="parsedStudentJobs"
+            />
+            <div
+                v-else
+                class="no-positions"
+            >
+                No positions available
+            </div>
+        </section-wrapper>
+
+        <section-wrapper theme="divider">
+            <divider-way-finder
+                class="divider"
+                color="about"
+            />
+        </section-wrapper>
+
+        <!-- STUDENT INTERNSHIPS -->
+        <section-wrapper
+            section-title="Student Internships"
+        >
+            <section-generic-list
+                v-if="parsedStudentInternships.length > 0"
+                :items="parsedStudentInternships"
+            />
+            <div
+                v-else
+                class="no-positions"
+            >
+                No positions available
+            </div>
+        </section-wrapper>
+
+        <section-wrapper theme="divider">
+            <divider-way-finder
+                class="divider"
+                color="about"
+            />
+        </section-wrapper>
+
+        <section-wrapper section-title="Awards, Grants, and Fellowships">
+            <section-generic-list
+                v-if="parsedAssociatedPrograms.length > 0"
+                :items="parsedAssociatedPrograms"
+            />
+        </section-wrapper>
+
+        <section-wrapper theme="divider">
+            <divider-way-finder color="about" />
+        </section-wrapper>
+
+        <!-- ASSOCIATED TOPICS -->
+        <section-wrapper>
+            <section-cards-with-illustrations
+                v-if="parsedAssociatedTopics.length > 0"
+                :items="parsedAssociatedTopics"
+                section-title="Associated Topics"
+            />
+        </section-wrapper>
+    </main>
+</template>
+
+<script>
+// Helpers
+import _get from "lodash/get"
+
+// GQL
+import STUDENT_OPPORTUNITIES_LIST from "~/gql/queries/JobStudentOpportunitiesList"
+
+export default {
+    async asyncData({ $graphql }) {
+        const data = await $graphql.default.request(STUDENT_OPPORTUNITIES_LIST)
+        return {
+            page: _get(data, "entry", {}),
+            allJobs: _get(data, "allJobs", {}),
+            // associatedTopics: _get(data, "entry.associatedTopics", {}),
+        }
+    },
+    head() {
+        let title = this.page
+            ? this.page.title
+            : "... loading"
+        return {
+            title: title,
+        }
+    },
+    computed: {
+        parsedStudentJobs() {
+            let allStudentJobs = this.allJobs.filter((obj) => {
+                return obj.jobType[0].title === "Student Job"
+            })
+            return allStudentJobs.map((obj) => {
+                return {
+                    ...obj,
+                    payRate: _get(obj, "payRate", null),
+                    associatedLocations: obj.associatedLocations.map((entry) => {
+                        return {
+                            ...entry,
+                            uri: `/${entry.uri}`
+                        }
+                    })
+                }
+            })
+        },
+        parsedStudentInternships() {
+            let allInternships = this.allJobs.filter((obj) => {
+                return obj.jobType[0].title === "Student Internship"
+            })
+            return allInternships.map((obj) => {
+                return {
+                    ...obj,
+                    payRate: _get(obj, "payRate", null),
+                    associatedLocations: obj.associatedLocations.map((entry) => {
+                        return {
+                            ...entry,
+                            uri: `/${entry.uri}`
+                        }
+                    })
+                }
+            })
+        },
+        parsedAssociatedPrograms() {
+            return this.page.associatedPrograms.map((obj) => {
+                return {
+                    ...obj,
+                    jobPostingURL: obj.programUrlBehavior === "externalSite"
+                        ? obj.buttonUrl[0].buttonUrl
+                        : `/${obj.jobPostingURL}`,
+                }
+            })
+        },
+        parsedAssociatedTopics() {
+            return this.page.associatedTopics.map((obj) => {
+                return {
+                    ...obj,
+                    to: obj.externalResourceUrl
+                        ? obj.externalResourceUrl
+                        : `/${obj.to}`,
+                }
+            })
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.page-student-opportunities {
+    .no-positions {
+        @include step-1;
+        font-style: italic;
+        color: var(--color-primary-blue-05);
+    }
+}
+</style>
