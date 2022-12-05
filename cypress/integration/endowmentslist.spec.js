f22681ec2e3bcc8b205cc334a8695a385bf522e5
Axa Mei Liauw <aliauw@library.ucla.edu>
2022-12-05 11:11:36 -0500

    fix: update endowments detail spec and add spec for listing page

---
+++ cypress/integration/endowmentslist.spec.js
@@ -0,0 +1,12 @@
+describe("Endowments Detail page", () => {
+    it("Visits an Endowments Detail Page", () => {
+        // the following data will not work in production
+        cy.visit("/give/endowments")
+
+        // UCLA Library brand
+        cy.get(".logo-ucla").should("be.visible")
+        cy.get(".page-endowment-listing").should("be.visible");
+        cy.get("h1.title").should("contain", "Endowments")
+        cy.percySnapshot({ widths: [768, 992, 1200] })
+    })
+})
