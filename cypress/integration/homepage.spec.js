e095914e2c30f46bdfe309efc8bc6276c9eb4d05
Ashton Prigge <34147754+aprigge@users.noreply.github.com>
2022-12-13 16:07:27 -0800

    feat:  homepage (#561)
    * rebase
    
    * rebase
    
    * rebase
    
    * rebase
    
    * rebase
    
    * rebase
    
    * add main
    
    * rebase
    
    * u[date component library and aprsed data
    
    * search links
    
    * rebase
    
    * update test
    
    * rebase
    
    * rebase
    
    * update tests
    
    * rebase
    
    * collectionde tail spec to match main
    
    * update spec
    
    * ux updates
    
    * ux updates
    
    * update banner-featured
    
    * update banner-featured
    
    * rebase
    
    * rebase
    
    * update searchlink logic
    
    * update searchlink logic
    
    * update searchlink logic
    
    * update searchlink logic
    
    * rebase
    
    * rebase
    
    * update path

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -16,50 +16,47 @@ describe("Website Homepage", () => {
             .and("contain", "Ask a Librarian")
             .and("contain", "My Account")
 
-        // removing test data to resolve test failure
-
         // NavPrimary
-        // cy.get(".nav-primary")
-        //     .should("contain", "Get help with...")
-        //     .and("contain", "Visit")
-        //     .and("contain", "About")
-        // .and("contain", "Support us") // this is no more part of primary navigation on test
+        cy.get(".nav-primary")
+            .should("contain", "Get help with...")
+            .and("contain", "Visit")
+            .and("contain", "About")
 
         // MastheadPrimary
-        // cy.get(".masthead-primary").find(".logo").should("be.visible")
+        cy.get(".masthead-primary").find(".logo").should("be.visible")
+        cy.get(".masthead-primary")
+            .find(".search-home")
+            .should("be.visible")
+            .and("contain", "Search the Library Site")
+            .and("contain", "Search Materials")
+        cy.get(".masthead-primary")
+            .find("input.input-search")
+            .should("have.attr", "placeholder", "Search by keyword")
+        cy.get(".masthead-primary")
+            .contains("a", "Databases A-Z")
+            .should(
+                "have.attr",
+                "href",
+                "https://guides.library.ucla.edu/az.php?&_ga=2.194620910.2109030156.1663611669-236460657.1661379075"
+            )
+        cy.get(".masthead-primary")
+            .contains("a", "Course Reserves")
+            .should(
+                "have.attr",
+                "href",
+                "https://www.library.ucla.edu/borrow-renew-return/course-reserves"
+            )
         // cy.get(".masthead-primary")
-        //     .find(".search-home")
-        //     .should("be.visible")
-        //     .and("contain", "Search the Library Site")
-        //     .and("contain", "Search Materials")
-        // cy.get(".masthead-primary")
-        //     .find("input.input-search")
-        //     .should("have.attr", "placeholder", "Search by keyword")
-        // cy.get(".masthead-primary")
-        //     .contains("a", "Course Reserves")
-        //     .should(
-        //         "have.attr",
-        //         "href",
-        //         "https://catalog.library.ucla.edu/vwebv/enterCourseReserve.do"
-        //     )
-        // cy.get(".masthead-primary")
-        //     .contains("a", "UCLA Research Guides")
-        //     .should("have.attr", "href", "https://guides.library.ucla.edu/")
-        // cy.get(".masthead-primary")
-        //     .contains("a", "Databases A-Z")
-        //     .should(
-        //         "have.attr",
-        //         "href",
-        //         "https://guides.library.ucla.edu/az.php"
-        //     )
+        //     .contains("a", "Advanced Search")
+        //     .should("have.attr", "href", "http://www.primo.com")
 
-        // //get help with
-        // cy.get(".section-cards-with-illustrations").should(
-        //     "contain",
-        //     "Get Help With"
-        // )
+        // Get help with
+        cy.get(".section-cards-with-illustrations").should(
+            "contain",
+            "Get Help With"
+        )
 
-        cy.get('[aria-label="Start chat"]').click({force: true})
+        cy.get('[aria-label="Start chat"]').click({ force: true })
 
         cy.percySnapshot({ widths: [768, 992, 1200] })
     })
