b968fa292d3d617ad5537e273de26229d604d0c4
Parinita Mulak <pghorpade@library.ucla.edu>
2022-12-14 17:14:32 -0800

    fix: Use libcal proxy to avoid CORS issue (#586)

--- plugins/scrape-formid.client.js
+++ plugins/scrape-formid.client.js
@@ -1,11 +1,13 @@
-export default function({$axios}, inject){
-    const libcalURL = "https://calendar.library.ucla.edu/event/"
+export default function({$axios,$config}, inject){
+    //const libcalURL = "https://calendar.library.ucla.edu/event/"
     inject('scrapeApi', {
         scrapeFormId,
     })
     async function scrapeFormId(eventId = ""){
-        const response = await fetch(libcalURL + eventId)
+        console.log("event ID:"+eventId)
+        const response = await fetch($config.libcalProxy+"event/" + eventId)
         const html = await response.text()
+        console.log("event html:"+html)
         var parser = new DOMParser()
         var doc = parser.parseFromString(html, "text/html")
         let formId = doc.querySelector("input[name='fid']").getAttribute("value") 
