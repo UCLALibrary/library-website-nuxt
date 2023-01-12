2a659224904bb47454b6912bee062e35af369646
Andy Wallace <andrewwallace@library.ucla.edu>
2023-01-12 14:39:44 -0800

    fix: window.innerWidth in Chrome devtools (#606)
    * fix: window.innerWidth in Chrome devtools
    
    Fixes a fairly obscure issue where the viewport width (used to determine which header to show) was incorrect when the responsive window size was set to 1024px or smaller in Chrome devtools.
    
    I was unable to reproduce the issue elsewhere and don't understand the full implications of this change, so I don't know if it's worth making. But as far as I can tell everything looks good.
    
    * Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
             { charset: "utf-8" },
             {
                 name: "viewport",
-                content: "width=device-width, initial-scale=1",
+                content: "width=device-width, initial-scale=1.0, minimum-scale=1.0",
             },
         ],
     },
