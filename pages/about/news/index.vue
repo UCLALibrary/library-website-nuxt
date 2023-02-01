0ec44041d40cae560bbc31f05a4b49d4c9d2db7e
dianneweinthal <44713143+dianneweinthal@users.noreply.github.com>
2023-02-01 15:05:51 -0800

    feat: add styles and section wrapper to news index

--- pages/about/news/index.vue
+++ pages/about/news/index.vue
@@ -75,16 +75,39 @@
         </section-wrapper>
         <section-wrapper v-else-if="hits && hits.length > 0">
             <div class="about-results">
-                {{ parseDisplayResults }}
+                Displaying {{ hits.length }} results for <strong><em>“{{ $route.query.q }}”</em></strong>
             </div>
             <section-staff-article-list :items="parseHitsResults" />
         </section-wrapper>
-        <div
-            v-else-if="noResultsFound"
-            class="about-results"
-        >
-            No Results found
-        </div>
+        <div v-else-if="noResultsFound" class="error-text">
+            <rich-text>
+                    <h1>Search for “{{ $route.query.q }}” not found.</h1>
+                    <p>
+                        We can’t find the term you are looking for on this page, but we're
+                        here to help. <br/> Try searching the whole site from <a
+                                href="https://library.ucla.edu"
+                            >UCLA Library Home</a>, or try one of the these regularly visited links:
+                    </p>
+                    <ul>
+                        <li>
+                            <a
+                                href="https://www.library.ucla.edu/research-teaching-support/research-help"
+                            >Research Help</a>
+                        </li>
+                        <li>
+                            <a
+                                href="/help/services-resources/ask-us"
+                            >Ask Us</a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://www.library.ucla.edu/use/access-privileges/disability-resources"
+                            >Accessibility Resources</a>
+                        </li>
+                    </ul>
+                </rich-text>
+            </div>
+                
         <section-wrapper theme="divider">
             <divider-way-finder color="about" />
         </section-wrapper>
@@ -374,10 +397,6 @@ export default {
 
 <style lang="scss" scoped>
 .page-news {
-    .about-results {
-        margin-top: var(--space-xl);
-        margin-bottom: var(--space-l);
-    }
     .search-margin {
         margin: var(--space-2xl) auto;
     }
