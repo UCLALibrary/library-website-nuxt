f08cb22d8e8543aa2af7762547d2fe4d43e95517
jendiamond <thejendiamond@gmail.com>
2023-02-02 10:22:03 -0800

    broken

--- pages/visit/locations/index.vue
+++ pages/visit/locations/index.vue
@@ -1,3 +1,5 @@
+newsIndex
+
 <template lang="html">
     <main
         id="main"
@@ -7,7 +9,8 @@
             :title="page.title"
             :text="page.text"
         />
-        <h3>{{ }}</h3>
+
+        <!-- SEARCH -->
         <search-generic
             search-type="visit"
             :filters="searchFilters"
@@ -30,6 +33,76 @@
             />
         </section-wrapper>
 
+        <h3>HITS -- {{ hits }}</h3>
+
+        <!-- RESULTS-->
+        <section-wrapper
+            v-if="
+                page &&
+                    parsedUclaLibraries &&
+                    parsedUclaLibraries.length &&
+                    hits.length == 0 &&
+                    !noResultsFound
+            "
+        >
+            <section-teaser-card :items="parsedUclaLibraries" />
+        </section-wrapper>
+
+        <section-wrapper v-else-if="hits && hits.length > 0">
+            <div
+                v-if="$route.query.q"
+                class="about-results"
+            >
+                Displaying {{ hits.length }} results for
+                <strong><em>“{{ $route.query.q }}”</em></strong>
+            </div>
+            <div
+                v-else
+                class="about-results"
+            >
+                Displaying {{ hits.length }} results
+            </div>
+            <section-teaser-card :items="parseHitsResults" />
+        </section-wrapper>
+
+        <!-- NO RESULTS-->
+        <section-wrapper v-else>
+            <div class="error-text">
+                <rich-text>
+                    <h1>Search for “{{ $route.query.q }}” not found.</h1>
+                    <p>
+                        We can’t find the term you are looking for on this page,
+                        but we're here to help. <br>
+                        Try searching the whole site from
+                        <a href="https://library.ucla.edu">UCLA Library Home</a>, or try one of the these regularly visited links:
+                    </p>
+                    <ul>
+                        <li>
+                            <a
+                                href="https://www.library.ucla.edu/research-teaching-support/research-help"
+                            >Research Help</a>
+                        </li>
+                        <li>
+                            <a href="/help/services-resources/ask-us">Ask Us</a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://www.library.ucla.edu/use/access-privileges/disability-resources"
+                            >Accessibility Resources</a>
+                        </li>
+                    </ul>
+                </rich-text>
+            </div>
+        </section-wrapper>
+
+        <section-wrapper>
+            <divider-way-finder
+                class="divider-way-finder"
+                color="visit"
+            />
+        </section-wrapper>
+
+        <!-- UCLA LIBRARIES -->
         <section-wrapper
             v-if="uclaLibraries"
             section-title="UCLA Library Locations"
@@ -45,6 +118,8 @@
             />
         </section-wrapper>
 
+
+        <!-- AFFILIATE LIBRARIES -->
         <section-wrapper
             v-if="affiliateLibraries && showOtherCampus"
             section-title="Other Campus Libraries & Archives"
@@ -78,9 +153,12 @@ import _get from "lodash/get"
 // GQL
 import LOCATIONS_LIST from "~/gql/queries/LocationsList"
 
-// UTILITIES
+// SEARCH UTILITIES
 import getListingFilters from "~/utils/getListingFilters"
 import config from "~/utils/searchConfig"
+import queryFilterHasValues from "~/utils/queryFilterHasValues"
+
+// HELPERS
 import parseAddress from "~/utils/parseAddress"
 import parseAmenities from "~/utils/parseAmenities"
 import removeTags from "~/utils/removeTags"
@@ -117,41 +195,48 @@ export default {
         }
     },
     async fetch() {
-        this.locations = []
+        this.uclaLibraries= []
         this.hits = []
+
         if (
             (this.$route.query.q && this.$route.query.q !== "") ||
-            this.$route.query.filters
+            (this.$route.query.filters &&
+                queryFilterHasValues(
+                    this.$route.query.filters,
+                    config.locationsList.filters
+                ))
         ) {
             if (!this.page.title) {
                 const data = await this.$graphql.default.request(LOCATIONS_LIST)
                 console.log("data for masthead:" + data)
                 this.page["title"] = _get(data, "entry.title", "")
-                this.page["text"] = _get(data, "entry.text", "")
+                //this.page["text"] = _get(data, "entry.text", "")
             }
+
             let query_text = this.$route.query.q || "*"
-
-            console.log("in router query in async data")
-
+            // console.log("in router query in async data")
+            // console.log("PAGE-TITLE" + this.page["title"])
             const results = await this.$dataApi.keywordSearchWithFilters(
                 query_text,
                 config.locationsList.searchFields,
                 "sectionHandle:location",
                 JSON.parse(this.$route.query.filters) || {},
                 config.locationsList.sortField,
+                config.locationsList.orderBy,
                 config.locationsList.resultFields,
                 config.locationsList.filters
             )
-            console.log("getsearchdata method:" + JSON.stringify(results))
+            console.log("GETSEARCHDATA method:" + JSON.stringify(results))
 
-            this.locations = []
+            this.uclaLibraries = []
             this.hits = []
             if (results && results.hits && results.hits.total.value > 0) {
                 this.hits = results.hits.hits
-                this.locations = []
+                this.uclaLibraries = []
+                console.log("HITS" + this.hits)
             } else {
                 this.hits = []
-                this.locations = []
+                this.uclaLibraries = []
             }
             this.searchGenericQuery = {
                 queryText: this.$route.query.q || "",
@@ -164,7 +249,7 @@ export default {
             this.hits = []
             // if route queries are empty fetch data from craft
             const data = await this.$graphql.default.request(LOCATIONS_LIST)
-            // console.log("data:" + data)
+            // console.log("DATA: :" + data)
             this.page = _get(data, "entry", {})
             this.locations = _get(data, "entries", [])
         }
@@ -226,10 +311,10 @@ export default {
     watch: {
         "$route.query": "$fetch",
         "$route.query.q"(newValue) {
-            console.log("watching querytEXT:" + newValue)
+            console.log("WATCHING querytEXT:" + newValue)
         },
         "$route.query.filters"(newValue) {
-            console.log("watching filters:" + newValue)
+            console.log("WATCHING filters:" + newValue)
         },
     },
     async mounted() {
@@ -240,39 +325,39 @@ export default {
         showMoreOtherCampusLibrary() {
             this.showOtherCampus = !this.showOtherCampus
         },
-        queryFilterHasValues() {
-            if (!this.$route.query.filters) return false
-            let routeQueryFilters = JSON.parse(this.$route.query.filters)
-            console.log(
-                "is route query exixts:" + JSON.stringify(routeQueryFilters)
-            )
-            let configFilters = config.locationsList.filters
-            for (const filter of configFilters) {
-                if (
-                    Array.isArray(routeQueryFilters[filter.esFieldName]) &&
-                    routeQueryFilters[filter.esFieldName].length > 0
-                ) {
-                    console.log(
-                        "why is this true is Array: " +
-                            routeQueryFilters[filter.esFieldName]
-                    )
-                    return true
-                } else if (
-                    routeQueryFilters[filter.esFieldName] &&
-                    !Array.isArray(routeQueryFilters[filter.esFieldName]) &&
-                    routeQueryFilters[filter.esFieldName] != ""
-                ) {
-                    console.log(
-                        "why is this truenot Array: " +
-                            routeQueryFilters[filter.esFieldName] +
-                            "config filter name is " +
-                            filter.esFieldName
-                    )
-                    return true
-                }
-            }
-            return false
-        },
+        // queryFilterHasValues() {
+        //     if (!this.$route.query.filters) return false
+        //     let routeQueryFilters = JSON.parse(this.$route.query.filters)
+        //     console.log(
+        //         "is route query exixts:" + JSON.stringify(routeQueryFilters)
+        //     )
+        //     let configFilters = config.locationsList.filters
+        //     for (const filter of configFilters) {
+        //         if (
+        //             Array.isArray(routeQueryFilters[filter.esFieldName]) &&
+        //             routeQueryFilters[filter.esFieldName].length > 0
+        //         ) {
+        //             console.log(
+        //                 "why is this true is Array: " +
+        //                     routeQueryFilters[filter.esFieldName]
+        //             )
+        //             return true
+        //         } else if (
+        //             routeQueryFilters[filter.esFieldName] &&
+        //             !Array.isArray(routeQueryFilters[filter.esFieldName]) &&
+        //             routeQueryFilters[filter.esFieldName] != ""
+        //         ) {
+        //             console.log(
+        //                 "why is this truenot Array: " +
+        //                     routeQueryFilters[filter.esFieldName] +
+        //                     "config filter name is " +
+        //                     filter.esFieldName
+        //             )
+        //             return true
+        //         }
+        //     }
+        //     return false
+        // },
         async setFilters() {
             const searchAggsResponse = await this.$dataApi.getAggregations(
                 config.locationsList.filters,
@@ -291,25 +376,16 @@ export default {
                 // console.log(obj["_source"]["image"])
                 return {
                     ...obj["_source"],
-                    description: obj["_source"].text,
-                    to:
-                        obj["_source"].externalResourceUrl
-                            ? obj["_source"].buttonUrl[0].buttonUrl
-                            : `/${obj["_source"].uri}`,
+                    to: obj["_source"].uri ? `/${obj["_source"].uri}` : null,
                     image: _get(obj["_source"], "heroImage[0].image[0]", null),
+                    address: parseAddress(obj["_source"])[0],
+                    addressLink: `https://map.ucla.edu/?id=${obj.campusMapId}&e=true`,
+                    amenities:
+                        obj["_source"].amenitiesIcons.length > 0
+                            ? parseAmenities(obj["_source"])
+                            : null,
+                    isUclaLibrary: true,
                 }
-
-                // to: obj.uri ? `/${obj.uri}` : null,
-                //     image: _get(obj, "heroImage[0].image[0]", null),
-                //     address: parseAddress(obj)[0],
-                //     addressLink: `https://map.ucla.edu/?id=${obj.campusMapId}&e=true`,
-                //     amenities:
-                //         obj.amenitiesIcons.length > 0
-                //             ? parseAmenities(obj)
-                //             : null,
-                //     isUclaLibrary: true,
-
-
             })
         },
         getSearchData(data) {
