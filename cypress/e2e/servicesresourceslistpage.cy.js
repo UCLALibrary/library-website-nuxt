24a35b183f3eb952e555b8e0e3774a28ebf55e60
Ashton Prigge <aprigge@library.ucla.edu>
2023-02-07 14:12:14 -0800

    update test

--- cypress/e2e/servicesresourceslistpage.cy.js
+++ cypress/e2e/servicesresourceslistpage.cy.js
@@ -5,11 +5,11 @@ describe("All Services & Resources Listing page", () => {
         cy.percySnapshot({ widths: [768, 992, 1200] })
     })
     it("Search Found", () => {
-        cy.visit("help/services-resources?q=service")
+        cy.visit("help/services-resources?q=test")
         cy.get(".logo-ucla").should("be.visible")
         cy.get("div.input-container > input[type=text]").should(
             "have.value",
-            "service"
+            "test"
         )
         // cy.get("div.about-results").invoke("text").should("not.be.empty")
         //cy.percySnapshot({ widths: [768, 992, 1200] })
