aa724b3364613200fd021f63dea973d1246568d4
jendiamond <thejendiamond@gmail.com>
2023-02-07 09:34:45 -0800

    fix: sitesearch test

--- cypress/e2e/sitesearch.cy.js
+++ cypress/e2e/sitesearch.cy.js
@@ -2,13 +2,12 @@ describe("Site Search page", () => {
     it("Search blank", () => {
         cy.visit("/search-site")
         cy.get("h1.title").should("contain", "Search Results")
-       
-        cy.get("div.error-text > div.rich-text > h1").should('contain',"Search for “” not found.")
+
+        cy.get("div > h2").should('contain',"Search for “” not found.")
 
     })
     it("Search all", () => {
         cy.visit("/search-site?q=*")
         cy.get(".about-results").should('be.visible')
     })
-
 })
