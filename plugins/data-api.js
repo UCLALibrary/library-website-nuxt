41fca16707dc6d52b99cfbb2cc15ee0a7ef780c1
pghorpade <pghorpade@library.ucla.edu>
2023-02-07 16:28:50 -0800

    fix: GH action iplist keeps changing so cannot create alias per pull request

--- plugins/data-api.js
+++ plugins/data-api.js
@@ -27,6 +27,7 @@ export default function ({ $config }, inject) {
                             "title^4",
                             "summary^3",
                             "text^3",
+                            "fullText^3",
                             "richText^2",
                         ],
                         fuzziness: "auto",
