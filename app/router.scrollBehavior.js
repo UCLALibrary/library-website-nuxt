8206de235fa0739a2bf73002240cc47e95ce6196
pghorpade <pghorpade@library.ucla.edu>
2023-02-21 16:26:27 -0800

    fix: print the values

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -1,4 +1,7 @@
 export default function (to, from, savedPosition) {
+    console.log("ScrollBehaviour to:"+to)
+    console.log("ScrollBehaviour from:"+from)
+    console.log("ScrollBehaviour savedPosition:"+savedPosition)
     if (savedPosition) {
         return savedPosition
     } else {
