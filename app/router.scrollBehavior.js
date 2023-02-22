e90d40f7d94a1cc165660f98c3b281bfd6630ac7
pghorpade <pghorpade@library.ucla.edu>
2023-02-22 14:02:51 -0800

    fix: added save postion for non listing pages and if y offset is less than 2000px

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -2,16 +2,66 @@ export default async function (to, from, savedPosition) {
     console.log("ScrollBehaviour to:"+JSON.stringify(to))
     console.log("ScrollBehaviour from:"+JSON.stringify(from))
     console.log("ScrollBehaviour savedPosition:"+JSON.stringify(savedPosition))
-    /* if (savedPosition) {
-        
-        // eslint-disable-next-line no-unused-vars
-        return new Promise((resolve, reject) => {
+    
+
+    const findEl = async (hash, x = 0) => {
+        return (
+            document.querySelector(hash) ||
+        new Promise((resolve) => {
+            if (x > 0) {
+                return resolve(document.querySelector("#app"))
+            }
             setTimeout(() => {
-                resolve(savedPosition)
-            }, 500)
+                resolve(findEl(hash, 1))
+            }, 1000)
         })
-    } else {*/
-    // Forcing to scroll on top for now. 
-    return { x: 0, y: 0 }
-    /* }*/
+        )
+    }
+    const notListingPage = (fullPath) =>{
+        switch (fullPath) {
+            case "/about/staff":
+                return true
+             
+            case "/about/news":
+                return true
+             
+            case "/visit/locations":
+                return false
+             
+            case "/collections/explore":
+                return false
+            case "/visit/events-exhibitions":
+                return false
+            case "/search-site":
+                return false
+    
+            default:
+                return true
+              
+        }
+      
+    }
+
+    if (to.hash) {
+        console.log("to hash:"+ to.hash)
+        const el = await findEl(to.hash)
+        if(el) {
+            if ("scrollBehavior" in document.documentElement.style) {
+                console.log("hash path hit scroll to")
+                return window.scrollTo({ top: el.offsetTop, behavior: "smooth" })
+            } else {
+                return window.scrollTo(0, el.offsetTop)
+            }
+        }
+    } else {
+        if(notListingPage(to.fullPath)) {
+            if (savedPosition && savedPosition["y"] < 2000) {
+                return savedPosition
+            }
+        }
+    }
+    return { x: 0, y: 0, behavior: "smooth" }
+  
 }
+
+
