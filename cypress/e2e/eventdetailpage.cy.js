3b47344436ce07b2f375a292101747d9646096a7
Andy Wallace <andrewwallace@library.ucla.edu>
2023-04-07 10:37:55 -0700

    ci: cypress test for EventSeries detail page

---
+++ cypress/e2e/eventdetailpage.cy.js
@@ -0,0 +1,23 @@
+describe("Event Detail page", () => {
+    it("Visit a Event Detail Page", () => {
+        cy.visit("/visit/events-exhibitions/marion-davies-captain-of-her-soul")
+
+        // Branding
+        cy.get(".logo-ucla").should("be.visible")
+        
+        // Breadcrumbs
+        cy.get(".nav-breadcrumb .parent-page-url").should("contain", "Events & Exhibitions")
+            .and("have.attr", "href", "/visit/events-exhibitions")
+        cy.get(".nav-breadcrumb .current-page-title").should("contain", "Marion Davies: Captain of Her Soul")
+
+        cy.get(".banner-header h1.title").should("contain", "Marion Davies: Captain of Her Soul")
+
+        cy.percySnapshot({ widths: [768, 992, 1200] })
+    })
+
+    context("When the page doesn't exist", () => {
+        it("Raises a 404 error", () => {
+            cy.request({url: "/help/no_entry", failOnStatusCode: false}).its("status").should("equal", 404)
+        })
+    })
+})
