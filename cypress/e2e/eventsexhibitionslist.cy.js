ad5b1642881ef9c6fb0bcb7a746018a69e2fb625
pghorpade <pghorpade@library.ucla.edu>
2023-05-02 12:04:01 -0700

    tests: fix tests name

--- cypress/e2e/eventsexhibitionslist.cy.js
+++ cypress/e2e/eventsexhibitionslist.cy.js
@@ -8,7 +8,7 @@ describe("Events & Exhibitions List page", () => {
         cy.get("h1.title").should("contain", "Events & Exhibitions")
         cy.percySnapshot({ widths: [768, 992, 1200] })
     })
-    it("Visit Locations Listing page filter by category", () => {
+    it("Visit Events & Exhibitions Listing page filter by event type", () => {
         cy.visit('/visit/events-exhibitions/?q&filters=%7B"eventType.title.keyword"%3A%5B"Workshop"%5D%7D')
 
         cy.get ('h2.about-results').should("be.visible")
