23bea5198dff2feb4857330c9d0bb5329ee6c7d0
pghorpade <pghorpade@library.ucla.edu>
2023-08-21 18:13:23 -0700

    chore: install cypress and confugure and test homepage basic test

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress")
+
+module.exports = defineConfig({
+    defaultCommandTimeout: 60000,
+    video: false,
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        baseUrl: "http://localhost:3000",
+    },
+})
