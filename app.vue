293c58ef8cb9e9359c70415558cdd130f6ed87df
Parinita Mulak <pghorpade@library.ucla.edu>
2023-08-23 11:59:23 -0700

    feat: Use local vite vue3.x component library using pnpm link --global (#729)

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <hr />
+    <h2>DividerGeneral Component</h2>
+    <DividerGeneral />
+    <DividerGeneral :is-bold="true" />
+    <hr />
+    <h2>DividerWayFinder Component</h2>
+    <DividerWayFinder color="visit" />
+    <hr />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
+    // when using local pnpm link with component library uncomment this line
+    vite: {
+        server: {
+            fs: {
+                strict: false,
+            },
+        },
+    },
+    /*
+     ** Global CSS
+     */
+    css: [
+        /*"ucla-library-design-tokens/scss/fonts.scss",
+        "ucla-library-design-tokens/scss/app-global.scss",
+        "~/assets/styles/global.scss",*/
+        // once we have npm package for vite vue3 component library use
+        // "ucla-library-website-components-vue3.x/style.css"
+        // use the following line when using pnpm link --global ucla-library-website-components-vue3.x
+        "ucla-library-website-components-vue3.x/dist/style.css",
+    ],
 })
