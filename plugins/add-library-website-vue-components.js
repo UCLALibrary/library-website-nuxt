b7c899527e102e75d4297ec3bc109f31a63a2421
pghorpade <pghorpade@library.ucla.edu>
2023-08-23 11:56:55 -0700

    feat: Use local vite vue3.x component library using pnpm link --global

---
+++ plugins/add-library-website-vue-components.js
@@ -0,0 +1,8 @@
+import UCLALibraryWebsiteComponents from "ucla-library-website-components-vue3.x"
+
+export default defineNuxtPlugin((nuxtApp) => {
+    // Doing something with nuxtApp
+    console.log("print external component: " + UCLALibraryWebsiteComponents)
+    nuxtApp.vueApp.use(UCLALibraryWebsiteComponents)
+    console.log("plugin UCLALibraryWebsiteComponents is locked and loaded")
+})
