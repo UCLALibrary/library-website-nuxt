1dadb83d4c184ee5b86e1d352cb75d198fdc1466
pghorpade <pghorpade@library.ucla.edu>
2023-08-24 11:32:45 -0700

    fix: layout initial commit

--- app.vue
+++ app.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
-    <hr />
-    <h2>DividerGeneral Component</h2>
-    <DividerGeneral />
-    <DividerGeneral :is-bold="true" />
-    <hr />
-    <h2>DividerWayFinder Component</h2>
-    <DividerWayFinder color="visit" />
-    <hr />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+    <div :class="classes">
+        <VueSkipTo to="#main" label="Skip to main content" />
+        <!--header-smart />
+
+        <section-wrapper class="section-alert" theme="divider">
+            <site-notification-alert v-if="libraryAlert" class="library-alert" v-bind="libraryAlert" />
+        </section-wrapper-->
+
+        <main>
+            <slot></slot>
+        </main>
+
+        <!--footer>
+            <footer-primary :form="true" />
+            <footer-sock />
+        </footer-->
+        <div id="libchat_5a44dfe7cc29aaee5bba635ab13fa753" />
+    </div>
+</template>
