9d89707d34a860246a7833aaca6fa81428058b31
pghorpade <pghorpade@library.ucla.edu>
2023-08-25 12:23:43 -0700

    chore: add layouts , seo and meta tags

--- layouts/impact.vue
+++ layouts/impact.vue
@@ -1,3 +1,66 @@
+<script setup>
+// MOCK DATA
+import * as MOCK_API from "~/data/mock-api.json"
+
+const footerPrimaryItems = [...MOCK_API.shortLinks]
+const parsedItems = computed(() => {
+    // Restructuring item to support text key
+    return footerPrimaryItems.map((obj) => {
+        return {
+            ...obj,
+            text: obj.name,
+        }
+    })
+})
+const pressItems = [{ ...MOCK_API.links[0] }]
+const parsedPressItems = computed(() => {
+    // Restructuring item to support text key
+    return pressItems.map((obj) => {
+        return {
+            ...obj,
+            text: obj.name,
+        }
+    })
+})
+
+const classes = computed(() => [
+    "layout",
+    "layout-default",
+    // { "has-scrolled": this.$store.state.sTop },
+    // { "has-scrolled-past-header": this.$store.state.sTop >= 150 },
+])
+
+useHead({
+    titleTemplate: "%s | UCLA Library",
+})
+</script>
 <template>
-    <slot></slot>
+    <div :class="classes">
+        <!--nav-primary class="primary" /-->
+        <main>
+            <slot></slot>
+        </main>
+
+        <!--footer-primary :social-items="parsedItems" :press-items="parsedPressItems" :form="false" /-->
+    </div>
 </template>
+<style lang="scss" scoped>
+.layout-default {
+    min-height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    align-content: center;
+    align-items: center;
+
+    >* {
+        width: 100%;
+    }
+
+    .page {
+        flex: 1 1 auto;
+    }
+}
+</style>
