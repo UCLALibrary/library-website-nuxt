fa610d527320b293c005e4430a06ec43f27f58fd
pghorpade <pghorpade@library.ucla.edu>
2023-09-15 14:13:20 -0700

    fix: remove semicolon added by prettier vscode extension

--- plugins/browser-events.client.js
+++ plugins/browser-events.client.js
@@ -3,25 +3,25 @@ function onResize(store, event = {}) {
     const dimensions = {
         height: window.innerHeight,
         width: window.innerWidth,
-    };
-    store.setWindowDimension(dimensions.width, dimensions.height);
+    }
+    store.setWindowDimension(dimensions.width, dimensions.height)
 }
 // eslint-disable-next-line no-unused-vars
 function onScroll(store, event = {}) {
     store.setSTop(
         window.pageYOffset || window.scrollY || document.body.scrollTop
-    );
+    )
 }
 export default defineNuxtPlugin((nuxtApp) => {
-    const windowSizeGlobals = useGlobals(nuxtApp.$pinia);
+    const windowSizeGlobals = useGlobals(nuxtApp.$pinia)
     window.addEventListener("resize", (event) => {
-        onResize(windowSizeGlobals, event);
-    });
+        onResize(windowSizeGlobals, event)
+    })
     window.addEventListener("scroll", (event) => {
-        onScroll(windowSizeGlobals, event);
-    });
+        onScroll(windowSizeGlobals, event)
+    })
 
     // Kick off events to populate store with intial values
-    onResize(windowSizeGlobals);
-    onScroll(windowSizeGlobals);
-});
+    onResize(windowSizeGlobals)
+    onScroll(windowSizeGlobals)
+})
