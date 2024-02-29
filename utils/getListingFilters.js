5cbb3cfd251e79f876a96c084996aa14e54c162a
tinuola <tinuola@users.noreply.github.com>
2024-02-29 12:52:39 -0800

    feat: port news index and util functions

---
+++ utils/getListingFilters.js
@@ -0,0 +1,23 @@
+/**
+ * Take a URI and figure out what "section" of the site it is pointing too
+ * @param {Array} timeLineData
+ * @returns {Array}
+ */
+
+import _get from 'lodash/get'
+
+function getListingFilters(searchAggsResponse, filterFields) {
+  return filterFields.map((field) => {
+    return {
+      label: field.label,
+      esFieldName: field.esFieldName,
+      inputType: field.inputType,
+      items: _get(searchAggsResponse, `${field.label}.buckets`, []).map(
+        (value) => {
+          return { name: value.key }
+        }),
+    }
+  })
+}
+
+export default getListingFilters
