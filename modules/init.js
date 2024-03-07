848b2f49a8137177ad5bd5430515c777420f53b2
pghorpade <pghorpade@library.ucla.edu>
2024-03-06 17:41:49 -0800

    feat: add sitemap nuxt module

--- modules/init.js
+++ modules/init.js
@@ -5,54 +5,54 @@ export default defineNuxtModule({
 
   setup(options, nuxt) {
     console.log('Nuxt module start ')
-    if (process.env.NODE_ENV !== "development") {
+    if (process.env.NODE_ENV !== 'development') {
     // const endpoint = nuxt.options.runtimeConfig.public.craftGraphqlURL
-    nuxt.hooks.hook('build:before', async (nitro) => {
-      console.log('Ready to create library temp index...')
-      const timeElapsed = Date.now()
-      const now = new Date(timeElapsed)
+      nuxt.hooks.hook('build:before', async (nitro) => {
+        console.log('Ready to create library temp index...')
+        const timeElapsed = Date.now()
+        const now = new Date(timeElapsed)
 
-      const esLibraryIndexTemp = `${nuxt.options.runtimeConfig.public.esIndexPrefix}-${now.toISOString().toLowerCase().replaceAll(':', '-')}`
-      console.log('Index named:' + esLibraryIndexTemp)
-      try {
-        const response = await fetch(`${nuxt.options.runtimeConfig.public.esURL}/${esLibraryIndexTemp}`, {
-          headers: {
-            Authorization: `ApiKey ${nuxt.options.runtimeConfig.esWriteKey}`,
-            'Content-Type': 'application/json',
-          },
-          method: 'PUT',
-          body: JSON.stringify({
-            settings: {
-              analysis: {
-                analyzer: {
-                  default: {
-                    type: 'custom',
-                    tokenizer: 'standard',
-                    filter: ['stemmer', 'lowercase', 'stop', 'asciifolding'],
+        const esLibraryIndexTemp = `${nuxt.options.runtimeConfig.public.esIndexPrefix}-${now.toISOString().toLowerCase().replaceAll(':', '-')}`
+        console.log('Index named:' + esLibraryIndexTemp)
+        try {
+          const response = await fetch(`${nuxt.options.runtimeConfig.public.esURL}/${esLibraryIndexTemp}`, {
+            headers: {
+              Authorization: `ApiKey ${nuxt.options.runtimeConfig.esWriteKey}`,
+              'Content-Type': 'application/json',
+            },
+            method: 'PUT',
+            body: JSON.stringify({
+              settings: {
+                analysis: {
+                  analyzer: {
+                    default: {
+                      type: 'custom',
+                      tokenizer: 'standard',
+                      filter: ['stemmer', 'lowercase', 'stop', 'asciifolding'],
+                    },
+                    default_search: {
+                      type: 'custom',
+                      tokenizer: 'standard',
+                      filter: ['stemmer', 'lowercase', 'stop', 'asciifolding'],
+                    }
                   },
-                  default_search: {
-                    type: 'custom',
-                    tokenizer: 'standard',
-                    filter: ['stemmer', 'lowercase', 'stop', 'asciifolding'],
-                  }
-                },
+                }
               }
-            }
-          }),
-        })
-        const body = await response.text()
-        const testJson = JSON.parse(body)
-        nuxt.options.tempIndex = esLibraryIndexTemp
-        nuxt.options.runtimeConfig.public.esTempIndex = esLibraryIndexTemp
-        console.log('Index created:' + JSON.stringify(testJson))
-        console.log('Elastic Search index created succesfully!')
-      } catch (err) {
-        console.error('Error:', err)
-        console.error('Response body:', body)
-        throw err
-      }
-    })
-  }
+            }),
+          })
+          const body = await response.text()
+          const testJson = JSON.parse(body)
+          nuxt.options.tempIndex = esLibraryIndexTemp
+          nuxt.options.runtimeConfig.public.esTempIndex = esLibraryIndexTemp
+          console.log('Index created:' + JSON.stringify(testJson))
+          console.log('Elastic Search index created succesfully!')
+        } catch (err) {
+          console.error('Error:', err)
+          console.error('Response body:', body)
+          throw err
+        }
+      })
+    }
     console.log('Nuxt module end ')
   }
 })
