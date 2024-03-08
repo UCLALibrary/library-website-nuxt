b691652678df236416347e6d5d6c8fd8cdc0b8e4
tinuola <tinuola@users.noreply.github.com>
2024-03-08 14:43:28 -0800

    feat: add catchall content page, getHeaders plugin

---
+++ pages/[...slug].vue
@@ -0,0 +1,154 @@
+<!-- eslint-disable no-console -->
+<script setup>
+// HELPERS
+import _get from 'lodash/get'
+
+// GQL
+import GENERAL_CONTENT_DETAIL from '../gql/queries/GeneralContentDetail.gql'
+
+const { $graphql } = useNuxtApp()
+
+const route = useRoute()
+
+const path = route.params.slug
+
+console.log(path)
+
+const { data, error } = await useAsyncData('general-content', async () => {
+  // const path = params.pathMatch.replace(/^\/|\/$/g, '') // trim initial and/or final slashes
+
+  const data = await $graphql.default.request(GENERAL_CONTENT_DETAIL, path)
+
+  console.log('General Content page path is: ' + path)
+
+  // if (data.value && data.value.entry && data.value.entry.slug) {
+  //   $elasticsearchplugin.index(data.value.entry, path.replaceAll('/', '--'))
+  // } else {
+  //   error({ statusCode: 404, message: 'Page not found' })
+  // }
+
+  return data
+})
+
+console.log(data.value)
+
+if (error.value) {
+  throw createError({
+    statusCode: 404, statusMessage: 'Page not found.' + error.value, fatal: true
+  })
+}
+
+if (!data.value.entry) {
+  console.log('no data throw impact report main story error')
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Page Not Found'
+  })
+}
+
+const page = ref(_get(data.value, 'entry', {}))
+let h2Array = ref([]) // anchor tags
+
+useHead({
+  title: page.value ? page.value.title : '... loading'
+})
+
+const parseParentPageURL = computed(() => {
+  if (page.value.parent && page.value.parent.uri)
+    return `/${page.value.parent.uri}`
+
+  return '/'
+})
+
+const parseParentTitle = computed(() => {
+  if (page.value.parent && page.value.parent.title)
+    return page.value.parent.title
+
+  return 'Home'
+})
+
+const parsedButtonText = computed(() => {
+  return _get(page.value, 'button[0].buttonText', '')
+})
+
+const parsedButtonTo = computed(() => {
+  return _get(page.value, 'button[0].buttonUrl', '')
+})
+
+const { $getHeaders } = useNuxtApp()
+
+onMounted(() => {
+  // Call the plugin method to get the .section-header2 and .section-header3 elements
+  h2Array = $getHeaders.getHeadersMethod()
+})
+</script>
+
+<template lang="html">
+  <main
+    id="main"
+    class="page page-general-content"
+  >
+    <nav-breadcrumb
+      v-if="page"
+      :title="page.title"
+      class="breadcrumb"
+      :to="parseParentPageURL"
+      :parent-title="parseParentTitle"
+    />
+
+    <banner-text
+      v-if="page && (!page.heroImage || page.heroImage.length == 0)"
+      class="banner-text"
+      :category="page.format"
+      :title="page.title"
+      :text="page.summary"
+      :button-text="parsedButtonText"
+      :to="parsedButtonTo"
+    />
+
+    <section-wrapper class="section-banner">
+      <banner-header
+        v-if="page && page.heroImage && page.heroImage.length == 1"
+        :image="page.heroImage[0].image[0]"
+        :category="page.format"
+        :title="page.title"
+        :text="page.summary"
+        :to="parsedButtonTo"
+        :prompt="parsedButtonText"
+      />
+    </section-wrapper>
+
+    <section-wrapper theme="divider">
+      <divider-way-finder
+        class="divider-way-finder"
+        color="default"
+      />
+    </section-wrapper>
+
+    <page-anchor
+      v-if="h2Array.length >= 3"
+      :section-titles="h2Array"
+    />
+
+    <flexible-blocks
+      v-if="page"
+      class="flexible-content"
+      :blocks="page.blocks"
+    />
+  </main>
+</template>
+
+<style
+  lang="scss"
+  scoped
+>
+.page-general-content {
+  :deep(.divider-way-finder) {
+    --color-border: var(--color-visit-fushia-03);
+  }
+
+  .section-banner {
+    margin-top: 0;
+  }
+}
+</style>
