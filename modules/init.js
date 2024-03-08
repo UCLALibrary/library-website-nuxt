e6a05804487072e89d56d270bc8614ed9e71adc4
pghorpade <pghorpade@library.ucla.edu>
2024-03-07 16:20:33 -0800

    fix: do not run init on nuxi prepare

--- modules/init.js
+++ modules/init.js
@@ -6,10 +6,7 @@ export default defineNuxtModule({
   setup(options, nuxt) {
     console.log('Nuxt module start ')
     console.log('process.env.NODE_ENV:' + process.env.NODE_ENV)
-    if (nuxt.options._prepare) {
-      process.exit()
-    }
-    if (process.env.NODE_ENV !== 'development') {
+    if (!nuxt.options._prepare && process.env.NODE_ENV !== 'development') {
     // const endpoint = nuxt.options.runtimeConfig.public.craftGraphqlURL
       nuxt.hooks.hook('build:before', async (nitro) => {
         console.log('Ready to create library temp index...')
