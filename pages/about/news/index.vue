81f15be12921bace2315f62c7f54e38a03ff27b0
Parinita Mulak <pghorpade@library.ucla.edu>
2024-03-13 15:05:05 -0700

    fix: font errors (#776)
    * fix: font errors
    
    * fix: lint errors

--- pages/about/news/index.vue
+++ pages/about/news/index.vue
@@ -322,7 +322,7 @@ function parseArticleCategory(categories) {
         page.featuredNews.length &&
         hits.length == 0 &&
         !noResultsFound
-        "
+      "
       class="section-no-top-margin"
     >
       <banner-featured
@@ -338,12 +338,14 @@ function parseArticleCategory(categories) {
         class="banner section-featured-banner"
       />
 
-      <divider-general v-if="page &&
-        page.featuredNews &&
-        page.featuredNews.length &&
-        hits.length == 0 &&
-        !noResultsFound
-        " />
+      <divider-general
+        v-if="page &&
+          page.featuredNews &&
+          page.featuredNews.length &&
+          hits.length == 0 &&
+          !noResultsFound
+        "
+      />
 
       <section-teaser-highlight
         v-if="parsedSectionHighlight.length"
@@ -358,7 +360,7 @@ function parseArticleCategory(categories) {
         page.featuredNews.length &&
         hits.length == 0 &&
         !noResultsFound
-        "
+      "
       theme="divider"
     >
       <divider-way-finder color="about" />
