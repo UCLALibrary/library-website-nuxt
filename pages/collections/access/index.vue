5cc68bab6b99f5e4cad3c8be5a4c1422ccd41ea2
Jess Divers <jessica.divers@library.ucla.edu>
2024-04-10 16:00:54 -0700

    feat: refactor collections-access-index to vue 3

---
+++ pages/collections/access/index.vue
@@ -0,0 +1,287 @@
+<script setup>
+// HELPERS
+import _get from "lodash/get"
+import removeTags from "../utils/removeTags"
+
+// GQL
+import ACCESS_COLLECTIONS from "../gql/queries/CollectionsAccessList.gql"
+const { $graphql } = useNuxtApp()
+
+// UTILITIES
+import config from "../utils/searchConfig"
+
+// ROUTING
+const route = useRoute()
+definePageMeta({
+  layout: 'default',
+  path: '/collections/access',
+  alias: ['/listing-collections/access'],
+})
+
+
+// ASYNC DATA
+const { data: page, error } = await useAsyncData('access-collections', async () => {
+  const data = await $graphql.default.request(ACCESS_COLLECTIONS)
+  console.log('data in fn', data)
+  // TODO: after elastic search ready, implement these
+  // if (
+  //   pageAsyncData.entry.accessCollections &&
+  //   pageAsyncData.entry.accessCollections.length > 0
+  // ) {
+  //   for (let collection of pageAsyncData.entry.accessCollections) {
+  //     console.log("Collection indexing:" + collection.slug)
+  //     console.log("Collection:" + collection)
+  //     collection.searchType = "accessCollections"
+  //     collection.to = collection.uri
+  //       ? collection.uri
+  //       : collection.externalResourceUrl
+  //     collection.category =
+  //       collection.workshopOrEventSeriesType ===
+  //         "help/services-resources"
+  //         ? "workshop"
+  //         : collection.serviceOrResourceType
+  //           ? collection.serviceOrResourceType
+  //           : collection.typeHandle === "externalResource"
+  //             ? "resource"
+  //             : collection.typeHandle === "generalContentPage"
+  //               ? "resource"
+  //               : collection.typeHandle
+  //     await $elasticsearchplugin.index(collection, collection.slug)
+  //   }
+  // }
+
+  // return {
+  //   page: _get(pageAsyncData, "entry", {}),
+  // }
+  return data
+})
+if (error.value) {
+  throw createError({
+    statusCode: 404, statusMessage: 'Page not found.', fatal: true
+  })
+}
+
+// DATA VARS
+// const page = ref(_get(data, "entry", {})) // todo check if this is correct, data = pagreAsyncData?
+const noResultsFound = ref(false)
+const hits = ref([])
+const searchGenericQuery = ref({
+  queryText: route.query.q || '',
+})
+
+// TODO after elastic search ready, implement these
+// fetchOnServer: false,
+// fetchKey: "collections-access",
+
+// FETCH
+//   async fetch() {
+//     this.hits = []
+//     if (this.$route.query.q && this.$route.query.q !== "") {
+//       const results = await this.$dataApi.keywordSearchWithFilters(
+//         this.$route.query.q || "*",
+//         config.accessCollections.searchFields,
+//         "searchType:accessCollection",
+//         [],
+//         config.accessCollections.sortField,
+//         config.accessCollections.orderBy,
+//         config.accessCollections.resultFields,
+//         []
+//       )
+//       this.hits = []
+//       if (results && results.hits && results.hits.total.value > 0) {
+//         this.hits = results.hits.hits
+//         this.noResultsFound = false
+//       } else {
+//         this.hits = []
+//         this.noResultsFound = true
+//       }
+//       this.searchGenericQuery = {
+//         queryText: this.$route.query.q || "",
+//       }
+//     } else {
+//       this.hits = []
+//       this.noResultsFound = false
+//       this.searchGenericQuery = { queryText: "" }
+//     }
+//   },
+
+// HEAD
+useHead({
+  title: page.value ? page.value.title : '... loading',
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: removeTags(page.value.text)
+    },
+  ],
+})
+
+// COMPUTED 
+const parsedAccessCollections = computed(() => {
+  return page.value.accessCollections.map((obj) => {
+    return {
+      ...obj,
+      to: obj.externalResourceUrl
+        ? obj.externalResourceUrl
+        : `/${obj.uri}`,
+    }
+  })
+})
+const parsedAssociatedTopics = computed(() => {
+  return page.value.associatedTopics.map((obj) => {
+    return {
+      ...obj,
+      to: obj.externalResourceUrl
+        ? obj.externalResourceUrl
+        : `/${obj.to}`,
+    }
+  })
+})
+const parseHitsResults = computed(() => {
+  console.log("ParseHitsResults checking results data:" + JSON.stringify(hits))
+  return parseHits(hits)
+})
+
+// WATCHERS - TODO: after elastic search ready, implement these
+// watch(() => route.query, async (newValue) => { 
+//   // await $fetch(newValue)
+// })
+// watch(() => route.query.q, (newValue) => {
+//   console.log("watching queryTEXT: " + newValue)
+//   // if (newValue === "") this.hits = []
+// }
+
+// METHODS
+function parseHits(hits) {
+  return hits.value.map((obj) => {
+    console.log(
+      "What should the category be?:" +
+      obj["_source"].sectionHandle
+    )
+    return {
+      ...obj["_source"],
+      to: obj["_source"].externalResourceUrl
+        ? obj["_source"].externalResourceUrl
+        : `/${obj["_source"].uri}`,
+    }
+  })
+}
+function getSearchData(data) {
+  route.push({
+    path: "/collections/access",
+    query: {
+      q: data.text,
+    },
+  })
+}
+</script>
+<template lang="html">
+  <main
+    id="main"
+    class="page page-collections-access"
+  >
+    <nav-breadcrumb
+      to="/collections"
+      :title="page.entry.title"
+      parent-title="Collections"
+      class="secondary-breadcrumb"
+    />
+
+    <masthead-secondary
+      :title="page.entry.title"
+      :text="page.entry.text"
+      class="secondary"
+    />
+    <h1> HALLO JESS</h1>
+
+    <!-- <search-generic
+      search-type="default"
+      class="generic-search"
+      :search-generic-query="searchGenericQuery"
+      placeholder="ACCESS COLLECTIONS"
+      @search-ready="getSearchData"
+    />
+
+    <section-wrapper theme="divider">
+      <divider-way-finder class="search-margin" />
+    </section-wrapper>
+
+    <section-wrapper v-show="page.accessCollections && hits.length == 0 && !noResultsFound
+        ">
+      <section-cards-with-illustrations
+        class="section"
+        :items="parsedAccessCollections"
+        :is-horizontal="true"
+      />
+    </section-wrapper>
+    <section-wrapper v-show="hits && hits.length > 0">
+      <h2
+        v-if="$route.query.q"
+        class="about-results"
+      >
+        Displaying {{ hits.length }} results for
+        <strong><em>“{{ $route.query.q }}</em></strong>”
+      </h2>
+      <h2
+        v-else
+        class="about-results"
+      >
+        Displaying {{ hits.length }} results
+      </h2>
+      <section-cards-with-illustrations
+        class="section"
+        :items="parseHitsResults"
+        :is-horizontal="true"
+      />
+    </section-wrapper>
+
+    <section-wrapper
+      v-show="noResultsFound"
+      class="section-no-top-margin"
+    >
+      <div class="error-text">
+        <rich-text>
+          <h2>Search for “{{ $route.query.q }}” not found.</h2>
+          <p>
+            We can’t find the term you are looking for on this page,
+            but we're here to help. <br>
+            Try searching the whole site from
+            <a href="https://library.ucla.edu">UCLA Library Home</a>, or try one of the these regularly visited links:
+          </p>
+          <ul>
+            <li>
+              <a href="https://www.library.ucla.edu/research-teaching-support/research-help">Research Help</a>
+            </li>
+            <li>
+              <a href="/help/services-resources/ask-us">Ask Us</a>
+            </li>
+            <li>
+              <a href="https://www.library.ucla.edu/use/access-privileges/disability-resources">Accessibility
+                Resources</a>
+            </li>
+          </ul>
+        </rich-text>
+      </div>
+    </section-wrapper>
+
+    <section-wrapper>
+      <divider-way-finder class="divider divider-way-finder" />
+    </section-wrapper> -->
+
+    <!-- <section-wrapper>
+      <section-cards-with-illustrations
+        class="section"
+        :items="parsedAssociatedTopics"
+        section-title="Associated Topics"
+        to="/help/services-resources"
+        button-text="All services & Resources"
+        :is-horizontal="false"
+      />
+    </section-wrapper> -->
+  </main>
+</template>
+<style
+  lang="scss"
+  scoped
+></style>
