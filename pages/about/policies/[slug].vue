46e9f585d84cbf8f5e95de260bd2dd0521347744
tinuola <tinuola@users.noreply.github.com>
2024-04-10 16:07:32 -0700

    feat: add policies slug page

---
+++ pages/about/policies/[slug].vue
@@ -0,0 +1,133 @@
+<script setup>
+import { onMounted } from 'vue'
+
+// HELPERS
+import _get from "lodash/get"
+import removeTags from "../utils/removeTags"
+
+// GQL
+import POLICY_DETAIL from "../gql/queries/PolicyDetail.gql"
+
+const { $graphql } = useNuxtApp()
+const route = useRoute()
+const config = useRuntimeConfig()
+
+const { data, error } = await useAsyncData(`policy-detail-${route.params.slug}`, async () => {
+
+  if (
+    route.params.slug ===
+    "report-problematic-content-and-description-in-uclas-library-collections-and-archives"
+  ) {
+    redirect(
+      "https://ucla.libwizard.com/id/38f45c482a5fcb0b715a7e9e3ddee8b2"
+    )
+  } else if (
+    route.params.slug ===
+    "toward-ethical-and-inclusive-descriptive-practices-in-ucla-library-special-collections"
+  ) {
+    redirect(`${config.host}/about/policies/ethical-description`)
+  } else {
+    const data = await $graphql.default.request(POLICY_DETAIL, {
+      slug: route.params.slug,
+    })
+
+    // if (data) await $elasticsearchplugin.index(data.entry, route.params.slug)
+    console.log('expected async data: ', data)
+    return data
+  }
+})
+
+if (error.value) {
+  throw createError({
+    statusCode: 404, statusMessage: 'Page not found.' + error.value, fatal: true
+  })
+}
+
+if (!data.value.entry) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Page Not Found'
+  })
+}
+
+const page = ref(_get(data.value, 'entry', {}))
+let h2Array = ref([]) // anchor tags
+
+console.log('expected page data: ', page.value)
+
+useHead({
+  title: page.value ? page.value.title : '... loading',
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: removeTags(page.value.text)
+    }
+  ]
+})
+
+const { $getHeaders } = useNuxtApp()
+
+onMounted(() => {
+  console.log("In mounted client side")
+  // Call the plugin method to get the .section-header2 and .section-header3 elements
+  h2Array = $getHeaders.getHeadersMethod()
+  console.log('headers: ', h2Array)
+})
+
+</script>
+
+<template lang="html">
+  <main
+    id="main"
+    class="page page-general-content"
+  >
+    <nav-breadcrumb
+      to="/about/policies"
+      :title="page.title"
+      parent-title="Policies"
+    />
+
+    <banner-text
+      v-if="page && (!page.heroImage || page.heroImage.length == 0)"
+      class="banner-text"
+      :category="page.format"
+      :title="page.title"
+      :text="page.text"
+    />
+
+    <section-wrapper
+      v-if="page && page.heroImage && page.heroImage.length == 1"
+      class="section-banner"
+    >
+      <banner-header
+        :media="page.heroImage[0].image[0]"
+        :category="page.format"
+        :title="page.title"
+        :text="page.text"
+      />
+    </section-wrapper>
+
+    <section-wrapper theme="divider">
+      <divider-way-finder
+        class="divider-way-finder"
+        color="about"
+      />
+    </section-wrapper>
+
+    <page-anchor
+      v-if="h2Array.length >= 3"
+      :section-titles="h2Array"
+    />
+
+    <flexible-blocks
+      v-if="page"
+      class="flexible-content"
+      :blocks="page.blocks"
+    />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.page-general-content {}
+</style>
