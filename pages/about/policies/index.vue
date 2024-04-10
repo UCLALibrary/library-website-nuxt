457ccf2426f3242238111b8b2ca08aab40a55ce8
tinuola <tinuola@users.noreply.github.com>
2024-04-10 15:15:28 -0700

    feat: add policy index page

---
+++ pages/about/policies/index.vue
@@ -0,0 +1,110 @@
+<script setup>
+import { onMounted } from 'vue'
+
+// HELPERS
+import _get from "lodash/get"
+import removeTags from "../utils/removeTags"
+
+// GQL
+import POLICIES_LIST from "../gql/queries/PoliciesList.gql"
+
+const { $graphql } = useNuxtApp()
+
+const route = useRoute()
+
+const { data } = await useAsyncData('policy-list', async () => {
+  const data = await $graphql.default.request(POLICIES_LIST, {
+    uri: route.params.uri,
+  })
+
+  return data
+})
+
+const page = ref(_get(data.value, "entry", {}))
+const policyBlock = ref(_get(data.value, "entry.policyBlock", {}))
+const blocks = ref(_get(data.value, "entry.blocks", {}))
+let h2Array = ref([]) // anchor tags
+
+useHead({
+  title: page.value ? page.value.title : "... loading",
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: removeTags(page.value.text)
+    }
+  ]
+})
+
+const parsedPolicyBlocks = computed(() => {
+  return policyBlock.value.map((obj) => {
+    return {
+      ...obj,
+      parsedAssociatedEntries: obj.associatedEntries.map((entry) => {
+        return {
+          ...entry,
+          to: `/${entry.uri}`
+        }
+      })
+    }
+  })
+})
+
+const { $getHeaders } = useNuxtApp()
+
+onMounted(() => {
+  // Call the plugin to get the .section-header2 and .section-header3 elements
+  h2Array = $getHeaders.getHeadersMethod()
+})
+</script>
+
+<template>
+  <main
+    id="main"
+    class="page page-policies"
+  >
+    <masthead-secondary
+      :title="page.title"
+      :text="page.text"
+    />
+
+    <page-anchor
+      v-if="h2Array.length >= 3"
+      :section-titles=h2Array
+    />
+
+    <div
+      v-for="(policy, index) in parsedPolicyBlocks"
+      :key="`PolicyBlocksKey-${policy}`"
+    >
+      <section-wrapper
+        :section-title="policy.sectionTitle"
+        :section-summary="policy.sectionSummary"
+      >
+        <simple-cards
+          :items="policy.parsedAssociatedEntries"
+          button="View all"
+        />
+      </section-wrapper>
+
+      <section-wrapper theme="divider">
+        <divider-way-finder class="divider" />
+      </section-wrapper>
+    </div>
+
+    <flexible-blocks :blocks="page.blocks" />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.page-policies {
+
+  // refactor simple cards component to fix truncation
+  :deep(.simple-cards .text p) {
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 5;
+  }
+}
+</style>
