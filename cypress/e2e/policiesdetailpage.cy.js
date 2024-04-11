f9cf53efc93d2114694b12245c4e678b0bcfae2d
tinuola <tinuola@users.noreply.github.com>
2024-04-10 18:29:32 -0700

    test: add e2e test for policy slug

---
+++ cypress/e2e/policiesdetailpage.cy.js
@@ -0,0 +1,18 @@
+describe("Policy Detail page", () => {
+    it("Visits a Policy Detail Page", () => {
+        cy.visit("about/policies/shhh")
+        cy.get(".logo-ucla").should("be.visible")
+        // cy.get(".page-anchor").scrollIntoView().should('be.visible')
+        cy.percySnapshot({ widths: [768, 992, 1200] })
+    })
+
+    context("When there isn't an entry in craft", () => {
+        it("Raises a 404 error", () => {
+            // cy.visit("/about/blogs/no_entry", {failOnStatusCode: false})
+            // cy.request({url: "/about/policies/no_entry", failOnStatusCode: false}).its("status").should("equal", 404)
+            // cy.visit("/about/policies/no_entry")
+            // cy.get("p.error").should("contain","404")
+            // cy.get("h1.error-title").should("contain","Page not found")
+        })
+    })
+})
