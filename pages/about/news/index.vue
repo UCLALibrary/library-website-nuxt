38195bd556b40e4f74afc8e1f9d8eed3c8c9c39d
Parinita Mulak <pghorpade@library.ucla.edu>
2024-04-23 15:30:26 -0700

    feat: elastic search enabled on news listing page (#791)
    * fix: lint errors
    
    * fix: rename esindex to esalias

--- pages/about/news/index.vue
+++ pages/about/news/index.vue
@@ -7,14 +7,12 @@ import removeTags from '../utils/removeTags'
 // GQL
 import ARTICLE_LIST from '../gql/queries/ArticleList.gql'
 
-// TODO: Enable when Elastic Search is implemented
-// UTILITIES
-// import getListingFilters from '../utils/getListingFilters'
-// import config from '../utils/searchConfig'
-// import queryFilterHasValues from '../utils/queryFilterHasValues'
+import getListingFilters from '../utils/getListingFilters'
+import config from '../utils/searchConfig'
+import queryFilterHasValues from '../utils/queryFilterHasValues'
 // console.log('In news listing page')
 
-const { $graphql } = useNuxtApp()
+const { $graphql, $dataApi } = useNuxtApp()
 const { data, error } = await useAsyncData('news', async () => {
   const data = await $graphql.default.request(ARTICLE_LIST)
   return data
@@ -32,8 +30,8 @@ if (!data.value.entry && !data.value.entries) {
 }
 
 // console.log('In news listing page data.value: ', JSON.stringify(data.value))
-// TODO: Enable when Elastic Search is implemented
-// const route = useRoute()
+
+const route = useRoute()
 
 // Data
 const page = ref(_get(data.value, 'entry', {}))
@@ -43,92 +41,6 @@ watch(data, (newVal, oldVal) => {
   page.value = _get(newVal, 'entry', {})
   news.value = _get(newVal, 'entries', [])
 })
-//
-
-const hits = ref([])
-const title = ref('')
-const noResultsFound = ref(false)
-
-// TODO: Enable/update when Elastic Search is implemented
-// const searchFilters = ref([])
-// const searchGenericQuery = ref({
-//   queryText: $route.query.q || '',
-//   queryFilters:
-//     (route.$query.filters &&
-//       JSON.parse($route.query.filters)) ||
-//     {},
-// })
-
-// TODO: Enable/update when Elastic Search is implemented
-// async fetch() {
-//   // console.log("Fetch hook  news index ")
-
-//   this.news = []
-//   this.hits = []
-
-//   if (
-//     (this.$route.query.q && this.$route.query.q !== '') ||
-//     (this.$route.query.filters &&
-//       queryFilterHasValues(
-//         this.$route.query.filters,
-//         config.newsIndex.filters
-//       ))
-//   ) {
-//     if (!this.page.title) {
-//       const data = await this.$graphql.default.request(ARTICLE_LIST)
-//       // console.log("data for masthead:" + data)
-
-//       this.page.title = _get(data, 'entry.title', '')
-//       this.page.text = _get(data, 'entry.text', '')
-//     }
-//     // eslint-disable-next-line camelcase
-//     const query_text = this.$route.query.q || '*'
-
-//     // console.log("in router query in asyc data")
-//     const results = await this.$dataApi.keywordSearchWithFilters(
-//       query_text,
-//       config.newsIndex.searchFields,
-//       'sectionHandle:article',
-//       (this.$route.query.filters &&
-//         JSON.parse(this.$route.query.filters)) ||
-//       {},
-//       config.newsIndex.sortField,
-//       config.newsIndex.orderBy,
-//       config.newsIndex.resultFields,
-//       config.newsIndex.filters
-//     )
-//     // console.log("getsearchdata method:" + JSON.stringify(results))
-
-//     this.news = []
-//     this.hits = []
-//     if (results && results.hits && results.hits.total.value > 0) {
-//       this.hits = results.hits.hits
-//       this.noResultsFound = false
-//       this.news = []
-//     } else {
-//       this.noResultsFound = true
-//       this.hits = []
-
-//       this.news = []
-//     }
-//     this.searchGenericQuery = {
-//       queryText: this.$route.query.q || '',
-//       queryFilters:
-//         (this.$route.query.filters &&
-//           JSON.parse(this.$route.query.filters)) ||
-//         {},
-//     }
-//   } else {
-//     this.hits = []
-//     this.noResultsFound = false
-//     // if route queries are empty fetch data from craft
-//     const data = await this.$graphql.default.request(ARTICLE_LIST)
-//     // //console.log("data:" + data)
-
-//     this.page = _get(data, 'entry', {})
-//     this.news = _get(data, 'entries', [])
-//   }
-// },
 
 useHead({
   title: page.value ? page.value.title : '... loading',
@@ -136,20 +48,13 @@ useHead({
     {
       hid: 'description',
       name: 'description',
-      content: removeTags(page.value.text)
+      content: removeTags(page.value?.text)
     }
   ]
 })
 
-// TODO: Enable/update when Elastic Search is implemented
-// const parseDisplayResults = computed(() => {
-//   if (hits.length > 1)
-//     return `Displaying ${hits.length} results`
-//   else return `Displaying ${hits.length} result`
-// })
-
 const parsedFeaturedNews = computed(() => {
-  return page.value.featuredNews.map((obj) => {
+  return page.value?.featuredNews?.map((obj) => {
     return {
       ...obj,
       to:
@@ -167,11 +72,12 @@ const parsedFeaturedNews = computed(() => {
 })
 
 const parsedBannerHeader = computed(() => {
-  return parsedFeaturedNews.value[0]
+  // console.log('parsedBannerHeader', parsedFeaturedNews.value)
+  return parsedFeaturedNews.value ? parsedFeaturedNews.value[0] : {}
 })
 
 const parsedSectionHighlight = computed(() => {
-  return parsedFeaturedNews.value.slice(1, 3).map((obj) => {
+  return parsedFeaturedNews.value?.slice(1, 3).map((obj) => {
     return {
       ...obj,
       bylineTwo:
@@ -182,8 +88,17 @@ const parsedSectionHighlight = computed(() => {
   })
 })
 
+function parseArticleCategory(categories) {
+  if (!categories || categories.length === 0) return ''
+  let result = ''
+  categories.forEach((obj) => {
+    result = result + obj.title + ', '
+  })
+  return result.slice(0, -2)
+}
+
 const parsedNewsList = computed(() => {
-  return news.value.map((obj) => {
+  return news.value?.map((obj) => {
     return {
       ...obj,
       to:
@@ -199,11 +114,11 @@ const parsedNewsList = computed(() => {
 })
 
 const parsedDate = computed(() => {
-  return format(new Date(news.value.postDate), 'MMMM d, Y')
+  return format(new Date(news.value?.postDate), 'MMMM d, Y')
 })
 
 const parsedByline = computed(() => {
-  const byline = (news.value.contributors || []).map((entry) => {
+  const byline = (news.value?.contributors || []).map((entry) => {
     return `${entry.byline} ${entry.title || entry.staffMember[0].title
       }`
   })
@@ -212,103 +127,142 @@ const parsedByline = computed(() => {
   })
 })
 
-const parsedPlaceholder = computed(() => {
-  return `Search ${page.title}`
+// ES search functionality
+const hits = ref([])
+const title = ref('')
+const noResultsFound = ref(false)
+const searchFilters = ref([])
+const searchGenericQuery = ref({
+  queryText: route.query.q || '',
+  queryFilters:
+    (route.query.filters &&
+      JSON.parse(route.query.filters)) ||
+    {},
 })
 
-// TODO: Enable when Elastic Search is implemented
-// const parseHitsResults = computed(() => {
-//   /* //console.log(
-//           "ParseHits Results checking results data:" +
-//               JSON.stringify(this.hits)
-//       ) */
+// This watcher is called when router push updates the query params
+watch(
+  () => route.query,
+  (newVal, oldVal) => {
+    console.log('ES newVal, oldVal', newVal, oldVal)
+    searchGenericQuery.value.queryText = route.query.q || ''
+    searchGenericQuery.value.queryFilters = (route.query.filters && JSON.parse(route.query.filters)) || {}
+    searchES()
+  }, { deep: true, immediate: true }
+)
 
-//   return parseHits(hits.value)
-// })
+async function searchES() {
+  if (
+    (route.query.q && route.query.q !== '') ||
+    (route.query.filters &&
+      queryFilterHasValues(
+        route.query.filters,
+        config.newsIndex.filters
+      ))
+  ) {
+    console.log('Search ES HITS query,', route.query.q)
+    const queryText = route.query.q || '*'
+    const results = await $dataApi.keywordSearchWithFilters(
+      queryText,
+      config.newsIndex.searchFields,
+      'sectionHandle:article',
+      (route.query.filters &&
+        JSON.parse(route.query.filters)) ||
+      {},
+      config.newsIndex.sortField,
+      config.newsIndex.orderBy,
+      config.newsIndex.resultFields,
+      config.newsIndex.filters
+    )
 
-// TODO: Enable/update when Elastic Search is implemented
-// watch: {
-//   '$route.query': '$fetch',
-//     '$route.query.q'(newValue) {
-//     // console.log("watching querytEXT:" + newValue)
-//   },
-//   '$route.query.filters'(newValue) {
-//     // console.log("watching filters:" + newValue)
-//   },
-// },
-
-// TODO: Enable/update when Elastic Search is implemented
-// async function mounted() {
-// console.log("In mounted")
-/* //console.log("ESREADkey:" + this.$config.esReadKey)
-    //console.log("ESURLkey:" + this.$config.esURL) */
-// bookmarked search queries are not calling fetch
-// this.setFilters()
-// }
-
-function parseArticleCategory(categories) {
-  if (!categories || categories.length === 0) return ''
-  let result = ''
-  categories.forEach((obj) => {
-    result = result + obj.title + ', '
-  })
-  return result.slice(0, -2)
+    if (results && results.hits && results.hits.total.value > 0) {
+      console.log('Search ES HITS,', results.hits.hits)
+      hits.value = results.hits.hits
+      noResultsFound.value = false
+    } else {
+      noResultsFound.value = true
+      hits.value = []
+    }
+  } else {
+    // console.log('data.value', data.value)
+    // console.log('page.value', page.value)
+    // console.log('news.value', news.value)
+    hits.value = []
+    noResultsFound.value = false
+  }
 }
 
-// TODO: Enable/update when Elastic Search is implemented
-// async function setFilters() {
-//   const searchAggsResponse = await this.$dataApi.getAggregations(
-//     config.newsIndex.filters,
-//     'article'
-//   )
+const parseDisplayResults = computed(() => {
+  if (hits.length > 1)
+    return `Displaying ${hits.length} results`
+  else return `Displaying ${hits.length} result`
+})
 
-//   /* console.log(
-//           "Search Aggs Response: " + JSON.stringify(searchAggsResponse)
-//       ) */
-//   searchFilters = getListingFilters(
-//     searchAggsResponse,
-//     config.newsIndex.filters
-//   )
-// }
+const parsedPlaceholder = computed(() => {
+  return `Search ${page.value?.title}`
+})
+const parseHitsResults = computed(() => {
+  /* console.log(
+          "ParseHits Results checking results data:" +
+              JSON.stringify(hits)
+      ) */
 
-// TODO: Enable when Elastic Search is implemented
-// function parseHits(hits = []) {
-//   return hits.value.map((obj) => {
-//     // //console.log(obj["_source"]["image"])
-//     return {
-//       ...obj._source,
-//       description: obj._source.text,
-//       date: obj._source.postDate,
-//       articleCategories: obj._source.category,
-//       to: `/${obj._source.uri}`,
-//       image: _get(obj._source, 'heroImage[0].image[0]', null),
-//       staffName: obj._source.fullName,
-//       // category: _get(obj["_source"], "category[0].title", null),
-//       category: parseArticleCategory(
-//         obj._source.category
-//       ),
-//     }
-//   })
-// }
+  return parseHits(hits.value)
+})
 
-// TODO: Enable when Elastic Search is implemented
-// function getSearchData(data) {
-// console.log("On the page getsearchdata called")
-/* this.page = {}
-      this.hits = [] */
-//   route.push({
-//     path: '/about/news',
-//     query: {
-//       q: data.text,
-//       filters: JSON.stringify(data.filters),
-//     },
-//   })
-// }
+function parseHits(hits = []) {
+  return hits?.map((obj) => {
+    // //console.log(obj["_source"]["image"])
+    return {
+      ...obj._source,
+      description: obj._source.text,
+      date: obj._source.postDate,
+      articleCategories: obj._source.category,
+      to: `/${obj._source.uri}`,
+      image: _get(obj._source, 'heroImage[0].image[0]', null),
+      staffName: obj._source.fullName,
+      // category: _get(obj["_source"], "category[0].title", null),
+      category: parseArticleCategory(
+        obj._source.category
+      ),
+    }
+  })
+}
 
-/* Used with Options API; to delete? */
-// fetchOnServer: false,
-// multiple components can return the same `fetchKey` and Nuxt will track them both separately
-// fetchKey: 'news-index',
+//  This is event handler which is invoked by search-generic component selections
+function getSearchData(data) {
+  console.log('On the page getsearchdata called')
+  useRouter().push({
+    path: '/about/news',
+    query: {
+      q: data.text,
+      filters: JSON.stringify(data.filters),
+    },
+  })
+}
+
+// fetch filters for the page from ES after page loads in Onmounted hook on the client side
+async function setFilters() {
+  const searchAggsResponse = await $dataApi.getAggregations(
+    config.newsIndex.filters,
+    'article'
+  )
+
+  /* console.log(
+          "Search Aggs Response: " + JSON.stringify(searchAggsResponse)
+      ) */
+  searchFilters.value = getListingFilters(
+    searchAggsResponse,
+    config.newsIndex.filters
+  )
+}
+
+onMounted(async () => {
+  console.log('onMounted called')
+  // console.log("ESREADkey:" + config.esReadKey)
+  // console.log("ESURLkey:" + config.esURL)
+  await setFilters()
+})
 </script>
 
 <template>
@@ -334,14 +288,13 @@ function parseArticleCategory(categories) {
     <section-wrapper theme="divider">
       <divider-way-finder class="search-margin" />
     </section-wrapper>
-
     <section-wrapper
       v-show="page &&
         page.featuredNews &&
         page.featuredNews.length &&
-        hits.length == 0 &&
+        hits.length === 0 &&
         !noResultsFound
-      "
+        "
       class="section-no-top-margin"
     >
       <banner-featured
@@ -357,17 +310,15 @@ function parseArticleCategory(categories) {
         class="banner section-featured-banner"
       />
 
-      <divider-general
-        v-if="page &&
-          page.featuredNews &&
-          page.featuredNews.length &&
-          hits.length == 0 &&
-          !noResultsFound
-        "
-      />
+      <divider-general v-show="page &&
+        page.featuredNews &&
+        page.featuredNews.length &&
+        hits.length === 0 &&
+        !noResultsFound
+        " />
 
       <section-teaser-highlight
-        v-if="parsedSectionHighlight.length"
+        v-show="parsedSectionHighlight.length"
         class="section"
         :items="parsedSectionHighlight"
       />
@@ -377,16 +328,19 @@ function parseArticleCategory(categories) {
       v-show="page &&
         page.featuredNews &&
         page.featuredNews.length &&
-        hits.length == 0 &&
+        hits.length === 0 &&
         !noResultsFound
-      "
+        "
       theme="divider"
     >
       <divider-way-finder color="about" />
     </section-wrapper>
 
     <section-wrapper
-      v-if="news && news.length > 0"
+      v-show="news &&
+        news.length > 0 &&
+        hits.length === 0 &&
+        !noResultsFound"
       section-title="All News"
     >
       <section-staff-article-list :items="parsedNewsList" />
@@ -398,11 +352,11 @@ function parseArticleCategory(categories) {
       class="section-no-top-margin"
     >
       <h2
-        v-if="$route.query.q"
+        v-if="route.query.q"
         class="about-results"
       >
         Displaying {{ hits.length }} results for
-        <strong><em>“{{ $route.query.q }}”</em></strong>
+        <strong><em>“{{ route.query.q }}”</em></strong>
       </h2>
       <h2
         v-else
@@ -420,7 +374,7 @@ function parseArticleCategory(categories) {
     >
       <div class="error-text">
         <rich-text>
-          <h2>Search for “{{ $route.query.q }}” not found.</h2>
+          <h2>Search for “{{ route.query.q }}” not found.</h2>
           <p>
             We can’t find the term you are looking for on this page,
             but we're here to help. <br>
