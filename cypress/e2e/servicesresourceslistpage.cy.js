462b80d724d9791422f31d7f86b9c23e58643abc
jendiamond <thejendiamond@gmail.com>
2024-04-30 16:38:48 -0700

    enable testing for search and page

--- cypress/e2e/servicesresourceslistpage.cy.js
+++ cypress/e2e/servicesresourceslistpage.cy.js
@@ -1,18 +1,18 @@
 describe('All Services & Resources Listing page', () => {
   it('Visits the All Services & Resources Listing Page', () => {
-    // cy.visit("help/services-resources")
-    // cy.get(".logo-ucla").should("be.visible")
-    // cy.percySnapshot({ widths: [768, 992, 1200] })
+    cy.visit("help/services-resources")
+    cy.get(".logo-ucla").should("be.visible")
+    cy.percySnapshot({ widths: [768, 992, 1200] })
   })
 
   it('Search Found', () => {
-    // cy.visit("help/services-resources?q=test")
-    // cy.get(".logo-ucla").should("be.visible")
-    // cy.get("input[type=search]").should(
-    //     "have.value",
-    //     "test"
-    // )
-    // cy.get("h2.about-results").invoke("text").should("not.be.empty")
-    // cy.percySnapshot({ widths: [768, 992, 1200] })
+    cy.visit("help/services-resources?q=test")
+    cy.get(".logo-ucla").should("be.visible")
+    cy.get("input[type=search]").should(
+        "have.value",
+        "test"
+    )
+    cy.get("h2.about-results").invoke("text").should("not.be.empty")
+    cy.percySnapshot({ widths: [768, 992, 1200] })
   })
 })
