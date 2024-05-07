af8d196f39cd09c90583ba5a32e424c1ed818329
pghorpade <pghorpade@library.ucla.edu>
2024-05-07 16:19:13 -0700

    fix: es tests are afiling on percy checks

--- cypress/e2e/eventsexhibitionslist.cy.js
+++ cypress/e2e/eventsexhibitionslist.cy.js
@@ -9,8 +9,8 @@ describe('Events & Exhibitions List page', () => {
     cy.percySnapshot({ widths: [768, 992, 1200] })
   })
   it('Visit Events & Exhibitions Listing page filter by event type', () => {
-    cy.visit('/visit/events-exhibitions/?q&filters=%7B"eventType.title.keyword"%3A%5B"Workshop"%5D%7D')
+    cy.visit('/visit/events-exhibitions/?q&filters=%7B"eventType.title.keyword"%3A%5B"Workshop"%5D%7D', { timeout: 13000 })
 
-    cy.get('h2.about-results').should('be.visible')
+    cy.get('h2.about-results').invoke('text').should('not.be.empty')
   })
 })
