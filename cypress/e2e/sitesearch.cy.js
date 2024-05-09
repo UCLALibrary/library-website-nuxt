c3cf13c79f49b880561b6026b68038cb7df34222
pghorpade <pghorpade@library.ucla.edu>
2024-05-09 11:29:10 -0700

    fix: cypress timeout error

--- cypress/e2e/sitesearch.cy.js
+++ cypress/e2e/sitesearch.cy.js
@@ -6,7 +6,12 @@ describe('Site Search page', () => {
     cy.get('div > h2').should('contain', 'Search for “” not found.')
   })
   it('Search all', () => {
-    cy.visit('/search-site?q=*')
-    cy.get('.about-results').should('be.visible')
+    cy.visit('/search-site?q=*', { timeout: 13000 })
+    cy.get('.logo-ucla').should('be.visible')
+    cy.get('input[type=search]').should(
+      'have.value',
+      '*'
+    )
+    cy.get('h2.about-results').invoke('text').should('not.be.empty')
   })
 })
