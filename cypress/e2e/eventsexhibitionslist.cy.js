2c6e75a677e75f0e73dd670e9f79eb83c3b2c889
pghorpade <pghorpade@library.ucla.edu>
2024-05-09 12:06:49 -0700

    fix: Add cy.intercept

--- cypress/e2e/eventsexhibitionslist.cy.js
+++ cypress/e2e/eventsexhibitionslist.cy.js
@@ -1,18 +1,22 @@
 describe('Events & Exhibitions List page', () => {
   it('Visits a Events & Exhibitions List Page', () => {
+    cy.intercept('/visit/events-exhibitions*').as('getEventsRoutes')
     cy.visit('/visit/events-exhibitions', { timeout: 30000 })
-    cy.wait(1000)
+    cy.wait('@getEventsRoutes').then(() => {
     // UCLA Library brand
-    cy.get('.logo-ucla').should('be.visible')
+      cy.get('.logo-ucla').should('be.visible')
 
-    cy.get('.page-events-exhibits').should('be.visible')
-    cy.get('h1.title').should('contain', 'Events & Exhibitions')
-    cy.percySnapshot({ widths: [768, 992, 1200] })
+      cy.get('.page-events-exhibits').should('be.visible')
+      cy.get('h1.title').should('contain', 'Events & Exhibitions')
+      cy.percySnapshot({ widths: [768, 992, 1200] })
+    })
   })
   it('Visit Events & Exhibitions Listing page filter by event type', () => {
+    cy.intercept('/visit/events-exhibitions*').as('getEventsSearchRoutes')
     cy.visit('/visit/events-exhibitions/?q&filters=%7B"eventType.title.keyword"%3A%5B"Workshop"%5D%7D', { timeout: 30000 })
-    cy.wait(1000)
-    cy.get('.logo-ucla').should('be.visible')
-    cy.get('h2.about-results').invoke('text').should('not.be.empty')
+    cy.wait('@getEventsRoutes').then(() => {
+      cy.get('.logo-ucla').should('be.visible')
+      cy.get('h2.about-results').invoke('text').should('not.be.empty')
+    })
   })
 })
--- cypress/e2e/sitesearch.cy.js
+++ cypress/e2e/sitesearch.cy.js
@@ -6,13 +6,15 @@ describe('Site Search page', () => {
     cy.get('div > h2').should('contain', 'Search for “” not found.')
   })
   it('Search all', () => {
+    cy.intercept('/search-site*').as('getSearchRoutes')
     cy.visit('/search-site?q=*', { timeout: 30000 })
-    cy.wait(1000)
-    cy.get('.logo-ucla').should('be.visible')
-    cy.get('input[type=search]').should(
-      'have.value',
-      '*'
-    )
-    cy.get('h2.about-results').invoke('text').should('not.be.empty')
+    cy.wait('@getSearchRoutes').then(() => {
+      cy.get('.logo-ucla').should('be.visible')
+      cy.get('input[type=search]').should(
+        'have.value',
+        '*'
+      )
+      cy.get('h2.about-results', { timeout: 80000 }).invoke('text').should('not.be.empty')
+    })
   })
 })
