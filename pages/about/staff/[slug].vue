13968d0ee6617019368b8a139f9cc27dd5d49768
Jess <jessica.divers@gmail.com>
2024-05-13 17:19:06 -0700

    feat: APPS-2666 Migrate slug/detail template to nuxt3.x for route /about/staff/[slug].vue (#804)
    * feat: start refactoring page
    
    * feat: more refactoring
    
    * fix: blockstaffdetail data formatting
    
    * fix: tests
    
    * fix: run linter, add mock data
    
    * fix: image data restored
    
    * chore: update component package
    
    ---------
    
    Co-authored-by: Jess Divers <jessica.divers@library.ucla.edu>

---
+++ pages/about/staff/[slug].vue
@@ -0,0 +1,187 @@
+<script lang="ts" setup>
+// HELPERS
+import _get from 'lodash/get'
+import removeTags from '../utils/removeTags'
+
+// GQL
+import STAFF_DETAIL from '../gql/queries/StaffDetail.gql'
+
+const route = useRoute()
+const { $graphql, $elasticsearchplugin } = useNuxtApp()
+
+// ASYNC DATA into PAGE const
+const { data, error } = await useAsyncData(`staff/${route.params.slug}`, async () => {
+  const data = await $graphql.default.request(STAFF_DETAIL, { slug: route.params.slug })
+  // console.log('In staff Slug page data', data)
+  return data
+})
+
+if (error.value) {
+  throw createError({
+    ...error.value, statusMessage: 'Page not found.' + error.value, fatal: true
+  })
+}
+if (!data.value.entry) {
+  // eslint-disable-next-line no-console
+  console.log('In staff Slug page no data')
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Page Not Found',
+    fatal: true
+  })
+}
+// ES Index
+if (route.params.slug && process.server) {
+  const { $elasticsearchplugin } = useNuxtApp()
+  // console.log("elasticsearchplugin", $elasticsearchplugin, route.params.slug)
+  await $elasticsearchplugin?.index(data.value.entry, route.params.slug)
+}
+
+const page = ref(_get(data.value, 'entry', {}))
+const entries = ref(_get(data.value, 'entries', {}))
+
+watch(data, (newVal, oldVal) => {
+  // console.log('In watch preview enabled, newVal, oldVal', newVal, oldVal)
+  page.value = _get(newVal, 'entry', {})
+  entries.value = _get(newVal, 'entries', {})
+})
+
+useHead({
+  title: page.value ? page.value.title : '... loading',
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: removeTags(page.value.text)
+    }
+  ]
+})
+
+// COMPUTED
+const parsedImage = computed(() => {
+  return _get(page.value, 'image[0]', {})
+})
+const parsedAlternativeFullName = computed(() => {
+  return _get(page.value, 'alternativeName', '')
+})
+const parsedLanguage = computed(() => {
+  return _get(page.value, 'alternativeName[0].languageAltName', '')
+})
+const parsedItems = computed(() => {
+  // was page value entries
+  return entries.value.map((obj) => {
+    return {
+      ...obj,
+      to:
+        obj.externalResourceUrl != null
+          ? _get(obj, 'externalResourceUrl', '')
+          : `/${obj.to}`,
+      image: _get(obj, 'heroImage[0].image[0]', null),
+      staffName: `${obj.nameFirst} ${obj.nameLast}`,
+    }
+  })
+})
+</script>
+<template>
+  <main
+    id="main"
+    class="page page-staff-detail"
+  >
+    <!-- staff page here -->
+    <nav-breadcrumb
+      to="/about/staff"
+      :title="page.title"
+      parent-title="Staff Directory"
+    />
+    <section-wrapper>
+      <block-staff-detail
+        :image="parsedImage"
+        :name-first="page.nameFirst"
+        :name-last="page.nameLast"
+        :alternative-name="parsedAlternativeFullName"
+        :language="parsedLanguage"
+        :job-title="page.jobTitle"
+        :departments="page.departments"
+        :locations="page.locations"
+        :pronouns="page.pronouns"
+        :email="page.email"
+        :phone="page.phone"
+        :consultation="page.consultation"
+        :topics="page.topics"
+        :academic-departments="page.academicDepartments"
+        :biography="page.biography"
+      />
+    </section-wrapper>
+
+    <section-wrapper
+      v-if="parsedItems?.length ||
+        page.publications ||
+        page.orcid
+      "
+      class="selected-articles"
+      theme="divider"
+    >
+      <divider-way-finder
+        class="divider"
+        color="about"
+      />
+    </section-wrapper>
+
+    <div
+      v-if="parsedItems?.length ||
+        page.publications ||
+        page.orcid
+      "
+      class="selected-articles"
+    >
+      <section-wrapper>
+        <section-staff-orcid-publications
+          class="staff-orcid-publications"
+          :orcid="page.orcid"
+          :publications="page.publications"
+        />
+      </section-wrapper>
+
+      <section-wrapper theme="divider">
+        <divider-way-finder
+          v-if="parsedItems?.length &&
+            (page.publications || page.orcid)
+          "
+          class="divider divider-first"
+          color="about"
+        />
+      </section-wrapper>
+
+      <section-wrapper>
+        <section-staff-article-list
+          v-if="parsedItems?.length"
+          class="staff-article-list-section"
+          section-title="Articles"
+          :items="parsedItems"
+        />
+      </section-wrapper>
+    </div>
+
+    <section-wrapper theme="divider">
+      <divider-way-finder
+        class="divider divider-first"
+        color="about"
+      />
+    </section-wrapper>
+    <section-wrapper>
+      <block-call-to-action
+        class="block-call-to-action"
+        :is-global="true"
+      />
+    </section-wrapper>
+  </main>
+</template>
+<style lang="scss" scoped>
+.page-staff-detail {
+  .selected-articles {
+    :deep(.divider .dotted) {
+      border-color: var(--color-secondary-grey-03);
+    }
+  }
+}
+</style>
