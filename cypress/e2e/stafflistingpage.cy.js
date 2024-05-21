8fca37bea28ae97e353b9a690f9c81baf0a1a62b
Parinita Mulak <pghorpade@library.ucla.edu>
2024-05-21 10:38:15 -0700

    feat: APPS-2669 Add ES to staff-listing template (#809)
    * feat: Fix checkbox by updating it by watching props.filters
    
    * feat: update component library package version to fix checkbox state no update issue
    
    * tests: fix add wait after interecpt
    
    * fix: uodate search url in tests

---
+++ cypress/e2e/stafflistingpage.cy.js
@@ -0,0 +1,26 @@
+describe('Article News Listing page', () => {
+  it('Visit the Article News Listing Page', () => {
+    cy.visit('/about/staff')
+
+    // UCLA Library brand
+    cy.get('.logo-ucla').should('be.visible')
+    cy.get('.page-staff').should('be.visible')
+    cy.get('h1.title').should('contain', 'Staff Directory')
+    cy.get('.search-generic-filter-buttons').find('button').should('have.length', 2)
+
+    cy.get('.search-generic-filter-buttons').find('button').each(($el, index, $list) => {
+      cy.wrap($el).click()
+      cy.get('fieldset.base-checkbox-group > ul.list > li.list-item').find('label').should('have.length.greaterThan', 0)
+    })
+    cy.get('.search-generic-filter-buttons > div.single-checkbox').should('be.visible')
+    cy.percySnapshot({ widths: [768, 992, 1200] })
+  })
+
+  it('Visit Subject Librarian Listing page', () => {
+    cy.intercept('/about/staff/*').as('getStaffRoutes')
+    cy.visit('/about/staff/?q=&filters={"subjectLibrarian.keyword":["yes"]}', { timeout: 30000 })
+    cy.wait('@getStaffRoutes').then(() => {
+      cy.get('td.academic-department').should('be.visible')
+    })
+  })
+})
