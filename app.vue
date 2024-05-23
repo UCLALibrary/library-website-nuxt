efa7389285f4612a59de2c516073b945f4f1b86e
tinuola <tinuola@users.noreply.github.com>
2024-05-23 12:42:06 -0700

    chore: update loading, page transition configs in nuxt3

--- app.vue
+++ app.vue
@@ -3,6 +3,10 @@ const { enabled, state } = usePreviewMode()
 </script>
 <template>
   <div>
+    <NuxtLoadingIndicator
+      color="#ffe800"
+      height="3"
+    />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
