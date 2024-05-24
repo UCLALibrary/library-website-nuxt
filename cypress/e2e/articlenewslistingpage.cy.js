6e05ad713c40bb7fe8b501657e613a248255b27a
Jess Divers <jessica.divers@library.ucla.edu>
2024-05-24 11:47:11 -0700

    fix: intercept and wait on flaky tests

--- cypress/e2e/articlenewslistingpage.cy.js
+++ cypress/e2e/articlenewslistingpage.cy.js
@@ -1,19 +1,21 @@
 describe('Article News Listing page', () => {
   it('Visit the Article News Listing Page', () => {
+    cy.intercept('/about/news').as('getNews')
     cy.visit('/about/news')
+    cy.wait('@getNews').then(() => {
+      // UCLA Library brand
+      cy.get('.logo-ucla').should('be.visible')
+      cy.get('.page-news').should('be.visible')
+      cy.get('h1.title').should('contain', 'Library News')
+      // cy.get('.search-generic-filter-buttons').find('button').should('have.length', 2)
 
-    // UCLA Library brand
-    cy.get('.logo-ucla').should('be.visible')
-    cy.get('.page-news').should('be.visible')
-    cy.get('h1.title').should('contain', 'Library News')
-    // cy.get('.search-generic-filter-buttons').find('button').should('have.length', 2)
+      // cy.get('.search-generic-filter-buttons').find('button').each(($el, index, $list) => {
+      //   cy.wrap($el).click()
+      //   cy.get('fieldset.base-checkbox-group > ul.list > li.list-item').find('label').should('have.length.greaterThan', 0)
+      // })
 
-    // cy.get('.search-generic-filter-buttons').find('button').each(($el, index, $list) => {
-    //   cy.wrap($el).click()
-    //   cy.get('fieldset.base-checkbox-group > ul.list > li.list-item').find('label').should('have.length.greaterThan', 0)
-    // })
-
-    cy.percySnapshot({ widths: [768, 992, 1200] })
+      cy.percySnapshot({ widths: [768, 992, 1200] })
+    })
   })
 
   // it('Visit News Article Listing page filter by category', () => {
