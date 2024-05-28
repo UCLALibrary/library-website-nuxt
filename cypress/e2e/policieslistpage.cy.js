9cc603f654f8b44270e0046d522d3843bde30da5
Jess Divers <jessica.divers@library.ucla.edu>
2024-05-28 12:46:30 -0700

    fix: add intercept to cypress tests

--- cypress/e2e/policieslistpage.cy.js
+++ cypress/e2e/policieslistpage.cy.js
@@ -1,13 +1,16 @@
 describe('Policy List page', () => {
   it('Visits a Policy List Page', () => {
+    cy.intercept('about/policies*').as('getPolicyListRoutes')
     cy.visit('about/policies')
-    cy.get('.logo-ucla').should('be.visible')
-    cy.get('.page-policies').should('be.visible')
-    cy.get('h1.title').should(
-      'contain',
-      'Policies'
-    )
-    // cy.get(".page-anchor").scrollIntoView().should('be.visible')
-    cy.percySnapshot({ widths: [768, 992, 1200] })
+    cy.wait('@getPolicyListRoutes').then(() => {
+      cy.get('.logo-ucla').should('be.visible')
+      cy.get('.page-policies').should('be.visible')
+      cy.get('h1.title').should(
+        'contain',
+        'Policies'
+      )
+      // cy.get(".page-anchor").scrollIntoView().should('be.visible')
+      cy.percySnapshot({ widths: [768, 992, 1200] })
+    })
   })
 })
