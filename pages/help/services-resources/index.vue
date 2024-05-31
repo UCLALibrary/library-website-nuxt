64e0806824bcaa81916c89b514c35b3973cdc7ec
tinuola <tinuola@users.noreply.github.com>
2024-05-31 14:46:33 -0700

    refactor: add code to clean url structure

--- pages/help/services-resources/index.vue
+++ pages/help/services-resources/index.vue
@@ -8,6 +8,7 @@ import _get from 'lodash/get'
 import getListingFilters from '../utils/getListingFilters'
 import config from '../utils/searchConfig'
 import queryFilterHasValues from '../utils/queryFilterHasValues'
+import parseFilters from '../utils/parseFilters'
 
 // UTILITIES
 import removeTags from '../utils/removeTags'
@@ -70,47 +71,83 @@ watch(data, (newVal, oldVal) => {
 const hits = ref([])
 const noResultsFound = ref(false)
 const searchFilters = ref([])
+// const searchGenericQuery = ref({
+//   queryText: route.query?.q || '',
+//   queryFilters:
+//     (route.query?.filters &&
+//       JSON.parse(route.query.filters)) ||
+//     {},
+// })
 const searchGenericQuery = ref({
-  queryText: route.query?.q || '',
-  queryFilters:
-    (route.query?.filters &&
-      JSON.parse(route.query.filters)) ||
-    {},
+  queryText: route.query.q || '',
+  queryFilters: parseFilters(route.query.filters || ""),
 })
 
 // THIS WATCHER IS CALLED WHEN THE ROUTER PUSHES UPDATES TO THE QUERY PARAM
 // ie: someone changes the query or starts viewing from a bookmarked page
-watch(() => route.query, (oldValue, newValue) => {
-  // console.log('ES newVal, oldVal', newVal, oldVal)
-  searchGenericQuery.value.queryText = route.query.q || ''
-  searchGenericQuery.value.queryFilters = (route.query.filters && JSON.parse(route.query.filters)) || {}
-  searchES()
-}, { deep: true, immediate: true })
+
+// watch(() => route.query, (oldValue, newValue) => {
+//   // console.log('ES newVal, oldVal', newVal, oldVal)
+//   searchGenericQuery.value.queryText = route.query.q || ''
+//   searchGenericQuery.value.queryFilters = (route.query.filters && JSON.parse(route.query.filters)) || {}
+//   searchES()
+// }, { deep: true, immediate: true })
+
+watch(
+  () => route.query,
+  (newVal, oldVal) => {
+    console.log('ES newVal, oldVal', newVal, oldVal)
+    searchGenericQuery.value.queryText = route.query.q || ''
+    searchGenericQuery.value.queryFilters = parseFilters(route.query.filters || "")
+    searchES()
+  }, { deep: true, immediate: true }
+)
 
 // ES search function
 async function searchES() {
+  // if (
+  //   (route.query && route.query.q && route.query.q !== '') ||
+  //   (route.query.filters &&
+  //     queryFilterHasValues(
+  //       route.query.filters,
+  //       config.serviceOrResources.filters
+  //     ))
+  // )
   if (
-    (route.query && route.query.q && route.query.q !== '') ||
+    (route.query.q && route.query.q !== '') ||
     (route.query.filters &&
       queryFilterHasValues(
-        route.query.filters,
+        parseFilters(route.query.filters || ""),
         config.serviceOrResources.filters
       ))
   ) {
     // console.log('Search ES HITS query,', route.query.q)
     const queryText = route.query.q || '*'
+
+    // const results = await $dataApi.keywordSearchWithFilters(
+    //   queryText,
+    //   config.serviceOrResources.searchFields,
+    //   '(sectionHandle:serviceOrResource OR sectionHandle:workshopSeries OR sectionHandle:helpTopic) OR (sectionHandle:externalResource AND displayEntry:yes)',
+    //   (route.query.filters &&
+    //     JSON.parse(route.query.filters)) ||
+    //   [],
+    //   config.serviceOrResources.sortField,
+    //   config.serviceOrResources.orderBy,
+    //   config.serviceOrResources.resultFields,
+    //   []
+    // )
     const results = await $dataApi.keywordSearchWithFilters(
       queryText,
       config.serviceOrResources.searchFields,
       '(sectionHandle:serviceOrResource OR sectionHandle:workshopSeries OR sectionHandle:helpTopic) OR (sectionHandle:externalResource AND displayEntry:yes)',
-      (route.query.filters &&
-        JSON.parse(route.query.filters)) ||
+      parseFilters(route.query.filters || ""),
       [],
       config.serviceOrResources.sortField,
       config.serviceOrResources.orderBy,
       config.serviceOrResources.resultFields,
       []
     )
+
     if (results && results.hits && results.hits.total.value > 0) {
       // console.log('Search ES HITS,', results.hits.hits)
       hits.value = results.hits.hits
@@ -248,16 +285,34 @@ async function setFilters() {
 }
 
 //  This event handler is invoked by the search-generic component (filtered search )selections
+// function getSearchData(data) {
+//   // console.log('On the page getsearchdata called')
+//   const filterData =
+//     (data.filters && JSON.stringify(data.filters)) || {}
+//   useRouter().push({
+//     path: '/help/services-resources',
+//     query: {
+//       q: data.text,
+//       filters: filterData,
+//     },
+//   })
+// }
 function getSearchData(data) {
-  // console.log('On the page getsearchdata called')
-  const filterData =
-    (data.filters && JSON.stringify(data.filters)) || {}
+  console.log('On the page getsearchdata called')
+  // Construct the filters parameter dynamically
+  const filters = []
+  for (const key in data.filters) {
+    if (data.filters[key].length > 0) {
+      filters.push(`${key}:(${data.filters[key].join(' OR ')})`)
+    }
+  }
+  // Use the router to navigate with the new query parameters
   useRouter().push({
-    path: '/help/services-resources',
+    path: '/collections/explore',
     query: {
       q: data.text,
-      filters: filterData,
-    },
+      filters: filters.join(' AND ')
+    }
   })
 }
 </script>
@@ -371,10 +426,7 @@ function getSearchData(data) {
   </main>
 </template>
 
-<style
-  lang="scss"
-  scoped
->
+<style lang="scss" scoped>
 .page-help {
   :deep(label.label) {
     text-transform: capitalize;
