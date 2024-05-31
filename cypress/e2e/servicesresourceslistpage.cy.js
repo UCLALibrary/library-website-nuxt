3fcdffc375642440aab6b5ebc4acbf1a521cfd63
tinuola <tinuola@users.noreply.github.com>
2024-05-31 15:32:56 -0700

    test: update cypress test

--- cypress/e2e/servicesresourceslistpage.cy.js
+++ cypress/e2e/servicesresourceslistpage.cy.js
@@ -1,7 +1,7 @@
 describe('All Services & Resources Listing page', () => {
   it('Visits the All Services & Resources Listing Page', () => {
     cy.intercept('/help/*').as('getHelpRoutes')
-    cy.visit('help/services-resources')
+    cy.visit('/help/services-resources')
     cy.wait('@getHelpRoutes').then(() => {
       cy.get('.logo-ucla').should('be.visible')
       cy.percySnapshot({ widths: [768, 992, 1200] })
@@ -9,8 +9,8 @@ describe('All Services & Resources Listing page', () => {
   })
 
   it('Search Found', () => {
-    cy.intercept('/help/services-resources/?*').as('getResourcesSearchRoutes')
-    cy.visit('help/services-resources/?q=test', { timeout: 35000 })
+    cy.intercept('/help/services-resources?*').as('getResourcesSearchRoutes')
+    cy.visit('/help/services-resources?q=test', { timeout: 35000 })
     cy.wait('@getResourcesSearchRoutes').then(() => {
       cy.get('.logo-ucla').should('be.visible')
       cy.get('input[type=search]').should(
@@ -20,5 +20,11 @@ describe('All Services & Resources Listing page', () => {
       cy.get('h2.about-results').invoke('text').should('not.be.empty')
       cy.percySnapshot({ widths: [768, 992, 1200] })
     })
+  })
+
+  it('Visit Services Listing page filter by category', () => {
+    cy.visit('/help/services-resources?q=Radical&filters=serviceOrResourceType.keyword:(workshop+series)')
+
+    cy.get('h2.about-results').should('be.visible')
   })
 })
