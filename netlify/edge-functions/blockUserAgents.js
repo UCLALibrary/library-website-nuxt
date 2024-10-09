64b74c82f8b80a3dfaafc58bdf2176bffad1257d
pghorpade <pghorpade@library.ucla.edu>
2024-10-09 13:42:00 -0700

    feat: refactor userblocking script and add perset to nitro for edge function to work in netlify

--- netlify/edge-functions/blockUserAgents.js
+++ netlify/edge-functions/blockUserAgents.js
@@ -1,14 +1,17 @@
-export default async (request, context) => {
-  const blockedUserAgents = [
-    'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; ClaudeBot/1.0; +claudebot@anthropic.com)', 'claudebot'
-  ]
+const blockedUserAgents = [
+  'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; ClaudeBot/1.0; claudebot@anthropic.com)',
+  'claudebot'
+]
 
-  const userAgent = await request.headers.get('user-agent')
+export default async (request) => {
+  const userAgent = request.headers.get('user-agent')
+
+
   if (blockedUserAgents.includes(userAgent)) {
-    return new Response('Access Denied', {
-      status: 403
-    })
+    return new Response('Access Denied', { status: 403 }) // Forbidden
   }
 
-  return context.next()
+
+  // Continue with the request as normal
+  return
 }
