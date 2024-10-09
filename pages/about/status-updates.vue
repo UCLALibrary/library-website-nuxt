c6e657124a282bd71f32785fd4e608281e697429
pghorpade <pghorpade@library.ucla.edu>
2024-10-09 13:56:43 -0700

    fix: lint errors

--- pages/about/status-updates.vue
+++ pages/about/status-updates.vue
@@ -16,14 +16,14 @@ onMounted(async () => {
     <hr>
     <h3> Remove this iframe later</h3>
     <iframe
-      title="Content Box frame"
       id="s-lg-widget-frame-1727199122170"
+      title="Content Box frame"
       width="100%"
       height="250"
       scrolling="yes"
       style="border: 1px solid #bbb; border-radius: 4px;"
       src="//lgapi-us.libapps.com/widget_box.php?site_id=705&widget_type=8&output_format=2&widget_title=Library+Alert&widget_height=250&widget_width=100%25&widget_embed_type=1&guide_id=1427138&box_id=33325708&map_id=39190263&content_only=0&include_jquery=1&config_id=1727199122170"
-    ></iframe>
+    />
 
     <div v-if="iframeContent">
       <h2>Extracted Content:</h2>
