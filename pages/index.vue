17e90355ba90e3d1479fc2444c5bf583a4a30c89
Jess Divers <jessica.divers@library.ucla.edu>
2024-10-18 15:43:26 -0700

    fix: restore homepage data, update package

--- pages/index.vue
+++ pages/index.vue
@@ -186,31 +186,17 @@ const bannerFeaturedNews = computed(() => {
 const parsedNewsList = computed(() => {
   const newsList = page.value.featuredNews.slice(1)
   return newsList.map((obj) => {
-    // console.log({
-    //   ...obj,
-    //   to:
-    //     obj.externalResourceUrl != null
-    //       ? _get(obj, 'externalResourceUrl', '')
-    //       : `/${obj.to}`,
-    //   image: _get(obj, 'heroImage[0].image[0]', ''),
-    //   category: parseArticleCategory(obj.articleCategories),
-    //   startDate: _get(obj, 'postDate', ''),
-    //   endDate: _get(obj, 'postDate', ''),
-    //   postDate: null
-    // })
     return {
-      // ...obj,
+      ...obj,
       to: 'https://www.library.ucla.edu',
       sectionHandle: 'news',
-      // obj.externalResourceUrl != null
-      //   ? _get(obj, 'externalResourceUrl', '')
-      //   : `/${obj.to}`,
-      // image: _get(obj, 'heroImage[0].image[0]', ''),
-      // category: parseArticleCategory(obj.articleCategories),
-      startDate: '2023-02-07T15:54:00-08:00',
-      endDate: '2023-02-07T15:54:00-08:00' // _get(obj, 'postDate', ''),
-      // endDate: _get(obj, 'postDate', ''),
-      // postDate: null
+      externalResourceUrl: obj.externalResourceUrl != null
+        ? _get(obj, 'externalResourceUrl', '')
+        : `/${obj.to}`,
+      image: _get(obj, 'heroImage[0].image[0]', ''),
+      category: parseArticleCategory(obj.articleCategories),
+      startDate: _get(obj, 'postDate', ''),
+      endDate: _get(obj, 'postDate', '')
     }
   })
 })
@@ -347,12 +333,12 @@ useHead({
         v-if="parsedNewsList.length > 1"
         :items="parsedNewsList"
       />
-      <nuxt-link
+      <!-- <nuxt-link
         to="/about/news/"
         class="button-more"
       >
         <ButtonMore text="See All News" />
-      </nuxt-link>
+      </nuxt-link> -->
     </SectionWrapper>
   </main>
 </template>
