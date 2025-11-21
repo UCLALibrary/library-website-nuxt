6fd1ec5e20e075bd478b59a74449e9b8dbd6a3c7
jendiamond <thejendiamond@gmail.com>
2025-11-21 11:11:32 -0800

    feat: strip down page and add TutorialsCTA page to bring in the CallToAction data from the TutorialsListing page

--- pages/help/tutorials/[slug].vue
+++ pages/help/tutorials/[slug].vue
@@ -1,33 +1,35 @@
 <script setup>
 // COMPONENTS
-import { NavBreadcrumb, BannerText, SectionHeader, SectionWrapper, DividerWayFinder, RichText, FlexibleBlocks, BlockCallToAction } from '@ucla-library-monorepo/ucla-library-website-components'
+import { NavBreadcrumb, BannerText, BannerHeader, SectionHeader, SectionWrapper, DividerWayFinder, RichText, FlexibleBlocks, BlockCallToActionTwoUp } from '@ucla-library-monorepo/ucla-library-website-components'
 
 // HELPERS
 import _get from 'lodash/get'
-import { format } from 'date-fns'
 import removeTags from '../utils/removeTags'
 
 // GQL
-import TUTORIAL_DETAIL from '../gql/queries/TutorialsDetail.gql'
-
-const route = useRoute()
-
-// console.log('In news Slug page')
+import TUTORIALS_DETAIL from '../gql/queries/TutorialsDetail.gql'
+import TUTORIALS_CTA from '../gql/queries/TutorialsCTA.gql'
 
 const { $graphql } = useNuxtApp()
-const { data, error } = await useAsyncData(`news/${route.params.slug}`, async () => {
-  const data = await $graphql.default.request(TUTORIAL_DETAIL, { slug: route.params.slug })
-  return data
+const route = useRoute()
+
+// FETCH DATA
+const { data, error } = await useAsyncData(`tutorials-${route.params.slug}`, async () => {
+  const detail = await $graphql.default.request(TUTORIALS_DETAIL, { slug: route.params.slug })
+  const cta = await $graphql.default.request(TUTORIALS_CTA)
+  return {detail, cta}
 })
 
+// handle network / graphql error
 if (error.value) {
   throw createError({
-    ...error.value, statusMessage: 'Page not found.' + error.value, fatal: true
+    ...error.value,
+    statusMessage: 'Page not found.' + error.value, fatal: true
   })
 }
 
-if (!data.value.entry) {
-  // console.log('In news Slug page no data')
+// Ensure the detail entry exists, using the correct path
+if (!data.value?.detail?.entry) {
   throw createError({
     statusCode: 404,
     statusMessage: 'Page Not Found',
@@ -35,22 +37,24 @@ if (!data.value.entry) {
   })
 }
 
-// console.log("import.meta.prerender", import.meta.prerender, process.client)
-
-if (data.value.entry.slug && import.meta.prerender) {
-  // console.log('News slug article category:', data.value.entry.category)
-  data.value.entry.tutorial = data.value.entry.tutorial
-
+// Indexing during prerender
+if (data.value.detail.entry.slug && import.meta.prerender) {
   const { index } = useIndexer()
-  // console.log("elasticsearchplugin", index, data.value.entry.slug)
-  await index(data.value.entry, data.value.entry.slug)
+  await index(data.value.detail.entry, route.params.slug)
 }
 
-const page = ref(_get(data.value, 'entry', {}))
+// Create reactive refs for template usage
+const page = ref(_get(data.value, 'detail.entry', {}))
+const cta = ref(_get(data.value, 'cta.entry.callToAction2Up', []))
+
+// Update refs when previewing / data changes
 watch(data, (newVal, oldVal) => {
   // console.log('In watch preview enabled, newVal, oldVal', newVal, oldVal)
-  page.value = _get(newVal, 'entry', {})
+  page.value = _get(newVal, 'detail.entry', {})
+  cta.value = _get(newVal, 'cta.entry.callToAction2Up', [])
 })
+
+const metaDescription = computed(() => removeTags(page.value.summary ?? page.value.title ?? ''))
 
 useHead({
   title: page.value ? page.value.title : '... loading',
@@ -58,33 +62,14 @@ useHead({
     {
       hid: 'description',
       name: 'description',
-      content: removeTags(page.text)
+      content: metaDescription.value
     }
   ]
 })
 
-
 const parsedTutorialType = computed(() => {
   return page.value.tutorialType.title ? page.value.tutorialType.title : ''
 })
-
-// const parsedPolicyBlocks = computed(() => {
-//   return policyBlock.value.map((obj) => {
-//     return {
-//       ...obj,
-//       parsedAssociatedEntries: obj.associatedEntries.map((entry) => {
-//         return {
-//           ...entry,
-//           to: `/${entry.uri}`
-//         }
-//       })
-//     }
-//   })
-// })
-
-// const parsedAuthors
-// const parsedContributorsNoMax
-
 </script>
 
 <template>
@@ -92,164 +77,26 @@ const parsedTutorialType = computed(() => {
     id="main"
     class="page page-news-detail"
   >
-    <NavBreadcrumb
-      to="/about/news/"
-      :title="page.title"
-      parent-title="All Library News"
-    />
 
     <h3>
-      <strong>title</strong>
-      <hr/>
+      <br/>
+      <strong>PAGE DATA</strong>
+      <br/>
       <pre>{{page}}</pre>
     </h3>
+    <hr />
+    <h3>
+      <strong>CALL TO ACTION DATA</strong>
+      <br/>
+      <pre>{{cta}}</pre>
+    </h3>
 
-    <BannerText
-      class="banner-text"
-      :category="page.tutorialType[0].title"
-      :title="page.title"
-      :text="page.summary"
-      :button-text="page.buttonUrl[0].buttonText"
-      :to="page.buttonUrl[0].buttonUrl"
-    />
-
-    <SectionWrapper theme="divider">
-      <DividerWayFinder
-        class="divider"
-        color="help"
-      />
-    </SectionWrapper>
-
-    <SectionWrapper
-      v-if="page.learningOutcomes"
-      section-title="Learning Outcomes"
-    >
-      <RichText
-        v-if="page.learningOutcomes"
-        class="learning-outcomes"
-        :rich-text-content="page.learningOutcomes"
-      />
-    </SectionWrapper>
-
-<!-- ALLFPBS -->
-
-    <FlexibleBlocks
-      class="flexible-content"
-      :blocks="page.blocks"
-    />
-
-
-<!-- RELATED RESOURCES (SIMPLE CARDS) -->
-    <!-- <div
-      v-for="(policy, index) in parsedPolicyBlocks"
-      :key="`PolicyBlocksKey-${policy}-${index}`"
-    >
-      <SectionWrapper
-        :section-title="policy.sectionTitle"
-        :section-summary="policy.sectionSummary"
-      >
-        <SimpleCards
-          :items="policy.parsedAssociatedEntries"
-          button="View all"
-        />
-      </SectionWrapper>
-
-      <SectionWrapper theme="divider">
-        <DividerWayFinder class="divider" />
-      </SectionWrapper>
-    </div> -->
-
-<!-- ABOUT THIS TUTORIAL -->
- <!-- contributorsNoMax -->
-
-    <SectionWrapper level="1">
-      <SectionWrapper>
-        <SectionHeader level="2">About this Tutorial</SectionHeader>
-        <SectionWrapper>
-          <SectionHeader level="3">Awards and Recognition</SectionHeader>
-          <SectionHeader level="3">Authors</SectionHeader>
-          <SectionHeader level="3">Contributors</SectionHeader>
-        </SectionWrapper>
-      </SectionWrapper>
-    </SectionWrapper>
-    <div class="awards" v-if="page.awardsAndRecognition.length > 0">
-
-     <h3 class="awards-recognition">Awards and Recognition</h3>
-    </div>
-    <!--
-      <div class="description-with-image">
-        <div class="description">
-          <RichText
-            v-if="page.donors[0].lastName"
-            class="donors"
-          >
-            Made possible by a gift from {{ parsedDonors }}
-          </RichText>
-
-          <IconWithLink
-            v-if="page.subjectAreas[0]"
-            class="subject-area"
-            icon-name="svg-icon-book"
-            :text="page.subjectAreas[0].title"
-            :to="parsedSubjectAreaLink"
-          />
-
-          <ul v-if="parsedAssociatedLocations.length > 0">
-            <li
-              v-for="(
-                location, index
-              ) in parsedAssociatedLocations"
-              :key="`AssociatedLocation-${location}-${index}`"
-            >
-              <IconWithLink
-                class="associated-locations"
-                icon-name="svg-icon-location"
-                :text="location.title"
-                :to="location.to"
-              />
-            </li>
-          </ul>
-
-          <RichText
-            v-if="page.endowmentDescription"
-            class="description-text"
-            :rich-text-content="page.endowmentDescription"
-          />
-          <NuxtLink
-            v-if="page.spakCode"
-            class="catalog-link"
-            :to="catalogLink"
-          >
-            See all items purchased through this Endowment
-          </NuxtLink>
-        </div>
-
-        <ResponsiveImage
-          v-if="page.heroImage.length > 0"
-          :media="parsedImage"
-          class="bookplate"
-        />
-      </div>
-    </SectionWrapper> -->
-
-<!-- GLOBAL CALLTOACTION -->
-    <SectionWrapper>
-      <BlockCallToAction
-        class="block-call-to-action"
-        :is-global="true"
-      />
-      <!-- <BlockCallToActionTwoUp
-        :items="items"
-      /> -->
-    </SectionWrapper>
   </main>
 </template>
 
 <style lang="scss" scoped>
 .page-news-detail {
-  .highlighted-news {
-    @include visually-hidden;
-  }
+
 }
 </style>
 
