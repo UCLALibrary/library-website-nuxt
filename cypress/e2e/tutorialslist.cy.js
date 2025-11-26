91be2288693a60bf2dbca4c0d1298996ac1bf6c8
tinuola <tinuola@users.noreply.github.com>
2025-11-26 13:47:54 -0800

    feat: Add basic cypress test

---
+++ cypress/e2e/tutorialslist.cy.js
@@ -0,0 +1,20 @@
+describe('Tutorials Listing page', () => {
+  it('Visits the Tutorials Listing Page', () => {
+    cy.visit('/help/tutorials')
+
+    // UCLA Library brand
+    cy.get('.logo-ucla').should('be.visible')
+    cy.get('.page-tutorials-listing').should('be.visible')
+    cy.get('h1.title').should('contain', 'Tutorials')
+    cy.percySnapshot('tutorialslist', { widths: [768, 992, 1200] })
+  })
+
+  it('Searches Tutorials Listing', () => {
+    cy.visit('/help/tutorials/?q=tutorial&filters=', { timeout: 60000 })
+    cy.get('input[type=search]').should(
+      'have.value',
+      'tutorial'
+    )
+    cy.get('h2.about-results').invoke('text').should('not.be.empty')
+  })
+})
