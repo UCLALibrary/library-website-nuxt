e4afacb463ba29bcbc3a00e6967350fdc0b77300
jendiamond <thejendiamond@gmail.com>
2025-11-26 14:38:00 -0800

    feat: add cypress test

---
+++ cypress/e2e/tutorialsdetailpage.cy.js
@@ -0,0 +1,19 @@
+describe('Tutorials Detail page', () => {
+  it('Visit a Tutorials Detail Page', () => {
+    cy.visit('/help/tutorials/electron-configurations')
+
+    // UCLA Library brand
+    cy.get('.logo-ucla').should('be.visible')
+    cy.get('h1.title').should(
+      'contain',
+      'Electron Configurations'
+    )
+    cy.percySnapshot('tutorialsdetailpage', { widths: [768, 992, 1200] })
+  })
+
+  context("When there isn't an entry in craft", () => {
+    it('Raises a 404 error', () => {
+      cy.request({ url: '/help/tutorials/no_entry', failOnStatusCode: false }).its('status').should('equal', 404)
+    })
+  })
+})
