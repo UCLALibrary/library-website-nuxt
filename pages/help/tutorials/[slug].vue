e3b7c66c76ccdc53874304527b8894e4fc3fe3aa
jendiamond <thejendiamond@gmail.com>
2025-11-26 15:37:10 -0800

    feat: add checks for all components

--- pages/help/tutorials/[slug].vue
+++ pages/help/tutorials/[slug].vue
@@ -68,6 +68,14 @@ useHead({
   ]
 })
 
+const parsedButtonText = computed(() => {
+  return _get(page.value, 'button[0].buttonText', '')
+})
+
+const parsedButtonTo = computed(() => {
+  return _get(page.value, 'button[0].buttonUrl', '')
+})
+
 const parsedRelatedResources = computed(() => {
   return (page.value.resourceServiceWorkshop || []).map((obj) => {
     return {
@@ -134,11 +142,14 @@ const parsedContributors = computed(() => {
       :category="page.tutorialType[0].title"
       :title="page.title"
       :text="page.summary"
-      :button-text="page.buttonUrl[0].buttonText"
-      :to="page.buttonUrl[0].buttonUrl"
+      :button-text="parsedButtonText"
+      :to="parsedButtonTo"
     />
 
-    <SectionWrapper theme="divider">
+    <SectionWrapper
+      page.learningOutcomes
+      theme="divider"
+    >
       <DividerWayFinder
         class="divider"
         color="help"
@@ -150,18 +161,21 @@ const parsedContributors = computed(() => {
       section-title="Learning Outcomes"
     >
       <RichText
-        v-if="page.learningOutcomes"
         class="learning-outcomes"
         :rich-text-content="page.learningOutcomes"
       />
     </SectionWrapper>
 
     <FlexibleBlocks
+      v-if="page && page.blocks"
       class="flexible-content"
       :blocks="page.blocks"
     />
 
-    <SectionWrapper theme="divider">
+    <SectionWrapper
+      v-if="page.resourceServiceWorkshop.length"
+      theme="divider"
+    >
       <DividerWayFinder
         class="divider"
         color="help"
@@ -178,16 +192,27 @@ const parsedContributors = computed(() => {
       />
     </SectionWrapper>
 
-    <SectionWrapper theme="divider">
+    <SectionWrapper
+      v-if="page.awardsAndRecognition.length || page.authors.length || page.contributorsNoMax.length"
+      theme="divider"
+    >
       <DividerWayFinder
         class="divider"
         color="help"
       />
     </SectionWrapper>
 
-    <SectionWrapper class="about-this-tutorial" theme="divider" section-title="About this Tutorial">
+    <SectionWrapper
+      v-if="page.awardsAndRecognition.length || page.authors.length || page.contributorsNoMax.length"
+      class="about-this-tutorial"
+      theme="divider"
+      section-title="About this Tutorial"
+    >
 
-      <SectionWrapper class="about">
+      <SectionWrapper
+        v-if="page.awardsAndRecognition.length"
+        class="about"
+      >
         <SectionHeader level="3">Awards and Recognition</SectionHeader>
         <div class="awards-list">
             <BlockSponsor
@@ -201,19 +226,25 @@ const parsedContributors = computed(() => {
         </div>
       </SectionWrapper>
 
-      <SectionWrapper class="about">
+      <SectionWrapper
+        v-if="page.authors.length"
+        class="about"
+      >
         <SectionHeader level="3">Authors</SectionHeader>
         <p>{{parsedAuthors}}</p>
       </SectionWrapper>
 
-      <SectionWrapper class="about">
+      <SectionWrapper
+        v-if="page.contributorsNoMax.length"
+        class="about"
+      >
         <SectionHeader level="3">Contributors</SectionHeader>
         <p>{{parsedContributors}}</p>
       </SectionWrapper>
 
     </SectionWrapper>
 
-    <SectionWrapper>
+    <SectionWrapper v-if="cta">
       <BlockCallToActionTwoUp
         :items="cta"
       />
