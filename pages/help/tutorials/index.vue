da5b1d2e78ca4b9eda8817a50898eec20a2b74f9
tinuola <tinuola@users.noreply.github.com>
2025-12-02 20:01:02 -0800

    Refactor: Pull default listing from craft, search from ES

--- pages/help/tutorials/index.vue
+++ pages/help/tutorials/index.vue
@@ -19,6 +19,7 @@ const { $graphql } = useNuxtApp()
 
 const { data, error } = await useAsyncData('tutorials-list', async () => {
   const data = await $graphql.default.request(TUTORIALS_LIST)
+  console.log('data: ', data)
   return { data }
 })
 // console.log('Tutorials data: ', data.value, 'error: ', error.value)
@@ -50,10 +51,12 @@ const route = useRoute()
 
 // Data
 const page = ref(_get(data.value.data, 'entry', {}))
+const tutorials = ref(_get(data.value.data, 'entries', {}))
 
 watch(data, (newVal, oldVal) => {
   // console.log('In watch preview enabled, newVal, oldVal', newVal, oldVal)
   page.value = _get(newVal.data, 'entry', {})
+  tutorials.value = _get(newVal.data, 'entries', {})
 })
 
 useHead({
@@ -69,10 +72,10 @@ useHead({
 
 const parsedFeaturedTutorial = computed(() => {
   const obj = page?.value.featuredResourcesSection[0].featuredResources[0]
-  return {
+  return [{
     ...obj,
     to: `/${obj.to}`
-  }
+  }]
 })
 
 const parsedSecondaryTutorials = computed(() => {
@@ -80,131 +83,33 @@ const parsedSecondaryTutorials = computed(() => {
     return {
       ...obj,
       to: `/${obj.to}`,
-      image: obj.image[0],
-      // category: obj.tutorialType // TBD
+      image: obj.image[0]
     }
   })
-})
-
-const parsedBlockCTA2Up = computed(() => {
-  return page?.value.callToAction2Up.map((obj) => {
-    return {
-      svgName: obj.icon,
-      title: obj.titleCta,
-      text: obj.summary,
-      name: obj.buttonText,
-      to: obj.buttonLink,
-      isDark: obj.backgroundColor === 'true'
-    }
-  })
-})
-
-const hits = ref([])
-const noResultsFound = ref(false)
-const searchInitiated = ref(false)
-const searchFilters = ref([])
-const searchGenericQuery = ref({
-  queryText: route.query.q || '',
-  queryFilters: parseFilters(route.query.filters || ''),
-})
-
-const routeFilters = computed(() => {
-  return parseFilters(_get(route, 'query.filters', ''))
-})
-
-const hasSearchQuery = computed(() => {
-  return (route.query.q !== undefined && route.query.q !== '')
-    || (route.query.filters && queryFilterHasValues(routeFilters.value, config.tutorialsList.filters))
-})
-
-function parseFilters(filtersString) {
-  if (!filtersString) return {}
-
-  const filters = {}
-  const conditions = filtersString.split(' AND ')
-
-  conditions.forEach((condition) => {
-    const [key, value] = condition.split(':(')
-    const cleanedKey = key.trim()
-    const values = value.replace(')', '').split(' OR ').map(v => v.trim())
-
-    filters[cleanedKey] = values
-  })
-
-  return filters
-}
-
-// ES
-async function searchES() {
-  const queryText = route.query.q || '*'
-  const { keywordSearchWithFilters } = useSearch()
-  const results = await keywordSearchWithFilters(
-    queryText,
-    config.tutorialsList.searchFields,
-    ['tutorial'],
-    parseFilters(route.query.filters || ''),
-    config.tutorialsList.sortField,
-    config.tutorialsList.orderBy,
-    config.tutorialsList.resultFields,
-    []
-  )
-
-  if (hasSearchQuery.value) {
-    searchInitiated.value = true
-  }
-
-  if (results && results.hits && results.hits.total.value > 0) {
-    // console.log('Search ES HITS,', results.hits.hits)
-    hits.value = results.hits.hits
-    noResultsFound.value = false
-  } else {
-    noResultsFound.value = true
-    hits.value = []
-  }
-}
-
-watch(
-  () => route.query,
-  (newVal, oldVal) => {
-    // console.log('ES newVal, oldVal', newVal, oldVal)
-    searchGenericQuery.value.queryText = route.query.q || ''
-    searchGenericQuery.value.queryFilters = parseFilters(route.query.filters || '')
-    searchES()
-  }, { deep: true, immediate: true }
-)
-
-function parseHits(hits = []) {
-  return hits?.filter(obj => obj._source.typeHandle === 'tutorial').map((obj) => {
-    const parseTutorialType = obj._source?.tutorialType?.map(item => item.title).join(', ')
-
-    const parseTutorialCategory = obj._source?.tutorialCategory?.map(item => item.title)
-
-    const cleanedHits = {
-      category: parseTutorialType, // For SectionTeaserCard; the component expects a category field for labels
-      image: _get(obj._source, 'image[0]', null),
-      tutorialCategory: parseTutorialCategory,
-      title: _get(obj._source, 'title', null),
-      text: _get(obj._source, 'summary', null),
-      to: `/${obj._source.uri}`,
-      typeHandle: obj._source.typeHandle
-    }
-    return cleanedHits
-  })
-}
-
-const parseHitsResults = computed(() => {
-  return parseHits(hits.value)
 })
 
 const parsedTutorialsList = computed(() => {
-  if (parseHitsResults.value.length === 0) {
+  if (tutorials.value.length === 0) {
     return null
   }
+
+  const data = tutorials.value.map((obj) => {
+    const stringifyTutorialTypes = obj.tutorialType?.map(item => item.title).join(', ')
+
+    const getTutorialCategories = obj.tutorialCategory?.map(item => item.title)
+
+    return {
+      ...obj,
+      image: obj.image[0],
+      category: stringifyTutorialTypes,
+      tutorialCategory: getTutorialCategories
+    }
+  })
 
   const grouping = []
 
   // Get the category titles to create tutorial grouping / headings
-  parseHitsResults.value.forEach((tutorial) => {
+  data.forEach((tutorial) => {
     // If the tutorial has a category or categories...
     if (tutorial.tutorialCategory.length > 0) {
       tutorial.tutorialCategory.forEach((category) => {
@@ -244,6 +149,119 @@ const parsedTutorialsList = computed(() => {
   const sortedGrouping = grouping.sort((a, b) => a.groupTitle.localeCompare(b.groupTitle))
 
   return sortedGrouping
+  // return grouping
+  // return data
+})
+
+const parsedBlockCTA2Up = computed(() => {
+  return page?.value.callToAction2Up.map((obj) => {
+    return {
+      svgName: obj.icon,
+      title: obj.titleCta,
+      text: obj.summary,
+      name: obj.buttonText,
+      to: obj.buttonLink,
+      isDark: obj.backgroundColor === 'true'
+    }
+  })
+})
+
+const hits = ref([])
+const noResultsFound = ref(false)
+const searchFilters = ref([])
+const searchGenericQuery = ref({
+  queryText: route.query.q || '',
+  queryFilters: parseFilters(route.query.filters || ''),
+})
+
+const routeFilters = computed(() => {
+  return parseFilters(_get(route, 'query.filters', ''))
+})
+
+const hasSearchQuery = computed(() => {
+  return (route.query.q !== undefined && route.query.q !== '')
+    || (route.query.filters && queryFilterHasValues(routeFilters.value, config.tutorialsList.filters))
+})
+
+function parseFilters(filtersString) {
+  if (!filtersString) return {}
+
+  const filters = {}
+  const conditions = filtersString.split(' AND ')
+
+  conditions.forEach((condition) => {
+    const [key, value] = condition.split(':(')
+    const cleanedKey = key.trim()
+    const values = value.replace(')', '').split(' OR ').map(v => v.trim())
+
+    filters[cleanedKey] = values
+  })
+
+  return filters
+}
+
+// ES
+async function searchES() {
+  if (hasSearchQuery.value) {
+    const queryText = route.query.q || '*'
+    const { keywordSearchWithFilters } = useSearch()
+    const results = await keywordSearchWithFilters(
+      queryText,
+      config.tutorialsList.searchFields,
+      ['tutorial'],
+      parseFilters(route.query.filters || ''),
+      config.tutorialsList.sortField,
+      config.tutorialsList.orderBy,
+      config.tutorialsList.resultFields,
+      []
+    )
+
+    if (results && results.hits && results.hits.total.value > 0) {
+      // console.log('Search ES HITS,', results.hits.hits)
+      hits.value = results.hits.hits
+      noResultsFound.value = false
+    } else {
+      noResultsFound.value = true
+      hits.value = []
+    }
+  }
+  else {
+    hits.value = []
+    noResultsFound.value = false
+  }
+}
+
+watch(
+  () => route.query,
+  (newVal, oldVal) => {
+    // console.log('ES newVal, oldVal', newVal, oldVal)
+    searchGenericQuery.value.queryText = route.query.q || ''
+    searchGenericQuery.value.queryFilters = parseFilters(route.query.filters || '')
+    searchES()
+  }, { deep: true, immediate: true }
+)
+
+function parseHits(hits = []) {
+  return hits?.filter(obj => obj._source.typeHandle === 'tutorial').map((obj) => {
+    const parseTutorialType = obj._source?.tutorialType?.map(item => item.title).join(', ')
+
+    const parseTutorialCategory = obj._source?.tutorialCategory?.map(item => item.title)
+
+    const cleanedHits = {
+      category: parseTutorialType, // For SectionTeaserCard; the component expects a category field for labels
+      image: _get(obj._source, 'image[0]', null),
+      tutorialCategory: parseTutorialCategory,
+      title: _get(obj._source, 'title', null),
+      text: _get(obj._source, 'summary', null),
+      to: `/${obj._source.uri}`,
+      typeHandle: obj._source.typeHandle
+    }
+    return cleanedHits
+  })
+}
+
+const parseHitsResults = computed(() => {
+  return parseHits(hits.value)
 })
 
 // Event handler invoked by search-generic component selections
@@ -252,14 +270,11 @@ function getSearchData(data) {
   const filters = []
 
   if (Object.keys(data.filters).length) {
-    searchInitiated.value = true
     for (const key in data.filters) {
       if (data.filters[key].length > 0) {
         filters.push(`${key}:(${data.filters[key].join(' OR ')})`)
       }
     }
-  } else {
-    searchInitiated.value = false
   }
 
   // Use the router to navigate with the new query parameters
@@ -324,35 +339,40 @@ onMounted(async () => {
 
     <!-- FEATURED TUTORIALS -->
     <SectionWrapper
-      v-show="parsedTutorialsList && !hasSearchQuery"
+      v-show="hits.length === 0 && !noResultsFound"
       class="section-no-top-margin"
       :section-title="page.sectionTitle"
     >
       <RichText :rich-text-content="page.richTextDefault" />
-      <DividerWayFinder color="help" />
-      <SectionHeader class="section-header__featured-tutorials">
-        {{ page.featuredResourcesSection[0].titleGeneral }}
-      </SectionHeader>
-      <BannerFeatured
-        :media="parsedFeaturedTutorial.image[0]"
-        :title="parsedFeaturedTutorial.title"
-        breadcrumb="Featured"
-        :align-right="true"
-        :text="parsedFeaturedTutorial.text"
-        :to="parsedFeaturedTutorial.to"
-        prompt="View Tutorial"
-        class="banner section-featured-banner"
-      />
-      <DividerGeneral />
-      <SectionTeaserHighlight
-        class="section"
-        :items="parsedSecondaryTutorials"
-      />
+      <div v-show="parsedFeaturedTutorial.length > 0">
+        <DividerWayFinder color="help" />
+        <SectionHeader class="section-header__featured-tutorials">
+          {{ page.featuredResourcesSection[0].titleGeneral }}
+        </SectionHeader>
+
+        <BannerFeatured
+          :media="parsedFeaturedTutorial[0].image[0]"
+          :title="parsedFeaturedTutorial[0].title"
+          breadcrumb="Featured"
+          :align-right="true"
+          :text="parsedFeaturedTutorial[0].text"
+          :to="parsedFeaturedTutorial[0].to"
+          prompt="View Tutorial"
+          class="banner section-featured-banner"
+        />
+      </div>
+      <div v-show="parsedSecondaryTutorials.length > 0">
+        <DividerGeneral />
+        <SectionTeaserHighlight
+          class="section"
+          :items="parsedSecondaryTutorials"
+        />
+      </div>
       <DividerWayFinder color="help" />
     </SectionWrapper>
 
     <!-- TUTORIALS LISTING -->
-    <SectionWrapper v-show="parsedTutorialsList && !hasSearchQuery">
+    <SectionWrapper v-show="parsedTutorialsList.length > 0 && hits.length === 0 && !noResultsFound">
       <SectionHeader :level="2">
         All Tutorials
       </SectionHeader>
@@ -379,7 +399,7 @@ onMounted(async () => {
 
     <!-- SEARCH RESULTS -->
     <SectionWrapper
-      v-show="hasSearchQuery && searchInitiated"
+      v-show="hits && hits.length > 0"
       class="section-no-top-margin"
     >
       <h2
