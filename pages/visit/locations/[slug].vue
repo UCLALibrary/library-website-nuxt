01369f935dea60639450e22855815f9b621dfaf7
jendiamond <thejendiamond@gmail.com>
2026-04-28 11:52:41 -0700

    fix: linting

--- pages/visit/locations/[slug].vue
+++ pages/visit/locations/[slug].vue
@@ -198,10 +198,7 @@ onMounted(() => {
 </script>
 
 <template lang="html">
-  <main
-    id="main"
-    class="page page-location-detail"
-  >
+  <main id="main" class="page page-location-detail">
     <NavBreadcrumb
       to="/visit/locations/"
       :title="page.title"
