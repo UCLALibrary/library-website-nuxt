2efdd8b71e1e8a9f15665030c263fff06bc12496
jendiamond <thejendiamond@gmail.com>
2026-05-01 11:29:22 -0700

    feat: Add the vue-skip-to to app.vue

--- app/app.vue
+++ app/app.vue
@@ -8,12 +8,35 @@ const { enabled, state } = usePreviewMode()
       color="#ffe800"
       height="3"
     />
-    <!--VueSkipTo
+    <vue-skip-to
       to="#main"
       label="Skip to main content"
-    /-->
+    />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.vue-skip-to {
+  z-index: 300;
+}
+
+.skip-link {
+  position: absolute;
+  transform: translateY(-100%);
+  display: inline-block;
+  background: var(--color-primary-yellow-01);
+  color: var(--color-black);
+  @include step-0;
+  padding: 4px 16px;
+  transition: transform 0.3s;
+  width: auto;
+  left: 0;
+}
+
+.skip-link:focus {
+  transform: translateY(0%);
+}
+</style>
